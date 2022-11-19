9615a53d65f879215b5c9d5d8efbed4c181ef436
Timur Ercan <timur.ercan31@gmail.com>
2022-11-19 16:57:23 +0100

    naming

--- pages/api/health.ts
+++ pages/api/health.ts
@@ -1,12 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string;
+  status: string;
 };
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "Api up and running :)" });
+  res.status(200).json({ status: "Api up and running :)" });
 }
