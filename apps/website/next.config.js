87f76f0e832b09d5446b54ee4dbfa412b6dba771
Timur Ercan <timur.ercan31@gmail.com>
2022-11-24 21:12:53 +0100

    website del

--- apps/website/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    scrollRestoration: true,
-  },
-}
-
-module.exports = nextConfig
--- apps/website/src/pages/_app.jsx
+++
@@ -1,6 +0,0 @@
-import 'focus-visible'
-import '@/styles/tailwind.css'
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
