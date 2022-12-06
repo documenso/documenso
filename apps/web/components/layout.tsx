e8f9d5846a33f99f053bad37b6008c4aafdc9da6
Timur Ercan <timur.ercan31@gmail.com>
2022-12-06 19:25:12 +0100

    build fix

--- apps/web/components/layout.tsx
+++ apps/web/components/layout.tsx
@@ -1,6 +1,7 @@
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import Logo from "./logo";
 
 const user = {
   name: "Timur Ercan",
