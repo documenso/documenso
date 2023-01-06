3cbea886e598ccd71ea9779bf455ac2ec3ff7149
Timur Ercan <timur.ercan31@gmail.com>
2023-01-06 09:14:25 +0100

    oos

--- apps/web/components/settings.tsx
+++ apps/web/components/settings.tsx
@@ -214,22 +214,6 @@ export default function Setttings() {
                       className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"
                     />
                   </div>
-
-                  <div className="col-span-12 sm:col-span-6">
-                    <label
-                      htmlFor="company"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      Company
-                    </label>
-                    <input
-                      type="text"
-                      name="company"
-                      id="company"
-                      autoComplete="organization"
-                      className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"
-                    />
-                  </div>
                 </div>
               </div>
 
