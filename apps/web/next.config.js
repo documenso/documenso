1c6c5a8cc1cfed44f2d510ad3f685be931aad0da
Timur Ercan <timur.ercan31@gmail.com>
2023-01-07 15:45:03 +0100

    somehow working without transpile

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
+require("dotenv").config({ path: "../../.env" });
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   distDir: "build",
 };
+
+// const withTM = require("next-transpile-modules")(["@documenso/prisma"]);
+// const plugins = [];
+// plugins.push(withTM);
+
+// const moduleExports = () =>
+//   plugins.reduce((acc, next) => next(acc), nextConfig);
 
 module.exports = nextConfig;
