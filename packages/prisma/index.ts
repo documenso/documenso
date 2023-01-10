93d96c3768fad841f945eb0f33698021613b9748
Timur Ercan <timur.ercan31@gmail.com>
2023-01-10 14:50:44 +0100

    qoc

--- packages/prisma/index.ts
+++ packages/prisma/index.ts
@@ -8,7 +8,6 @@ declare global {
 const prisma = globalThis.client || new PrismaClient();
 
 // Save for reuse in dev environment to avoid many client instances in dev where restart and reloads
-
 if (process.env.NODE_ENV !== "production") {
   globalThis.client = prisma;
 }
