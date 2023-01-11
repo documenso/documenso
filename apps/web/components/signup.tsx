692fa49009601cd1b8049bd9ad317bc508d79f10
Timur Ercan <timur.ercan31@gmail.com>
2023-01-11 15:27:09 +0100

    qoc, no logo on register

--- apps/web/components/signup.tsx
+++ apps/web/components/signup.tsx
@@ -53,7 +53,6 @@ export default function Signup() {
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-md space-y-8">
           <div>
-            <Logo className="mx-auto h-10 w-auto"></Logo>
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
               Create a shiny, new <br></br>Documenso Account{" "}
               <svg
