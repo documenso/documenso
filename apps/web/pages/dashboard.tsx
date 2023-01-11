9035a0dabf800976af2873c03a289fefd58bfc7c
Timur Ercan <timur.ercan31@gmail.com>
2023-01-11 21:20:52 +0100

    design ideas

--- apps/web/pages/dashboard.tsx
+++ apps/web/pages/dashboard.tsx
@@ -4,21 +4,89 @@ import type { ReactElement } from "react";
 import Layout from "../components/layout";
 import Settings from "../components/settings";
 import type { NextPageWithLayout } from "./_app";
+import {
+  CheckIcon,
+  PaperAirplaneIcon,
+  SunIcon,
+} from "@heroicons/react/24/outline";
 
 const DashboardPage: NextPageWithLayout = () => {
   const status = useSession();
+  const stats = [
+    { name: "New", stat: "0", icon: SunIcon },
+    { name: "Sent", stat: "0", icon: PaperAirplaneIcon },
+    { name: "Signed", stat: "0", icon: CheckIcon },
+  ];
   return (
     <>
       <Head>
         <title>Dashboard | Documenso</title>
       </Head>
-      <div>
-        <p>This is the dashboard page.</p>
-        <div>Mail: {status?.data?.user?.email?.toString()}</div>
-        <div>{status.status}</div>
+      <div className="py-10">
+        <header>
+          <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">
+            Dashboard
+          </h1>
+        </header>
+        <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+          {stats.map((item) => (
+            <div
+              key={item.name}
+              className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6"
+            >
+              <dt className="truncate text-sm font-medium text-gray-500">
+                <item.icon
+                  className="flex-shrink-0 mr-3 h-6 w-6 inline text-neon"
+                  aria-hidden="true"
+                ></item.icon>
+                {item.name}
+              </dt>
+              <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">
+                {item.stat}
+              </dd>
+            </div>
+          ))}
+        </dl>
+        <div className="mt-12">
+          <button
+            type="button"
+            className="relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-neon focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            <svg
+              className="mx-auto h-12 w-12 text-gray-400"
+              stroke="currentColor"
+              fill="none"
+              viewBox="0 00 20 25"
+              aria-hidden="true"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+              />
+            </svg>
+            <span className="mt-2 block text-sm font-medium text-neon">
+              Upload a new document
+            </span>
+          </button>
+        </div>
       </div>
     </>
   );
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+    />
+  </svg>;
 };
 
 // todo layout as component
--- apps/web/pages/documents.tsx
+++ apps/web/pages/documents.tsx
@@ -5,6 +5,7 @@ import Settings from "../components/settings";
 import type { NextPageWithLayout } from "./_app";
 import { SessionProvider } from "next-auth/react";
 import Head from "next/head";
+import { PlusIcon } from "@heroicons/react/24/outline";
 
 const DocumentsPage: NextPageWithLayout = () => {
   const { data: session } = useSession();
@@ -14,7 +15,36 @@ const DocumentsPage: NextPageWithLayout = () => {
       <Head>
         <title>Documents | Documenso</title>
       </Head>
-      This is the documents page
+
+      <div className="text-center mt-24">
+        <svg
+          className="mx-auto h-12 w-12 text-gray-400"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+          />
+        </svg>
+
+        <h3 className="mt-2 text-sm font-medium text-gray-900">No documents</h3>
+        <p className="mt-1 text-sm text-gray-500">
+          Get started by creating a new document.
+        </p>
+        <div className="mt-6">
+          <button
+            type="button"
+            className="inline-flex items-center rounded-md border border-transparent bg-neon px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            <PlusIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+            New Project
+          </button>
+        </div>
+      </div>
     </>
   );
 };
