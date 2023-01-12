37aaeef6b52284b7540911ee85685f2e0a318f57
Timur Ercan <timur.ercan31@gmail.com>
2023-01-12 14:57:37 +0100

    qoc, install toaster

--- apps/web/pages/documents.tsx
+++ apps/web/pages/documents.tsx
@@ -25,8 +25,8 @@ const DocumentsPage: NextPageWithLayout = () => {
           aria-hidden="true"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
           />
         </svg>
