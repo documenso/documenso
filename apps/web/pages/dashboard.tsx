ba2fc66e24d85902077b9571605ba2ec667f7068
Timur Ercan <timur.ercan31@gmail.com>
2023-01-12 11:12:24 +0100

    design

--- apps/web/pages/dashboard.tsx
+++ apps/web/pages/dashboard.tsx
@@ -13,7 +13,7 @@ import {
 const DashboardPage: NextPageWithLayout = () => {
   const status = useSession();
   const stats = [
-    { name: "New", stat: "0", icon: SunIcon },
+    { name: "Draft", stat: "0", icon: SunIcon },
     { name: "Sent", stat: "0", icon: PaperAirplaneIcon },
     { name: "Signed", stat: "0", icon: CheckIcon },
   ];
@@ -60,8 +60,8 @@ const DashboardPage: NextPageWithLayout = () => {
               aria-hidden="true"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
               />
             </svg>
--- apps/web/pages/documents.tsx
+++ apps/web/pages/documents.tsx
@@ -41,7 +41,7 @@ const DocumentsPage: NextPageWithLayout = () => {
             className="inline-flex items-center rounded-md border border-transparent bg-neon px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
           >
             <PlusIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
-            New Project
+            Upload Document
           </button>
         </div>
       </div>
