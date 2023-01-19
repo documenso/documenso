a7830fedb9d52b93704301d480d9ad09bbfbdca3
Timur Ercan <timur.ercan31@gmail.com>
2023-01-19 19:27:37 +0100

    pdf

--- apps/web/components/pdf-viewer.js
+++ apps/web/components/pdf-viewer.js
@@ -2,7 +2,12 @@ import { useState } from "react";
 import { Document, Page } from "react-pdf/dist/esm/entry.webpack5";
 
 export default function PDFViewer() {
+  const [file, setFile] = useState("./sample.pdf");
   const [numPages, setNumPages] = useState(null);
+
+  function onFileChange(event) {
+    setFile(event.target.files[0]);
+  }
 
   function onDocumentLoadSuccess({ numPages: nextNumPages }) {
     setNumPages(nextNumPages);
@@ -10,24 +15,24 @@ export default function PDFViewer() {
 
   return (
     <>
-      <Document
-        file="/sample.pdf"
-        onLoadSuccess={onDocumentLoadSuccess}
-        onLoadError={(err) => {
-          console.error(err);
-        }}
-        onSourceError={(err) => {
-          console.error(err);
-        }}
-        onLoadProgress={(err) => {
-          console.error(err);
-        }}
-        onPassword={(err) => {
-          console.error(err);
-        }}
-      >
-        <Page pageNumber={1} />
-      </Document>
+      <div>
+        <div>
+          <label htmlFor="file">Load from file:</label>{" "}
+          <input onChange={onFileChange} type="file" />
+        </div>
+        <div>
+          <Document file={file} onLoadSuccess={onDocumentLoadSuccess}>
+            {Array.from({ length: numPages }, (_, index) => (
+              <Page
+                key={`page_${index + 1}`}
+                pageNumber={index + 1}
+                renderAnnotationLayer={false}
+                renderTextLayer={false}
+              />
+            ))}
+          </Document>
+        </div>
+      </div>
     </>
   );
 }
