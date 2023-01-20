d1d32a971f420ac0f12018bfe8a4a80c6460a2d5
Timur Ercan <timur.ercan31@gmail.com>
2023-01-20 13:54:20 +0100

    document by id endpoint

--- apps/web/pages/api/documents/[id].ts
+++ apps/web/pages/api/documents/[id].ts
@@ -1,1 +1,35 @@
-export {};
+import {
+  defaultHandler,
+  defaultResponder,
+  getUserFromToken,
+} from "@documenso/lib/server";
+import prisma from "@documenso/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+import { useRouter } from "next/router";
+
+async function getHandler(req: NextApiRequest, res: NextApiResponse) {
+  const user = getUserFromToken(req, res);
+  const { id: documentId } = req.query;
+
+  if (!user) return;
+
+  if (!documentId) {
+    res.status(400).send("Missing parameter documentId.");
+    return;
+  }
+
+  let document = await prisma.document.findFirst({
+    where: {
+      id: +documentId,
+    },
+  });
+
+  if (!document)
+    res.status(404).end(`No document with id ${documentId} found.`);
+
+  res.status(200).json(document);
+}
+
+export default defaultHandler({
+  GET: Promise.resolve({ default: defaultResponder(getHandler) }),
+});
