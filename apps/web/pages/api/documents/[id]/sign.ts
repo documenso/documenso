260701bf5131e5fc3fd0f2cd6cdbe4090a96f409
Timur Ercan <timur.ercan31@gmail.com>
2023-01-20 13:58:21 +0100

    sign endpoint stub

--- apps/web/pages/api/documents/[id]/sign.ts
+++ apps/web/pages/api/documents/[id]/sign.ts
@@ -1,1 +1,35 @@
-export {};
+import {
+  defaultHandler,
+  defaultResponder,
+  getUserFromToken,
+} from "@documenso/lib/server";
+import prisma from "@documenso/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+
+async function postHandler(req: NextApiRequest, res: NextApiResponse) {
+  const user = getUserFromToken(req, res);
+  const { id: documentId } = req.query;
+  if (!user) return;
+
+  if (!documentId) {
+    res.status(400).send("Missing parameter documentId.");
+    return;
+  }
+
+  let document = await prisma.document.findFirst({
+    where: {
+      id: +documentId,
+    },
+  });
+
+  if (!document)
+    res.status(404).end(`No document with id ${documentId} found.`);
+
+  // todo sign stuff
+
+  res.status(200);
+}
+
+export default defaultHandler({
+  POST: Promise.resolve({ default: defaultResponder(postHandler) }),
+});
