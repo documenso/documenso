c61fc30420bf22563230f62e2a916d487fb2cb13
Timur Ercan <timur.ercan31@gmail.com>
2023-01-27 16:54:08 +0100

    recipient token model and sending endpoint

--- apps/web/pages/api/documents/[id]/send.ts
+++ apps/web/pages/api/documents/[id]/send.ts
@@ -5,11 +5,14 @@ import {
 } from "@documenso/lib/server";
 import prisma from "@documenso/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
+import { sendSigningRequestMail } from "@documenso/lib/mail";
+import { SendStatus } from "@prisma/client";
 
 async function postHandler(req: NextApiRequest, res: NextApiResponse) {
-  const existingUser = await getUserFromToken(req, res);
+  const user = await getUserFromToken(req, res);
   const { id: documentId } = req.query;
-  const { token: recipientToken } = req.query;
+
+  if (!user) return;
 
   if (!documentId) {
     res.status(400).send("Missing parameter documentId.");
@@ -25,13 +28,21 @@ async function postHandler(req: NextApiRequest, res: NextApiResponse) {
   if (!document)
     res.status(404).end(`No document with id ${documentId} found.`);
 
+  // todo handle sending to single recipient even though more exist
+
   const recipients = prisma.recipient.findMany({
     where: { documentId: +documentId },
   });
 
-  // todo sign stuff
+  (await recipients).forEach(async (recipient) => {
+    sendSigningRequestMail(recipient, document);
+    await prisma.recipient.update({
+      where: { id: recipient.id },
+      data: { sendStatus: SendStatus.SENT },
+    });
+  });
 
-  return res.status(200).end();
+  res.status(200);
 }
 
 export default defaultHandler({
