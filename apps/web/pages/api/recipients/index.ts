c29c2625666fc1c50775294ce2f1683ad5605392
Timur Ercan <timur.ercan31@gmail.com>
2023-01-27 17:14:36 +0100

    create recipient and short uid

---
+++ apps/web/pages/api/recipients/index.ts
@@ -0,0 +1,47 @@
+import {
+  defaultHandler,
+  defaultResponder,
+  getUserFromToken,
+} from "@documenso/lib/server";
+import prisma from "@documenso/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+import short from "short-uuid";
+import { Document as PrismaDocument } from "@prisma/client";
+
+async function postHandler(req: NextApiRequest, res: NextApiResponse) {
+  const user = await getUserFromToken(req, res);
+  const { id: documentId } = req.query;
+  const body = req.body;
+
+  if (!user) return;
+
+  if (!documentId) {
+    res.status(400).send("Missing parameter documentId.");
+    return;
+  }
+
+  const document: PrismaDocument = await prisma.document.findFirstOrThrow({
+    where: {
+      id: +documentId,
+    },
+  });
+
+  // todo encapsulate entity ownerships
+  if (document.userId !== user.id) {
+    return res.status(401).send("User does not have access to this document.");
+  }
+
+  await prisma.recipient.create({
+    data: {
+      documentId: +documentId,
+      email: body.email,
+      token: short.generate(),
+    },
+  });
+
+  return res.status(201).end();
+}
+
+export default defaultHandler({
+  POST: Promise.resolve({ default: defaultResponder(postHandler) }),
+});
