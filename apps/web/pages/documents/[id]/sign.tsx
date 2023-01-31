614392d4aeccfe2fdd504033be00921efb258d3d
Timur Ercan <timur.ercan31@gmail.com>
2023-01-31 17:33:00 +0100

    sign test

--- apps/web/pages/documents/[id]/sign.tsx
+++ apps/web/pages/documents/[id]/sign.tsx
@@ -1,19 +1,26 @@
 import prisma from "@documenso/prisma";
 import Head from "next/head";
-import { ReactElement } from "react";
+import { ReactElement, useEffect } from "react";
 import Layout from "../../../components/layout";
 import Logo from "../../../components/logo";
 import { NextPageWithLayout } from "../../_app";
 import { Router } from "next/router";
 import { ReadStatus } from "@prisma/client";
+import SignaturePad from "signature_pad";
 
 const SignPage: NextPageWithLayout = () => {
+  useEffect(() => {
+    const canvas: any = document.querySelector("canvas");
+    const signaturePad = new SignaturePad(canvas);
+  });
   return (
     <>
       <Head>
         <title>Sign | Documenso</title>
       </Head>
       Hello, please sign at the dotted line.
+      <canvas className="mx-auto bg-neon"></canvas>
+      <hr></hr>
     </>
   );
 };
