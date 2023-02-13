42883f9dd8595c1f5ad6ffe127fc6b9729bb3f71
Timur Ercan <timur.ercan31@gmail.com>
2023-02-13 13:04:15 +0100

    404, 500 pages skeleton

---
+++ apps/web/pages/404.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function FourOhFour() {
+  return (
+    <>
+      <h1>404 - Page Not Found</h1>
+      <Link href="/">Go back home</Link>
+    </>
+  );
+}
