85d4dd9ff199df0687e6a09f56a6b40ef670c978
litaesther10 <estherlitalg@gmail.com>
2023-02-15 14:45:01 +0100

    404 Updated

--- apps/web/pages/404.jsx
+++ apps/web/pages/404.jsx
@@ -6,14 +6,12 @@ import Logo from "../components/logo";
 export default function Custom404() {
   return (
     <>
-      <main className="relative min-h-full bg-gray-200 isolate">
+      <main className="relative min-h-full bg-gray-100 isolate">
         <Logo className="absolute w-20 top-10 left-10" />
 
         <div className="px-6 py-48 mx-auto text-center max-w-7xl sm:py-40 lg:px-8">
-          <p className="text-base font-semibold leading-8 text-brown">
-            404
-          </p>
-          <h1 className="mt-4 text-3xl font-bold tracking-tight text-brown sm:text-5xl font-monteserrat">
+          <p className="text-base font-semibold leading-8 text-brown">404</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-brown sm:text-5xl">
             Page not found
           </h1>
           <p className="mt-4 text-base text-gray-700 sm:mt-6">
@@ -23,9 +21,10 @@ export default function Custom404() {
             <Button
               color="secondary"
               href="/"
-              className="text-base font-semibold leading-7 text-brown font-monteserrat"
+              icon={ArrowSmallLeftIcon}
+              className="text-base font-semibold leading-7 text-brown"
             >
-              <ArrowSmallLeftIcon className="w-5 h-5 mr-2" /> Back to home
+              Back to home
             </Button>
           </div>
         </div>
