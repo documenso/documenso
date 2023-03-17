f77f101e67bb979ca12a2625860c257afa047412
Timur Ercan <timur.ercan31@gmail.com>
2023-03-17 15:05:38 +0100

    unused deps

--- apps/web/components/editor/pdf-signer.tsx
+++ apps/web/components/editor/pdf-signer.tsx
@@ -9,7 +9,6 @@ import {
   CheckBadgeIcon,
   InformationCircleIcon,
 } from "@heroicons/react/24/outline";
-import toast from "react-hot-toast";
 import { FieldType } from "@prisma/client";
 import {
   createOrUpdateField,
