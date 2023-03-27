e79a622dddafd99d89d42339708e97f4993d45a7
Timur Ercan <timur.ercan31@gmail.com>
2023-03-27 12:58:17 +0200

    block non pdf upload

--- packages/features/uploadDocument.ts
+++ packages/features/uploadDocument.ts
@@ -6,7 +6,12 @@ export const uploadDocument = async (event: any) => {
   if (event.target.files && event.target.files[0]) {
     const body = new FormData();
     const document = event.target.files[0];
-    const fileName = event.target.files[0].name;
+    const fileName: string = event.target.files[0].name;
+
+    if (!fileName.endsWith(".pdf")) {
+      toast.error("Non-PDF documents are not supported yet.");
+      return;
+    }
     body.append("document", document || "");
     const response: any = await toast
       .promise(
