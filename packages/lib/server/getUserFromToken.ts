0e4c10b346ae4e33ffaa85a8589ef13969a80d81
Mythie <me@lucasjamessmith.me>
2023-04-05 07:29:03 +1000

    fix: make getUserFromToken ssr and api compatible

--- packages/lib/server/getUserFromToken.ts
+++ packages/lib/server/getUserFromToken.ts
@@ -1,23 +1,28 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@documenso/prisma";
 import { User as PrismaUser } from "@prisma/client";
+import { GetServerSidePropsContext, NextApiRequest, NextApiResponse } from "next";
 import { getToken } from "next-auth/jwt";
-import { signOut } from "next-auth/react";
 
 export async function getUserFromToken(
-  req: NextApiRequest,
-  res: NextApiResponse
+  req: NextApiRequest | GetServerSidePropsContext['req'],
+  res: NextApiResponse | GetServerSidePropsContext['res']
 ): Promise<PrismaUser | null> {
   const token = await getToken({ req });
   const tokenEmail = token?.email?.toString();
 
   if (!token) {
-    if (res.status) res.status(401).send("No session token found for request.");
+    if ('status' in res) {
+      res.status(401).send("No session token found for request.")
+    };
+
     return null;
   }
 
   if (!tokenEmail) {
-    res.status(400).send("No email found in session token.");
+    if ('status' in res) {
+      res.status(400).send("No email found in session token.");
+    }
+
     return null;
   }
 
@@ -26,7 +31,10 @@ export async function getUserFromToken(
   });
 
   if (!user) {
-    if (res) res.status(401).end();
+    if ('status' in res) {
+      res.status(401).end();
+    }
+
     return null;
   }
 
