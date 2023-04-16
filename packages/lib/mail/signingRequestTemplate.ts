d863f89232dce49058a4e07dd2bb15f8782eb622
Mythie <me@lucasjamessmith.me>
2023-04-17 06:54:01 +1000

    fix: signing email breaks on small devices
    Currently the signing email displays poorly on small devices with the line wrapping
    causing the button to look broken.
    
    Resolve this by using whitespace no-wrap.

--- packages/lib/mail/signingRequestTemplate.ts
+++ packages/lib/mail/signingRequestTemplate.ts
@@ -11,8 +11,8 @@ export const signingRequestTemplate = (
   user: any
 ) => {
   const customContent = `
-  <p style="margin: 30px;">
-    <a href="${ctaLink}" style="background-color: #37f095; color: white; border-color: transparent; border-width: 1px; border-radius: 0.375rem; font-size: 18px; padding-left: 16px; padding-right: 16px; padding-top: 10px; padding-bottom: 10px; text-decoration: none; margin-top: 4px; margin-bottom: 4px;">
+  <p style="margin: 30px 0px; text-align: center">
+    <a href="${ctaLink}" style="background-color: #37f095; white-space: nowrap; color: white; border-color: transparent; border-width: 1px; border-radius: 0.375rem; font-size: 18px; padding-left: 16px; padding-right: 16px; padding-top: 10px; padding-bottom: 10px; text-decoration: none; margin-top: 4px; margin-bottom: 4px;">
       ${ctaLabel}
     </a>
   </p>
