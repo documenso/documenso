1e8232905743f3173d2aa58b52245e824b49c4b0
Timur Ercan <timur.ercan31@gmail.com>
2023-04-21 13:48:26 +0200

    pre env condition debug

--- packages/lib/constants.ts
+++ packages/lib/constants.ts
@@ -4,5 +4,6 @@ export const NEXT_PUBLIC_WEBAPP_URL =
     : process.env.NEXT_PUBLIC_WEBAPP_URL;
 
 console.log("IS_PULL_REQUEST:" + process.env.IS_PULL_REQUEST);
+console.log("process.env.IS_PULL_REQUEST === 'true'" + process.env.IS_PULL_REQUEST === "true");
 console.log("RENDER_EXTERNAL_URL:" + process.env.RENDER_EXTERNAL_URL);
 console.log("NEXT_PUBLIC_WEBAPP_URL:" + process.env.NEXT_PUBLIC_WEBAPP_URL);
