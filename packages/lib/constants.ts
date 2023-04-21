507387942c5477bb1434dc1be2da81337ff8ebe2
Timur Ercan <timur.ercan31@gmail.com>
2023-04-21 14:06:22 +0200

    more debug

--- packages/lib/constants.ts
+++ packages/lib/constants.ts
@@ -5,5 +5,7 @@ export const NEXT_PUBLIC_WEBAPP_URL =
 
 console.log("IS_PULL_REQUEST:" + process.env.IS_PULL_REQUEST);
 console.log("process.env.IS_PULL_REQUEST === 'true'" + process.env.IS_PULL_REQUEST === "true");
+console.log("typeof process.env.IS_PULL_REQUEST: " + typeof process.env.IS_PULL_REQUEST);
+console.log("process.env.IS_PULL_REQUEST == 'true'" + process.env.IS_PULL_REQUEST == "true");
 console.log("RENDER_EXTERNAL_URL:" + process.env.RENDER_EXTERNAL_URL);
 console.log("NEXT_PUBLIC_WEBAPP_URL:" + process.env.NEXT_PUBLIC_WEBAPP_URL);
