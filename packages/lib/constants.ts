6f18be6b5bb950d43701339e38edc98603fdab22
Timur Ercan <timur.ercan31@gmail.com>
2023-04-21 12:42:31 +0200

    add render preview env support

--- packages/lib/constants.ts
+++ packages/lib/constants.ts
@@ -1,1 +1,3 @@
-export const NEXT_PUBLIC_WEBAPP_URL = process.env.NEXT_PUBLIC_WEBAPP_URL;
+export const NEXT_PUBLIC_WEBAPP_URL = process.env.IS_PULL_REQUEST
+  ? process.env.RENDER_EXTERNAL_URL
+  : process.env.NEXT_PUBLIC_WEBAPP_URL;
