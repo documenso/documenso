38a8279757261bc9e420832eefd23d6104ebd9cf
Timur Ercan <timur.ercan31@gmail.com>
2023-05-21 20:36:38 +0200

    fix

--- apps/web/pages/documents/[id]/sign.tsx
+++ apps/web/pages/documents/[id]/sign.tsx
@@ -73,7 +73,7 @@ export async function getServerSideProps(context: any) {
     return {
       redirect: {
         permanent: false,
-        destination: `/documents/${recipient.Document.id}/signed?token=recipientToken`,
+        destination: `/documents/${recipient.Document.id}/signed?token=${recipientToken}`,
       },
     };
   }
