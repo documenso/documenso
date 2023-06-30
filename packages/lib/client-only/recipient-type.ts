0de4fe5883344b5673837605a0b201693f64b0b6
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2023-06-30 23:49:34 +0000

    fix: update types from code review

--- packages/lib/client-only/recipient-type.ts
+++ packages/lib/client-only/recipient-type.ts
@@ -1,8 +1,13 @@
 import { Recipient } from '@documenso/prisma/client';
 
-export const type = (recipient: Recipient) =>
-  recipient.sendStatus === 'SENT' && recipient.signingStatus === 'SIGNED'
-    ? 'completed'
-    : recipient.sendStatus === 'SENT' && recipient.signingStatus === 'NOT_SIGNED'
-    ? 'waiting'
-    : 'unsigned';
+export const getRecipientType = (recipient: Recipient) => {
+  if (recipient.sendStatus === 'SENT' && recipient.signingStatus === 'SIGNED') {
+    return 'completed';
+  }
+
+  if (recipient.sendStatus === 'SENT' && recipient.signingStatus === 'NOT_SIGNED') {
+    return 'waiting';
+  }
+
+  return 'unsigned';
+};
