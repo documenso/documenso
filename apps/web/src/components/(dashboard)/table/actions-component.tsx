a27c6c3eb271d56c3e1c21b8efcef083edae0e03
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2023-07-05 20:53:08 +0000

    refactor: use tailwind in styling icons

--- apps/web/src/components/(dashboard)/table/actions-component.tsx
+++ apps/web/src/components/(dashboard)/table/actions-component.tsx
@@ -8,22 +8,19 @@ export function ActionButtons({ documentId }: { documentId: number }) {
   return (
     <div className="flex cursor-pointer gap-6">
       <Edit
-        size={18}
-        color="#71717A"
+        className="text-primary h-5 w-5"
         onClick={() => {
           console.log('Edit Document with id: ', documentId);
         }}
       />
       <Download
-        size={18}
-        color="#71717A"
+        className="text-primary h-5 w-5"
         onClick={() => {
           console.log('Download Document with id: ', documentId);
         }}
       />
       <Trash
-        size={18}
-        color="#71717A"
+        className="text-primary h-5 w-5"
         onClick={() => {
           console.log('Delete Document with id: ', documentId);
         }}
