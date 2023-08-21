94b9b1060b689dd2e2d24106579a1495ae565ef2
Mythie <me@lucasjamessmith.me>
2023-08-21 19:56:18 +1000

    fix: styling updates

--- apps/marketing/src/components/(marketing)/header.tsx
+++ apps/marketing/src/components/(marketing)/header.tsx
@@ -13,19 +13,19 @@ import { MobileNavigation } from './mobile-navigation';
 export type HeaderProps = HTMLAttributes<HTMLElement>;
 
 export const Header = ({ className, ...props }: HeaderProps) => {
-  const [isMobileOpen, setIsMobileOpen] = useState<boolean>(false);
-
-  const handleMenuToggle = () => {
-    setIsMobileOpen(!isMobileOpen);
-  };
+  const [isHamburgerMenuOpen, setIsHamburgerMenuOpen] = useState(false);
 
   return (
     <header className={cn('flex items-center justify-between', className)} {...props}>
-      <Link href="/" className="z-10" onClick={() => isMobileOpen && handleMenuToggle()}>
-        <Image src="/logo.png" alt="Documenso Logo" width={170} height={0}></Image>
+      <Link href="/" className="z-10" onClick={() => setIsHamburgerMenuOpen(false)}>
+        <Image src="/logo.png" alt="Documenso Logo" width={170} height={25} />
       </Link>
 
       <div className="hidden items-center gap-x-6 md:flex">
+        <Link href="/pricing" className="text-sm font-semibold text-[#8D8D8D] hover:text-[#6D6D6D]">
+          Pricing
+        </Link>
+
         <Link href="/blog" className="text-sm font-semibold text-[#8D8D8D] hover:text-[#6D6D6D]">
           Blog
         </Link>
@@ -43,8 +43,14 @@ export const Header = ({ className, ...props }: HeaderProps) => {
         </Link>
       </div>
 
-      <HamburgerMenu menuToggle={handleMenuToggle} isMenuOpen={isMobileOpen} />
-      <MobileNavigation isMenuOpen={isMobileOpen} menuToggle={handleMenuToggle} />
+      <HamburgerMenu
+        onToggleMenuOpen={() => setIsHamburgerMenuOpen((v) => !v)}
+        isMenuOpen={isHamburgerMenuOpen}
+      />
+      <MobileNavigation
+        isMenuOpen={isHamburgerMenuOpen}
+        onMenuOpenChange={setIsHamburgerMenuOpen}
+      />
     </header>
   );
 };
--- apps/marketing/src/components/(marketing)/mobile-hamburger.tsx
+++ apps/marketing/src/components/(marketing)/mobile-hamburger.tsx
@@ -1,34 +1,18 @@
 'use client';
 
-import { useEffect } from 'react';
-
 import { Menu, X } from 'lucide-react';
 
 import { Button } from '@documenso/ui/primitives/button';
 
-import { useWindowSize } from '~/hooks/use-window-size';
-
 export interface HamburgerMenuProps {
   isMenuOpen: boolean;
-  menuToggle: () => void;
+  onToggleMenuOpen?: () => void;
 }
 
-export const HamburgerMenu = ({ isMenuOpen, menuToggle }: HamburgerMenuProps) => {
-  const { width } = useWindowSize();
-
-  useEffect(() => {
-    // Update document.body.style.overflow based on the menu state
-    // If the window width is less than 768px, we want to prevent scrolling when the menu is open
-    if (width < 768 && isMenuOpen) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = 'auto';
-    }
-  }, [isMenuOpen, width]);
-
+export const HamburgerMenu = ({ isMenuOpen, onToggleMenuOpen }: HamburgerMenuProps) => {
   return (
     <div className="flex md:hidden">
-      <Button variant="default" className="z-20 w-10 p-0" onClick={menuToggle}>
+      <Button variant="outline" className="z-20 w-10 p-0" onClick={onToggleMenuOpen}>
         {isMenuOpen ? <X /> : <Menu />}
       </Button>
     </div>
