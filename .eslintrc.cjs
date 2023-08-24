2f4d9955e21596ec6cb7bbaf39fd0fb84309d650
Adithya Krishna <aadithya794@gmail.com>
2023-08-24 05:24:51 +0000

    feat: updated eslint rules
    Signed-off-by: Adithya Krishna <aadithya794@gmail.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
-  extends: ['@documenso/eslint-config', 'plugin:jsx-a11y/recommended'],
+  extends: ['@documenso/eslint-config'],
   rules: {
     '@next/next/no-img-element': 'off',
   },
--- packages/eslint-config/index.cjs
+++ packages/eslint-config/index.cjs
@@ -28,10 +28,13 @@ module.exports = {
 
   rules: {
     'react/no-unescaped-entities': 'off',
-
+    '@next/next/no-html-link-for-pages': 'off',
+    'jsx-a11y/role-supports-aria-props': 'off',
+    'react/jsx-curly-brace-presence': ['error', { props: 'never', children: 'never' }],
+    'react/self-closing-comp': ['error', { component: true, html: true }],
     'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-
+    'unused-imports/no-unused-imports': 'error',
     // We never want to use `as` but are required to on occasion to handle
     // shortcomings in third-party and generated types.
     //
