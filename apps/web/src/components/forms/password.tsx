c902fb8412b553c2b67f6a98716a26dbd7bc7d48
Adithya Krishna <adikrish@redhat.com>
2023-08-26 20:42:15 +0530

    fix: updated padding and set patterns
    Signed-off-by: Adithya Krishna <adikrish@redhat.com>

--- apps/web/src/components/forms/password.tsx
+++ apps/web/src/components/forms/password.tsx
@@ -83,14 +83,6 @@ export const PasswordForm = ({ className }: PasswordFormProps) => {
     }
   };
 
-  const onShowPassword = () => {
-    setShowPassword(!showPassword);
-  };
-
-  const onShowConfirmPassword = () => {
-    setShowConfirmPassword(!showConfirmPassword);
-  };
-
   return (
     <form
       className={cn('flex w-full flex-col gap-y-4', className)}
@@ -105,16 +97,16 @@ export const PasswordForm = ({ className }: PasswordFormProps) => {
           <Input
             id="password"
             type={showPassword ? 'text' : 'password'}
-            className="bg-background mt-2"
+            className="bg-background mt-2 pr-10"
             {...register('password')}
           />
 
           <Button
             variant="link"
             type="button"
-            className="absolute right-0 top-0 flex h-full items-center justify-center"
+            className="absolute right-0 top-0 flex h-full items-center justify-center pr-3"
             aria-label={showPassword ? 'Mask password' : 'Reveal password'}
-            onClick={onShowPassword}
+            onClick={() => setShowPassword((showPassword) => !showPassword)}
           >
             {showPassword ? (
               <EyeOff className="text-slate-500" />
@@ -136,16 +128,16 @@ export const PasswordForm = ({ className }: PasswordFormProps) => {
           <Input
             id="repeated-password"
             type={showConfirmPassword ? 'text' : 'password'}
-            className="bg-background mt-2"
+            className="bg-background mt-2 pr-10"
             {...register('repeatedPassword')}
           />
 
           <Button
             variant="link"
             type="button"
-            className="absolute right-0 top-0 flex h-full items-center justify-center"
+            className="absolute right-0 top-0 flex h-full items-center justify-center pr-3"
             aria-label={showConfirmPassword ? 'Mask password' : 'Reveal password'}
-            onClick={onShowConfirmPassword}
+            onClick={() => setShowConfirmPassword((showConfirmPassword) => !showConfirmPassword)}
           >
             {showConfirmPassword ? (
               <EyeOff className="text-slate-500" />
