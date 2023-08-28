090752c539f3f803e12d4622afb9a7881d168daa
Adithya Krishna <adikrish@redhat.com>
2023-08-28 08:14:42 +0530

    feat: added new eslint rules
    Signed-off-by: Adithya Krishna <adikrish@redhat.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,28 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
-  extends: ['@documenso/eslint-config'],
+  extends: [
+    '@documenso/eslint-config',
+    'plugin:package-json/recommended',
+    'plugin:import/recommended',
+  ],
+  plugins: ['package-json', 'import', 'simple-import-sort'],
   rules: {
     '@next/next/no-img-element': 'off',
+    'no-multi-spaces': [
+      'error',
+      {
+        ignoreEOLComments: false,
+        exceptions: {
+          BinaryExpression: false,
+          VariableDeclarator: false,
+          ImportDeclaration: false,
+          Property: false,
+        },
+      },
+    ],
+    'import/named': 'off',
+    'no-duplicate-imports': 'error',
   },
   settings: {
     next: {
