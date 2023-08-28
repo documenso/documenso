e8aacb05e18c29272fa0ef2c34a2eb225a3a15de
Adithya Krishna <adikrish@redhat.com>
2023-08-28 10:09:20 +0530

    feat: added & updated eslint rules
    Signed-off-by: Adithya Krishna <adikrish@redhat.com>

--- packages/eslint-config/index.cjs
+++ packages/eslint-config/index.cjs
@@ -5,10 +5,14 @@ module.exports = {
     'prettier',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:prettier/recommended',
+    'async',
+    'async/node',
+    'async/typescript',
   ],
 
-  plugins: ['prettier'],
+  plugins: ['eslint-plugin-node', 'prettier'],
 
   env: {
     node: true,
@@ -19,6 +23,8 @@ module.exports = {
   parser: '@typescript-eslint/parser',
 
   parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['../../apps/*/tsconfig.json', '../../packages/*/tsconfig.json'],
     ecmaVersion: 2022,
     ecmaFeatures: {
       jsx: true,
@@ -31,6 +37,21 @@ module.exports = {
 
     'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-redeclare': ['error'],
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/await-thenable': 'error',
+    '@typescript-eslint/require-await': 'error',
+    '@typescript-eslint/no-misused-promises': 'error',
+    '@typescript-eslint/promise-function-async': 'error',
+    'node/handle-callback-err': 'error',
+    'node/no-callback-literal': 'error',
+    'node/no-sync': 'error',
+    'no-return-await': 'error',
+    'no-await-in-loop': 'error',
+    'no-async-promise-executor': 'error',
+    'no-promise-executor-return': 'error',
+    'require-atomic-updates': 'error',
+    'prefer-promise-reject-errors': 'error',
 
     // We never want to use `as` but are required to on occasion to handle
     // shortcomings in third-party and generated types.
