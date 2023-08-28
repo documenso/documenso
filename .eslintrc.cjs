aad52a3e2e3fb96b315e4898b9c2821383d7e98a
Adithya Krishna <adikrish@redhat.com>
2023-08-28 08:37:59 +0530

    fix: updated eslint config
    Signed-off-by: Adithya Krishna <adikrish@redhat.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,28 +1,9 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
-  extends: [
-    '@documenso/eslint-config',
-    'plugin:package-json/recommended',
-    'plugin:import/recommended',
-  ],
-  plugins: ['package-json', 'import', 'simple-import-sort'],
+  extends: ['@documenso/eslint-config'],
   rules: {
     '@next/next/no-img-element': 'off',
-    'no-multi-spaces': [
-      'error',
-      {
-        ignoreEOLComments: false,
-        exceptions: {
-          BinaryExpression: false,
-          VariableDeclarator: false,
-          ImportDeclaration: false,
-          Property: false,
-        },
-      },
-    ],
-    'import/named': 'off',
-    'no-duplicate-imports': 'error',
   },
   settings: {
     next: {
