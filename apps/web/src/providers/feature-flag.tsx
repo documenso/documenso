463dc48ea664c91bf305326a0ef73e3b16d59043
Mythie <me@lucasjamessmith.me>
2023-08-30 17:31:23 +1000

    fix: extract feature-flag zod schema to separate file

--- apps/web/src/providers/feature-flag.tsx
+++ apps/web/src/providers/feature-flag.tsx
@@ -2,8 +2,6 @@
 
 import { createContext, useCallback, useContext, useEffect, useState } from 'react';
 
-import { z } from 'zod';
-
 import {
   FEATURE_FLAG_POLL_INTERVAL,
   LOCAL_FEATURE_FLAGS,
@@ -12,14 +10,7 @@ import {
 
 import { getAllFlags } from '~/helpers/get-feature-flag';
 
-export const ZFeatureFlagValueSchema = z.union([
-  z.boolean(),
-  z.string(),
-  z.number(),
-  z.undefined(),
-]);
-
-export type TFeatureFlagValue = z.infer<typeof ZFeatureFlagValueSchema>;
+import { TFeatureFlagValue } from './feature-flag.types';
 
 export type FeatureFlagContextValue = {
   getFlag: (_key: string) => TFeatureFlagValue;
