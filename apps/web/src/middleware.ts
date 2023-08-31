e756a21fda12e515e7c66001b76596c7712deed2
Mythie <me@lucasjamessmith.me>
2023-08-31 13:12:50 +1000

    fix: retain redirect to documents rather than dashboard

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -4,7 +4,7 @@ import { getToken } from 'next-auth/jwt';
 
 export default async function middleware(req: NextRequest) {
   if (req.nextUrl.pathname === '/') {
-    const redirectUrl = new URL('/dashboard', req.url);
+    const redirectUrl = new URL('/documents', req.url);
 
     return NextResponse.redirect(redirectUrl);
   }
@@ -13,8 +13,7 @@ export default async function middleware(req: NextRequest) {
     const token = await getToken({ req });
 
     if (token) {
-      console.log('has has token', req.url);
-      const redirectUrl = new URL('/dashboard', req.url);
+      const redirectUrl = new URL('/documents', req.url);
 
       return NextResponse.redirect(redirectUrl);
     }
