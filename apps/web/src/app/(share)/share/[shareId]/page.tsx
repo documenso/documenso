739f3763dd8c707d0ba9650ea2edd0e093677bab
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2023-09-05 11:40:42 +0000

    feat: update share page to match latest changes

--- apps/web/src/app/(share)/share/[shareId]/page.tsx
+++ apps/web/src/app/(share)/share/[shareId]/page.tsx
@@ -5,21 +5,11 @@ import { notFound } from 'next/navigation';
 
 import { getSharingId } from '@documenso/lib/server-only/share/get-share-id';
 
-// import Redirect from './redirect';
+import Redirect from './redirect';
 
-type MetadataProps = {
-  params: { shareId: string };
+export const metadata: Metadata = {
+  title: 'Documenso - Share',
 };
-
-export async function generateMetadata({ params }: MetadataProps): Promise<Metadata> {
-  const id = params.shareId;
-  const share = await getSharingId({ shareId: id });
-  const signature = share?.recipent.name || share?.recipent.email;
-
-  return {
-    title: 'Documenso - Share',
-  };
-}
 
 export type SharePageProps = {
   params: {
@@ -42,7 +32,7 @@ export default async function SharePage({ params: { shareId } }: SharePageProps)
     <div className="flex h-screen flex-col items-center justify-center">
       <h1 className="my-2 text-4xl font-semibold">Share Page</h1>
       <p className="my-2 text-xl">Redirecting...</p>
-      {/* <Redirect /> */}
+      <Redirect />
     </div>
   );
 }
