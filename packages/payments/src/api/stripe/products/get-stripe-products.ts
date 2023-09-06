c9e89d30bb4d5ee3c602e2003643a1631dcee846
divyanshchahar <divyanshchahar@outlook.com>
2023-09-06 20:41:52 +0530

    feat: adds get-stripe-products.ts

---
+++ packages/payments/src/api/stripe/products/get-stripe-products.ts
@@ -0,0 +1,35 @@
+/* eslint-disable no-async-promise-executor */
+
+/* eslint-disable @typescript-eslint/promise-function-async */
+
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { stripeProvider } from '../../../providers';
+
+export interface stripeProductsResponse {
+  data: [stripeProduct];
+}
+
+export interface stripeProduct {
+  id: string;
+  name: string;
+}
+
+/**
+ * Retrieve all `product` objects from stripe
+ * @returns Promise which resolves into array of stripe's `product` objects
+ */ export interface stripeProductsResponse {
+  data: [stripeProduct];
+}
+export const getStripeProduct = (): Promise<stripeProduct[]> => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await stripeProvider.products.list();
+
+      const products = response.data;
+
+      resolve(products);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
