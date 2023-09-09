9186cb4d7b220eb9e65c307fe0f7709286d0b182
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2023-09-09 10:42:03 +0000

    fix: hide popover when user selects a recipients

--- packages/ui/primitives/popover.tsx
+++ packages/ui/primitives/popover.tsx
@@ -10,6 +10,8 @@ const Popover = PopoverPrimitive.Root;
 
 const PopoverTrigger = PopoverPrimitive.Trigger;
 
+const PopoverClose = PopoverPrimitive.Close;
+
 const PopoverContent = React.forwardRef<
   React.ElementRef<typeof PopoverPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
@@ -30,4 +32,4 @@ const PopoverContent = React.forwardRef<
 
 PopoverContent.displayName = PopoverPrimitive.Content.displayName;
 
-export { Popover, PopoverTrigger, PopoverContent };
+export { Popover, PopoverTrigger, PopoverContent, PopoverClose };
