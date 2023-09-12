399826a6f55bc5543c4079532618273614a6fc4a
Mythie <me@lucasjamessmith.me>
2023-09-12 17:53:38 +1000

    fix: add removed layout guard

--- packages/lib/server-only/admin/get-recipients-stats.ts
+++ packages/lib/server-only/admin/get-recipients-stats.ts
@@ -16,6 +16,7 @@ export const getRecipientsStats = async () => {
     [SendStatus.SENT]: 0,
     [SendStatus.NOT_SENT]: 0,
   };
+
   results.forEach((result) => {
     const { readStatus, signingStatus, sendStatus, _count } = result;
     stats[readStatus] += _count;
@@ -23,5 +24,6 @@ export const getRecipientsStats = async () => {
     stats[sendStatus] += _count;
     stats.TOTAL_RECIPIENTS += _count;
   });
+
   return stats;
 };
