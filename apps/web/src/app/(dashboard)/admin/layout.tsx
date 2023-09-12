599e857a1e282141fba6add03b5cf7c6e3c110d0
Mythie <me@lucasjamessmith.me>
2023-09-12 17:53:38 +1000

    fix: add removed layout guard

--- apps/web/src/app/(dashboard)/admin/layout.tsx
+++ apps/web/src/app/(dashboard)/admin/layout.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 
+import { redirect } from 'next/navigation';
+
+import { getRequiredServerComponentSession } from '@documenso/lib/next-auth/get-server-session';
+import { isAdmin } from '@documenso/lib/next-auth/guards/is-admin';
+
 import { AdminNav } from './nav';
 
 export type AdminSectionLayoutProps = {
   children: React.ReactNode;
 };
 
-export default function AdminSectionLayout({ children }: AdminSectionLayoutProps) {
+export default async function AdminSectionLayout({ children }: AdminSectionLayoutProps) {
+  const user = await getRequiredServerComponentSession();
+
+  if (!isAdmin(user)) {
+    redirect('/documents');
+  }
+
   return (
     <div className="mx-auto mt-16 w-full max-w-screen-xl px-4 md:px-8">
       <div className="grid grid-cols-12 gap-x-8 md:mt-8">
