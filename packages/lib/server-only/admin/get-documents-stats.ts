581f08c59bf1d39bffb499f746b7942b7a9674c3
Lucas Smith <me@lucasjamessmith.me>
2023-09-12 07:25:44 +0000

    fix: update layout and wording

--- packages/lib/server-only/admin/get-documents-stats.ts
+++ packages/lib/server-only/admin/get-documents-stats.ts
@@ -1,5 +1,26 @@
 import { prisma } from '@documenso/prisma';
+import { ExtendedDocumentStatus } from '@documenso/prisma/types/extended-document-status';
 
-export const getDocsCount = async () => {
-  return await prisma.document.count();
+export const getDocumentStats = async () => {
+  const counts = await prisma.document.groupBy({
+    by: ['status'],
+    _count: {
+      _all: true,
+    },
+  });
+
+  const stats: Record<Exclude<ExtendedDocumentStatus, 'INBOX'>, number> = {
+    [ExtendedDocumentStatus.DRAFT]: 0,
+    [ExtendedDocumentStatus.PENDING]: 0,
+    [ExtendedDocumentStatus.COMPLETED]: 0,
+    [ExtendedDocumentStatus.ALL]: 0,
+  };
+
+  counts.forEach((stat) => {
+    stats[stat.status] = stat._count._all;
+
+    stats.ALL += stat._count._all;
+  });
+
+  return stats;
 };
