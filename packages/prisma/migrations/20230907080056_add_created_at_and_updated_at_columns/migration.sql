5a99a0b5eb3285daffbc286f1d185c4c517ac575
Mythie <me@lucasjamessmith.me>
2023-09-14 13:07:55 +1000

    fix: update migration for timestamp columns

--- packages/prisma/migrations/20230907080056_add_created_at_and_updated_at_columns/migration.sql
+++ packages/prisma/migrations/20230907080056_add_created_at_and_updated_at_columns/migration.sql
@@ -1,3 +1,13 @@
 -- AlterTable
-ALTER TABLE "Document" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE "Document" ADD COLUMN "createdAt" TIMESTAMP(3);
+ALTER TABLE "Document" ADD COLUMN "updatedAt" TIMESTAMP(3);
+
+-- DefaultValues
+UPDATE "Document"
+SET
+  "createdAt" = COALESCE("created"::TIMESTAMP, NOW()),
+  "updatedAt" = COALESCE("created"::TIMESTAMP, NOW());
+
+-- AlterColumn
+ALTER TABLE "Document" ALTER COLUMN "createdAt" SET NOT NULL DEFAULT NOW();
+ALTER TABLE "Document" ALTER COLUMN "updatedAt" SET NOT NULL DEFAULT NOW();
