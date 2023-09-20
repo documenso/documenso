71c7a6ee8ca0680f4a8f8d80b15548b940e43d27
Mythie <me@lucasjamessmith.me>
2023-09-20 02:32:06 +0000

    chore: add visibility toggle to reset password

--- apps/web/src/components/forms/signup.tsx
+++ apps/web/src/components/forms/signup.tsx
@@ -125,7 +125,7 @@ export const SignUpForm = ({ className }: SignUpFormProps) => {
             type="button"
             className="absolute right-0 top-0 flex h-full items-center justify-center pr-3"
             aria-label={showPassword ? 'Mask password' : 'Reveal password'}
-            onClick={() => setShowPassword((showPassword) => !showPassword)}
+            onClick={() => setShowPassword((show) => !show)}
           >
             {showPassword ? (
               <EyeOff className="text-muted-foreground h-5 w-5" />
