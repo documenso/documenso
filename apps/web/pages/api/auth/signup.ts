0acce4cc7243b75be82038afd356290441866a60
Ronit Panda <ronit@superkit.io>
2023-09-21 19:42:49 +0530

    fix: adds try catch block to serve generalized error messages on signup handler

--- apps/web/pages/api/auth/signup.ts
+++ apps/web/pages/api/auth/signup.ts
@@ -5,50 +5,54 @@ import prisma from "@documenso/prisma";
 import { IdentityProvider } from "@prisma/client";
 
 async function postHandler(req: NextApiRequest, res: NextApiResponse) {
-  const { email, password, source } = req.body;
-  const cleanEmail = email.toLowerCase();
+  try {
+    const { email, password, source } = req.body;
+    const cleanEmail = email.toLowerCase();
 
-  if (!cleanEmail || !/.+@.+/.test(cleanEmail)) {
-    res.status(400).json({ message: "Invalid email" });
-    return;
-  }
+    if (!cleanEmail || !/.+@.+/.test(cleanEmail)) {
+      res.status(400).json({ message: "Invalid email" });
+      return;
+    }
 
-  if (!password || password.trim().length < 7) {
-    return res.status(400).json({
-      message: "Password should be at least 7 characters long.",
+    if (!password || password.trim().length < 7) {
+      return res.status(400).json({
+        message: "Password should be at least 7 characters long.",
+      });
+    }
+
+    // User already exists if email already exists
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        email: cleanEmail,
+      },
     });
+
+    if (existingUser) {
+      const message: string = "This email is already registered.";
+      return res.status(409).json({ message });
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    await prisma.user.upsert({
+      where: { email: cleanEmail },
+      update: {
+        password: hashedPassword,
+        emailVerified: new Date(Date.now()),
+        identityProvider: IdentityProvider.DOCUMENSO,
+      },
+      create: {
+        email: cleanEmail,
+        password: hashedPassword,
+        identityProvider: IdentityProvider.DOCUMENSO,
+        source: source,
+      },
+    });
+
+    res.status(201).end();
+  } catch (error: any) {
+    res.status(500).json({ message: "Something went wrong" });
   }
-
-  // User already exists if email already exists
-  const existingUser = await prisma.user.findFirst({
-    where: {
-      email: cleanEmail,
-    },
-  });
-
-  if (existingUser) {
-    const message: string = "This email is already registered.";
-    return res.status(409).json({ message });
-  }
-
-  const hashedPassword = await hashPassword(password);
-
-  await prisma.user.upsert({
-    where: { email: cleanEmail },
-    update: {
-      password: hashedPassword,
-      emailVerified: new Date(Date.now()),
-      identityProvider: IdentityProvider.DOCUMENSO,
-    },
-    create: {
-      email: cleanEmail,
-      password: hashedPassword,
-      identityProvider: IdentityProvider.DOCUMENSO,
-      source: source,
-    },
-  });
-
-  res.status(201).end();
 }
 
 export default defaultHandler({
