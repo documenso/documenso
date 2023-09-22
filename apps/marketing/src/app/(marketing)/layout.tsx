ebebceea1f59341426799cea2884ed9911c3f7f4
David Nguyen <davidngu28@gmail.com>
2023-09-22 12:27:39 +1000

    feat: add uninserted field validation

--- apps/marketing/src/app/(marketing)/layout.tsx
+++ apps/marketing/src/app/(marketing)/layout.tsx
@@ -9,8 +9,8 @@ export type MarketingLayoutProps = {
 
 export default function MarketingLayout({ children }: MarketingLayoutProps) {
   return (
-    <div className="relative max-w-[100vw] overflow-y-auto overflow-x-hidden pt-20 md:pt-28">
-      <div className="bg-background/50 fixed left-0 top-0 z-50 w-full backdrop-blur-md">
+    <div className="relative max-w-[100vw] pt-20 md:pt-28">
+      <div className="fixed left-0 top-0 z-50 w-full bg-white/50 backdrop-blur-md">
         <Header className="mx-auto h-16 max-w-screen-xl px-4 md:h-20 lg:px-8" />
       </div>
 
