e8205c1390b0a8346db7779580a1e4c0d4bf4235
Mythie <me@lucasjamessmith.me>
2023-09-27 07:52:24 +1000

    fix: better share links

--- apps/web/src/app/(share)/share/[slug]/page.tsx
+++ apps/web/src/app/(share)/share/[slug]/page.tsx
@@ -1,11 +1,30 @@
 import { Metadata } from 'next';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 
-import { Redirect } from './redirect';
+import { APP_BASE_URL } from '@documenso/lib/constants/app';
 
-export const metadata: Metadata = {
-  title: 'Documenso - Share',
+type SharePageProps = {
+  params: { slug: string };
 };
 
+export function generateMetadata({ params: { slug } }: SharePageProps) {
+  return {
+    title: 'Documenso - Share',
+    description: 'I just signed a document with Documenso!',
+    openGraph: {
+      images: [`${APP_BASE_URL}/share/${slug}/opengraph`],
+    },
+  } satisfies Metadata;
+}
+
 export default function SharePage() {
-  return <Redirect />;
+  const userAgent = headers().get('User-Agent') ?? '';
+
+  // https://stackoverflow.com/questions/47026171/how-to-detect-bots-for-open-graph-with-user-agent
+  if (/bot|facebookexternalhit|WhatsApp|google|bing|duckduckbot|MetaInspector/i.test(userAgent)) {
+    return null;
+  }
+
+  redirect(process.env.NEXT_PUBLIC_MARKETING_URL ?? 'http://localhost:3001');
 }
