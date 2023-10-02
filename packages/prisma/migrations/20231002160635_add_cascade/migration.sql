e4026b579fe185144232b0bb2406b1ec79de26dd
nafees nazik <nafeesnazik21@gmail.com>
2023-10-02 21:53:39 +0530

    chore: remove migration

--- packages/prisma/migrations/20231002160635_add_cascade/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "DocumentShareLink" DROP CONSTRAINT "DocumentShareLink_documentId_fkey";
-
--- AddForeignKey
-ALTER TABLE "DocumentShareLink" ADD CONSTRAINT "DocumentShareLink_documentId_fkey" FOREIGN KEY ("documentId") REFERENCES "Document"("id") ON DELETE CASCADE ON UPDATE CASCADE;
