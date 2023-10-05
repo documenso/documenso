b653e4bfec10a0d344567231747dd77f01750dfa
zahid47 <epiczahid@gmail.com>
2023-10-05 07:46:50 +0600

    fix: only persist signature if they click confirm

--- apps/marketing/src/components/(marketing)/widget.tsx
+++ apps/marketing/src/components/(marketing)/widget.tsx
@@ -391,7 +391,7 @@ export const Widget = ({ className, children, ...props }: WidgetProps) => {
 
           <SignaturePad
             className="aspect-video w-full rounded-md border"
-            defaultValue={draftSignatureDataUrl || ''}
+            defaultValue={signatureDataUrl || ''}
             onChange={setDraftSignatureDataUrl}
           />
 
