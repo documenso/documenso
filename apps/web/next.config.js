f75f191a9a93e650c6fb0476079f8d0bf34462ca
Mythie <me@lucasjamessmith.me>
2023-10-17 13:50:54 +1100

    fix: add redirects for v0.9 requests

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -37,6 +37,32 @@ const config = {
       },
     ];
   },
+  async redirects() {
+    return [
+      {
+        permanent: true,
+        source: '/documents/:id/sign',
+        destination: '/sign/:token',
+        has: [
+          {
+            type: 'query',
+            key: 'token',
+          },
+        ],
+      },
+      {
+        permanent: true,
+        source: '/documents/:id/signed',
+        destination: '/sign/:token',
+        has: [
+          {
+            type: 'query',
+            key: 'token',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = config;
