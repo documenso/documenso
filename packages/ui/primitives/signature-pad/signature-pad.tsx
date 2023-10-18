94e10634fd5edffbee15fb3388c044e724404cd1
Sid <sidvermas1234@gmail.com>
2023-10-18 12:12:45 +0530

    fix: changed the .env path

--- packages/ui/primitives/signature-pad/signature-pad.tsx
+++ packages/ui/primitives/signature-pad/signature-pad.tsx
@@ -125,7 +125,6 @@ export const SignaturePad = ({
     setIsPressed(false);
 
     const point = Point.fromEvent(event, DPI, $el.current);
-
     const newPoints = [...points];
 
     if (addPoint) {
@@ -226,6 +225,13 @@ export const SignaturePad = ({
       />
 
       <div className="absolute bottom-4 right-4">
+        <button
+          type="button"
+          className="focus-visible:ring-ring ring-offset-background text-muted-foreground rounded-full p-0 text-xs focus-visible:outline-none focus-visible:ring-2"
+          onClick={() => {}}
+        >
+          Undo
+        </button>
         <button
           type="button"
           className="focus-visible:ring-ring ring-offset-background text-muted-foreground rounded-full p-0 text-xs focus-visible:outline-none focus-visible:ring-2"
