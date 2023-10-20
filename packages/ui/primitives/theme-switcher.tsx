fca0e1e61fea2b7c3470f9fbda9633e0b09f69a4
Adithya Krishna <aadithya794@gmail.com>
2023-10-20 19:12:50 +0530

    chore: adjusted the styling
    Signed-off-by: Adithya Krishna <aadithya794@gmail.com>

--- packages/ui/primitives/theme-switcher.tsx
+++ packages/ui/primitives/theme-switcher.tsx
@@ -12,37 +12,41 @@ interface ThemeSwitcherProps {
 const ThemeSwitcher: FC<ThemeSwitcherProps> = ({ theme, setTheme }) => {
   const variants = {
     light: { x: 0 },
-    dark: { x: 'calc((2.15rem + 0.25rem) * 1)' },
+    dark: { x: 'calc((2.15rem + 0.30rem) * 1)' },
     system: { x: 'calc((2rem + 0.5rem) * 2)' },
   };
+
+  const iconColor = theme === 'light' ? 'text-gray-600' : 'text-white';
 
   return (
     <div
       className={`relative flex items-center rounded-full p-1 ${
-        theme === 'dark' ? 'bg-gray-300' : 'bg-gray-200'
+        theme === 'light' ? 'bg-gray-200' : 'bg-stone-700'
       }`}
     >
       <motion.div
-        className="absolute h-8 w-10 rounded-full bg-white shadow-sm"
+        className={`absolute h-8 w-10 rounded-full shadow-sm ${
+          theme === 'light' ? 'bg-white' : 'bg-stone-600'
+        }`}
         initial={false}
         animate={theme}
         variants={variants}
         transition={{ duration: 0.15 }}
       ></motion.div>
       <button
-        className="z-10 flex h-8 w-10 items-center justify-center rounded-full"
+        className={`z-10 flex h-8 w-10 items-center justify-center rounded-full ${iconColor}`}
         onClick={() => setTheme('light')}
       >
         <Sun className="h-5 w-5" />
       </button>
       <button
-        className="z-10 flex h-8 w-10 items-center justify-center rounded-full"
+        className={`z-10 flex h-8 w-10 items-center justify-center rounded-full ${iconColor}`}
         onClick={() => setTheme('dark')}
       >
         <MoonStar className="h-5 w-5" />
       </button>
       <button
-        className="z-10 flex h-8 w-10 items-center justify-center rounded-full"
+        className={`z-10 flex h-8 w-10 items-center justify-center rounded-full ${iconColor}`}
         onClick={() => setTheme('system')}
       >
         <Monitor className="h-5 w-5" />
