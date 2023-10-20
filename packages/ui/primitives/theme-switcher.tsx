ecff22a69a85f563b617e2c02ace5d75478b5659
Adithya Krishna <aadithya794@gmail.com>
2023-10-20 19:43:31 +0530

    chore: moved to framer motion based transitions
    Signed-off-by: Adithya Krishna <aadithya794@gmail.com>

--- packages/ui/primitives/theme-switcher.tsx
+++ packages/ui/primitives/theme-switcher.tsx
@@ -16,37 +16,42 @@ const ThemeSwitcher: FC<ThemeSwitcherProps> = ({ theme, setTheme }) => {
     system: { x: 'calc((2rem + 0.5rem) * 2)' },
   };
 
-  const iconColor = theme === 'light' ? 'text-gray-600' : 'text-white';
+  const getIconColor = (currentTheme: 'light' | 'dark' | 'system') =>
+    theme === currentTheme ? 'text-black dark:text-white' : 'text-gray-400';
 
   return (
     <div
-      className={`relative flex items-center rounded-full p-1 ${
-        theme === 'light' ? 'bg-gray-200' : 'bg-stone-700'
+      className={`relative flex items-center rounded-full bg-gray-200 p-1 dark:bg-stone-700 ${
+        theme === 'dark' ? 'dark' : ''
       }`}
     >
       <motion.div
-        className={`absolute h-8 w-10 rounded-full shadow-sm ${
-          theme === 'light' ? 'bg-white' : 'bg-stone-600'
-        }`}
+        className="absolute h-8 w-10 rounded-full bg-white shadow-sm dark:bg-stone-600"
         initial={false}
         animate={theme}
         variants={variants}
-        transition={{ duration: 0.15 }}
+        transition={{ duration: 0.2 }}
       ></motion.div>
       <button
-        className={`z-10 flex h-8 w-10 items-center justify-center rounded-full ${iconColor}`}
+        className={`z-10 flex h-8 w-10 items-center justify-center rounded-full ${getIconColor(
+          'light',
+        )}`}
         onClick={() => setTheme('light')}
       >
         <Sun className="h-5 w-5" />
       </button>
       <button
-        className={`z-10 flex h-8 w-10 items-center justify-center rounded-full ${iconColor}`}
+        className={`z-10 flex h-8 w-10 items-center justify-center rounded-full ${getIconColor(
+          'dark',
+        )}`}
         onClick={() => setTheme('dark')}
       >
         <MoonStar className="h-5 w-5" />
       </button>
       <button
-        className={`z-10 flex h-8 w-10 items-center justify-center rounded-full ${iconColor}`}
+        className={`z-10 flex h-8 w-10 items-center justify-center rounded-full ${getIconColor(
+          'system',
+        )}`}
         onClick={() => setTheme('system')}
       >
         <Monitor className="h-5 w-5" />
