324485b803f2132aecab3a1703b5d7b2d795d2c8
Tameem Asim <tmeemu15@gmail.com>
2023-10-30 10:56:14 +0500

    fix: page-not-found at share page for document signed in single-player-mode #605

--- packages/ui/components/document/document-share-button.tsx
+++ packages/ui/components/document/document-share-button.tsx
@@ -58,7 +58,7 @@ export const DocumentShareButton = ({ token, documentId, className }: DocumentSh
 
   const onCopyClick = async () => {
     if (shareLink) {
-      await copyShareLink(`${window.location.origin}/share/${shareLink.slug}`);
+      await copyShareLink(`${process.env.NEXT_PUBLIC_WEBAPP_URL}/share/${shareLink.slug}`);
     } else {
       await createAndCopyShareLink({
         token,
