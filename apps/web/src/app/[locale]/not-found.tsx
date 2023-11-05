80071dd6e6c0a0481483253d6d0d93e1f8753359
serge <sergewilfried@outlook.fr>
2023-11-05 08:55:02 +0000

    feat(web): add i18n support for not-found page feat(web): add middleware for handling missing locale in URL feat(ui): add English translations for dashboard page

--- apps/web/src/app/[locale]/not-found.tsx
+++ apps/web/src/app/[locale]/not-found.tsx
@@ -1,24 +1,30 @@
 import Link from 'next/link';
+import { useParams } from 'next/navigation';
 
 import { getServerComponentSession } from '@documenso/lib/next-auth/get-server-session';
+import { useTranslation } from '@documenso/ui/i18n/client';
+import { LocaleTypes } from '@documenso/ui/i18n/settings';
 import { Button } from '@documenso/ui/primitives/button';
 
 import NotFoundPartial from '~/components/partials/not-found';
 
 export default async function NotFound() {
   const { session } = await getServerComponentSession();
+  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+  const locale = useParams()?.locale as LocaleTypes;
 
+  const { t } = useTranslation(locale, 'not-found');
   return (
     <NotFoundPartial>
       {session && (
         <Button className="w-32" asChild>
-          <Link href="/documents">Documents</Link>
+          <Link href="/documents">{t(`documents`)}</Link>
         </Button>
       )}
 
       {!session && (
         <Button className="w-32" asChild>
-          <Link href="/signin">Sign In</Link>
+          <Link href="/signin">{t(`sign-in`)}</Link>
         </Button>
       )}
     </NotFoundPartial>
