b3d9721b40bd38f55eafc0fe09b81bf91ac9aa63
nafees nazik <nafeesnazik21@gmail.com>
2023-11-06 20:41:01 +0530

    feat: add enable procedure

---
+++ packages/lib/server-only/2fa/enable-2fa.ts
@@ -0,0 +1,61 @@
+import { TOTPController } from 'oslo/otp';
+
+import { ErrorCode } from '@documenso/lib/next-auth/error-codes';
+import { prisma } from '@documenso/prisma';
+import { User } from '@documenso/prisma/client';
+
+import { decryptSymmetric } from '../../universal/crypto';
+
+type enableTwoFactorAuthenticationOptions = {
+  user: User;
+  code: string;
+};
+
+const hexStringToArrayBuffer = (hexString: string) => {
+  const buffer = Buffer.from(hexString, 'hex');
+  return buffer.buffer.slice(buffer.byteOffset, buffer.byteOffset + buffer.byteLength);
+};
+
+export const enableTwoFactorAuthentication = async ({
+  user,
+  code,
+}: enableTwoFactorAuthenticationOptions) => {
+  const encryptionKey = process.env.DOCUMENSO_ENCRYPTION_KEY;
+
+  if (!encryptionKey) {
+    throw new Error(ErrorCode.INTERNAL_SEVER_ERROR);
+  }
+
+  if (user.identityProvider !== 'DOCUMENSO') {
+    throw new Error(ErrorCode.CREDENTIALS_NOT_FOUND);
+  }
+
+  if (user.twoFactorEnabled) {
+    throw new Error(ErrorCode.TWO_FACTOR_ALREADY_ENABLED);
+  }
+
+  if (!user.twoFactorSecret) {
+    throw new Error(ErrorCode.TWO_FACTOR_SETUP_REQUIRED);
+  }
+
+  const secret = decryptSymmetric({ encryptedData: user.twoFactorSecret, key: encryptionKey });
+
+  const otpController = new TOTPController();
+
+  const isValidToken = await otpController.verify(code, hexStringToArrayBuffer(secret));
+
+  if (!isValidToken) {
+    throw new Error(ErrorCode.INCORRECT_TWO_FACTOR_CODE);
+  }
+
+  await prisma.user.update({
+    where: {
+      id: user.id,
+    },
+    data: {
+      twoFactorEnabled: true,
+    },
+  });
+
+  return { message: '2fa enabled successfully' };
+};
--- packages/trpc/server/two-factor-router/schema.ts
+++ packages/trpc/server/two-factor-router/schema.ts
@@ -3,3 +3,7 @@ import { z } from 'zod';
 export const ZSetupMutation = z.object({
   password: z.string().min(1),
 });
+
+export const ZEnableMutation = z.object({
+  code: z.string().min(1),
+});
