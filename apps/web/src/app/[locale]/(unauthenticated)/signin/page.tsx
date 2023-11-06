5b8118f7751aca60a26c2917b7ae7ac83975509e
serge <sergewilfried@outlook.fr>
2023-11-06 14:29:18 +0000

    feat(i18n): update translation keys in signin and signup pages

--- apps/web/src/app/[locale]/(unauthenticated)/signin/page.tsx
+++ apps/web/src/app/[locale]/(unauthenticated)/signin/page.tsx
@@ -6,20 +6,18 @@ import { SignInForm } from '~/components/forms/signin';
 
 const SignInPage = async ({ locale }) => {
   // Make sure to use the correct namespace here.
-  const { t } = await createTranslation(locale, 'marketing');
+  const { t } = await createTranslation(locale, 'dashboard');
 
   return (
     <div>
-      <h1 className="text-4xl font-semibold">Sign in to your account</h1>
+      <h1 className="text-4xl font-semibold">{t(`sign-in-your-account`)}</h1>
 
-      <p className="text-muted-foreground/60 mt-2 text-sm">
-        Welcome back, we are lucky to have you.
-      </p>
+      <p className="text-muted-foreground/60 mt-2 text-sm">{t(`welcome-back`)}</p>
 
       <SignInForm className="mt-4" />
 
       <p className="text-muted-foreground mt-6 text-center text-sm">
-        Don't have an account?{' '}
+        {t(`no-account`)}{' '}
         <Link href="/signup" className="text-primary duration-200 hover:opacity-70">
           {t('signup')}
         </Link>
@@ -30,7 +28,7 @@ const SignInPage = async ({ locale }) => {
           href="/forgot-password"
           className="text-muted-foreground text-sm duration-200 hover:opacity-70"
         >
-          Forgot your password?
+          {t(`forgot-password`)}
         </Link>
       </p>
     </div>
