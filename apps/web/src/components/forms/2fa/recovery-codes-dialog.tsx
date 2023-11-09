67182224b423d1b9dd8451ba91568bd7c452fd74
nafees nazik <nafeesnazik21@gmail.com>
2023-11-09 19:34:20 +0530

    feat: add recovery codes dialog

---
+++ apps/web/src/components/forms/2fa/recovery-codes-dialog.tsx
@@ -0,0 +1,84 @@
+import { useCopyToClipboard } from '@documenso/lib/client-only/hooks/use-copy-to-clipboard';
+import { Button } from '@documenso/ui/primitives/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@documenso/ui/primitives/dialog';
+import { useToast } from '@documenso/ui/primitives/use-toast';
+
+type RecoveryCodesDialogProps = {
+  open: boolean;
+  onOpenChange: (_val: boolean) => void;
+  backupCodes: string[] | null;
+};
+
+const formatBackupCode = (code: string) => `${code.slice(0, 5)}-${code.slice(5, 10)}`;
+
+export const RecoveryCodesDialog = ({
+  onOpenChange,
+  open,
+  backupCodes,
+}: RecoveryCodesDialogProps) => {
+  const [, copy] = useCopyToClipboard();
+  const { toast } = useToast();
+
+  const handleDownload = () => {
+    if (backupCodes) {
+      const textBlob = new Blob([backupCodes.map(formatBackupCode).join('\n')], {
+        type: 'text/plain',
+      });
+      const downloadLink = document.createElement('a');
+      downloadLink.href = URL.createObjectURL(textBlob);
+      downloadLink.download = 'documenso-2fa-backup-code.txt';
+
+      downloadLink.click();
+
+      URL.revokeObjectURL(downloadLink.href);
+
+      downloadLink.remove();
+    }
+  };
+
+  const handleCopy = async () => {
+    if (backupCodes) {
+      await copy(backupCodes.map(formatBackupCode).join('\n'));
+      toast({
+        title: 'Copied to clipboard',
+        description: 'Your backup codes has been copied to your clipboard.',
+      });
+    }
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Two-factor recovery codes</DialogTitle>
+          <DialogDescription>
+            In case you lose access to your device and are unable to receive two-factor
+            authentication codes, recovery codes can be employed to regain access to your account.
+          </DialogDescription>
+        </DialogHeader>
+
+        {backupCodes && (
+          <div className="bg-secondary mt-5 grid grid-cols-2 gap-1 rounded-md py-2 text-center font-mono md:pl-10 md:pr-10">
+            {backupCodes.map((code) => (
+              <div key={code}>{formatBackupCode(code)}</div>
+            ))}
+          </div>
+        )}
+
+        <DialogFooter>
+          <Button variant="secondary" onClick={handleCopy}>
+            Copy
+          </Button>
+          <Button onClick={handleDownload}>Download</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
