beb84736b8dd1d59a706c8ff9524dce38bb09189
nafees nazik <nafeesnazik21@gmail.com>
2023-11-09 22:05:15 +0530

    feat: add otp login dialog

---
+++ apps/web/src/components/forms/2fa/two-factor-login-dialog.tsx
@@ -0,0 +1,183 @@
+import { useState } from 'react';
+
+import { signIn } from 'next-auth/react';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+
+import { ErrorCode, isErrorCode } from '@documenso/lib/next-auth/error-codes';
+import { Button } from '@documenso/ui/primitives/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@documenso/ui/primitives/dialog';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@documenso/ui/primitives/form/form';
+import { Input } from '@documenso/ui/primitives/input';
+import { Textarea } from '@documenso/ui/primitives/textarea';
+import { useToast } from '@documenso/ui/primitives/use-toast';
+
+type TwoFactorLoginDialogProps = {
+  open: boolean;
+  onOpenChange: (_state: boolean) => void;
+  credentials: { password: string; email: string } | null;
+};
+
+const formSchema = z.object({
+  totpCode: z.string().optional(),
+  backupCode: z.string().optional(),
+});
+
+type TFormSchema = z.infer<typeof formSchema>;
+
+const FIELD_ERROR_MESSAGES: Partial<
+  Record<keyof typeof ErrorCode, { message: string; field: keyof TFormSchema }>
+> = {
+  [ErrorCode.INCORRECT_TWO_FACTOR_CODE]: { message: 'incorrect 2fa code', field: 'totpCode' },
+  [ErrorCode.INCORRECT_TWO_FACTOR_BACKUP_CODE]: {
+    message: 'incorrect backup code',
+    field: 'backupCode',
+  },
+};
+
+export const TwoFactorLoginDialog = ({
+  open,
+  onOpenChange,
+  credentials,
+}: TwoFactorLoginDialogProps) => {
+  const { toast } = useToast();
+  const [credentialType, setCredentialType] = useState<'backup-code' | 'otp'>('otp');
+  const form = useForm<TFormSchema>({
+    defaultValues: {
+      totpCode: '',
+      backupCode: '',
+    },
+  });
+
+  const onSubmit = async (data: TFormSchema) => {
+    if (credentials) {
+      const { email, password } = credentials;
+      const { totpCode, backupCode } = data;
+      try {
+        const result = await signIn('credentials', {
+          email,
+          password,
+          totpCode,
+          backupCode,
+          callbackUrl: '/documents',
+          redirect: false,
+        });
+
+        if (result?.error && isErrorCode(result.error)) {
+          const fieldError = FIELD_ERROR_MESSAGES?.[result.error];
+          if (fieldError) {
+            form.setError(fieldError.field, {
+              type: `custom`,
+              message: fieldError.message,
+            });
+          } else {
+            throw new Error(result.error);
+          }
+        }
+
+        if (!result?.url) {
+          throw new Error('An unknown error occurred');
+        }
+
+        window.location.href = result.url;
+      } catch (err) {
+        toast({
+          title: 'An unknown error occurred',
+          description:
+            'We encountered an unknown error while attempting to sign you In. Please try again later.',
+        });
+      }
+    }
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Two Factor Code</DialogTitle>
+          <DialogDescription>
+            Two-factor authentication enabled. Please enter the six-digit code from your
+            authenticator app.
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form id="2fa-login" onSubmit={form.handleSubmit(onSubmit)}>
+            {credentialType === 'otp' ? (
+              <FormField
+                control={form.control}
+                name="totpCode"
+                rules={{
+                  minLength: 6,
+                  maxLength: 6,
+                  required: {
+                    message: 'this field is required',
+                    value: true,
+                  },
+                }}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Two-Factor Code</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            ) : (
+              <FormField
+                control={form.control}
+                name="backupCode"
+                rules={{
+                  required: {
+                    value: true,
+                    message: 'this field is required',
+                  },
+                }}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Backup Code</FormLabel>
+                    <FormControl>
+                      <Textarea {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            )}
+          </form>
+        </Form>
+        <DialogFooter>
+          <div className="flex w-full items-center justify-between">
+            <Button
+              variant="secondary"
+              onClick={() => {
+                setCredentialType(credentialType === 'otp' ? 'backup-code' : 'otp');
+              }}
+            >
+              Use {credentialType === 'backup-code' ? 'secret' : ' backup'} code
+            </Button>
+
+            <Button type="submit" form="2fa-login">
+              Submit
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
--- apps/web/src/components/forms/signin.tsx
+++ apps/web/src/components/forms/signin.tsx
@@ -17,12 +17,16 @@ import { Input } from '@documenso/ui/primitives/input';
 import { Label } from '@documenso/ui/primitives/label';
 import { useToast } from '@documenso/ui/primitives/use-toast';
 
-const ERROR_MESSAGES = {
+import { TwoFactorLoginDialog } from './2fa/two-factor-login-dialog';
+
+const ERROR_MESSAGES: Partial<Record<keyof typeof ErrorCode, string>> = {
   [ErrorCode.CREDENTIALS_NOT_FOUND]: 'The email or password provided is incorrect',
   [ErrorCode.INCORRECT_EMAIL_PASSWORD]: 'The email or password provided is incorrect',
   [ErrorCode.USER_MISSING_PASSWORD]:
     'This account appears to be using a social login method, please sign in using that method',
 };
+
+const TwoFactorEnabledErrorCode = ErrorCode.TWO_FACTOR_MISSING_CREDENTIALS;
 
 const LOGIN_REDIRECT_PATH = '/documents';
 
@@ -40,6 +44,8 @@ export type SignInFormProps = {
 export const SignInForm = ({ className }: SignInFormProps) => {
   const { toast } = useToast();
   const [showPassword, setShowPassword] = useState(false);
+  const [totpEnabled, setTotpEnabled] = useState(false);
+  const [credentials, setCredentials] = useState<TSignInFormSchema | null>(null);
 
   const {
     register,
@@ -63,10 +69,18 @@ export const SignInForm = ({ className }: SignInFormProps) => {
       });
 
       if (result?.error && isErrorCode(result.error)) {
-        toast({
-          variant: 'destructive',
-          description: ERROR_MESSAGES[result.error],
-        });
+        if (result.error === TwoFactorEnabledErrorCode) {
+          setCredentials({ email, password });
+          setTotpEnabled(true);
+        }
+
+        const serverError = ERROR_MESSAGES?.[result.error];
+        if (serverError) {
+          toast({
+            variant: 'destructive',
+            description: serverError,
+          });
+        }
 
         return;
       }
@@ -99,80 +113,89 @@ export const SignInForm = ({ className }: SignInFormProps) => {
   };
 
   return (
-    <form
-      className={cn('flex w-full flex-col gap-y-4', className)}
-      onSubmit={handleSubmit(onFormSubmit)}
-    >
-      <div>
-        <Label htmlFor="email" className="text-muted-forground">
-          Email
-        </Label>
+    <>
+      <form
+        className={cn('flex w-full flex-col gap-y-4', className)}
+        onSubmit={handleSubmit(onFormSubmit)}
+      >
+        <div>
+          <Label htmlFor="email" className="text-muted-forground">
+            Email
+          </Label>
 
-        <Input id="email" type="email" className="bg-background mt-2" {...register('email')} />
+          <Input id="email" type="email" className="bg-background mt-2" {...register('email')} />
 
-        <FormErrorMessage className="mt-1.5" error={errors.email} />
-      </div>
-
-      <div>
-        <Label htmlFor="password" className="text-muted-forground">
-          <span>Password</span>
-        </Label>
-
-        <div className="relative">
-          <Input
-            id="password"
-            type={showPassword ? 'text' : 'password'}
-            minLength={6}
-            maxLength={72}
-            autoComplete="current-password"
-            className="bg-background mt-2 pr-10"
-            {...register('password')}
-          />
-
-          <Button
-            variant="link"
-            type="button"
-            className="absolute right-0 top-0 flex h-full items-center justify-center pr-3"
-            aria-label={showPassword ? 'Mask password' : 'Reveal password'}
-            onClick={() => setShowPassword((show) => !show)}
-          >
-            {showPassword ? (
-              <EyeOff className="text-muted-foreground h-5 w-5" />
-            ) : (
-              <Eye className="text-muted-foreground h-5 w-5" />
-            )}
-          </Button>
+          <FormErrorMessage className="mt-1.5" error={errors.email} />
         </div>
 
-        <FormErrorMessage className="mt-1.5" error={errors.password} />
-      </div>
+        <div>
+          <Label htmlFor="password" className="text-muted-forground">
+            <span>Password</span>
+          </Label>
 
-      <Button
-        size="lg"
-        loading={isSubmitting}
-        disabled={isSubmitting}
-        className="dark:bg-documenso dark:hover:opacity-90"
-      >
-        {isSubmitting ? 'Signing in...' : 'Sign In'}
-      </Button>
+          <div className="relative">
+            <Input
+              id="password"
+              type={showPassword ? 'text' : 'password'}
+              minLength={6}
+              maxLength={72}
+              autoComplete="current-password"
+              className="bg-background mt-2 pr-10"
+              {...register('password')}
+            />
 
-      <div className="relative flex items-center justify-center gap-x-4 py-2 text-xs uppercase">
-        <div className="bg-border h-px flex-1" />
-        <span className="text-muted-foreground bg-transparent">Or continue with</span>
-        <div className="bg-border h-px flex-1" />
-      </div>
+            <Button
+              variant="link"
+              type="button"
+              className="absolute right-0 top-0 flex h-full items-center justify-center pr-3"
+              aria-label={showPassword ? 'Mask password' : 'Reveal password'}
+              onClick={() => setShowPassword((show) => !show)}
+            >
+              {showPassword ? (
+                <EyeOff className="text-muted-foreground h-5 w-5" />
+              ) : (
+                <Eye className="text-muted-foreground h-5 w-5" />
+              )}
+            </Button>
+          </div>
 
-      <Button
-        type="button"
-        size="lg"
-        variant={'outline'}
-        className="bg-background text-muted-foreground border"
-        disabled={isSubmitting}
-        onClick={onSignInWithGoogleClick}
-      >
-        <FcGoogle className="mr-2 h-5 w-5" />
-        Google
-      </Button>
-    </form>
+          <FormErrorMessage className="mt-1.5" error={errors.password} />
+        </div>
+
+        <Button
+          size="lg"
+          loading={isSubmitting}
+          disabled={isSubmitting}
+          className="dark:bg-documenso dark:hover:opacity-90"
+        >
+          {isSubmitting ? 'Signing in...' : 'Sign In'}
+        </Button>
+
+        <div className="relative flex items-center justify-center gap-x-4 py-2 text-xs uppercase">
+          <div className="bg-border h-px flex-1" />
+          <span className="text-muted-foreground bg-transparent">Or continue with</span>
+          <div className="bg-border h-px flex-1" />
+        </div>
+
+        <Button
+          type="button"
+          size="lg"
+          variant={'outline'}
+          className="bg-background text-muted-foreground border"
+          disabled={isSubmitting}
+          onClick={onSignInWithGoogleClick}
+        >
+          <FcGoogle className="mr-2 h-5 w-5" />
+          Google
+        </Button>
+      </form>
+      <TwoFactorLoginDialog
+        open={totpEnabled}
+        onOpenChange={(val) => {
+          setTotpEnabled(val);
+        }}
+        credentials={credentials}
+      />
+    </>
   );
 };
