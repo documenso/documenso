c26810bb1d0294973ca7c80d0c5a4da978b1c6ac
serge <sergewilfried@outlook.fr>
2023-11-10 15:50:57 +0000

    perf: fix build issue

--- apps/marketing/src/app/[locale]/(marketing)/singleplayer/[token]/success/page.tsx
+++ apps/marketing/src/app/[locale]/(marketing)/singleplayer/[token]/success/page.tsx
@@ -3,17 +3,19 @@ import { notFound } from 'next/navigation';
 import { getDocumentAndRecipientByToken } from '@documenso/lib/server-only/document/get-document-by-token';
 import { getRecipientSignatures } from '@documenso/lib/server-only/recipient/get-recipient-signatures';
 import { DocumentStatus } from '@documenso/prisma/client';
+import { LocaleTypes } from '@documenso/ui/i18n/settings';
 
 import { SinglePlayerModeSuccess } from '~/components/(marketing)/single-player-mode/single-player-mode-success';
 
 export type SinglePlayerModeSuccessPageProps = {
   params: {
     token?: string;
+    locale: LocaleTypes;
   };
 };
 
 export default async function SinglePlayerModeSuccessPage({
-  params: { token },
+  params: { token, locale },
 }: SinglePlayerModeSuccessPageProps) {
   if (!token) {
     return notFound();
@@ -29,5 +31,5 @@ export default async function SinglePlayerModeSuccessPage({
 
   const signatures = await getRecipientSignatures({ recipientId: document.Recipient.id });
 
-  return <SinglePlayerModeSuccess document={document} signatures={signatures} />;
+  return <SinglePlayerModeSuccess locale={locale} document={document} signatures={signatures} />;
 }
