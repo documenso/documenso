6ddd7dd737b4def686019deb58bbfde6ed2362d4
serge <sergewilfried@outlook.fr>
2023-11-10 15:55:18 +0000

    refactor(marketing): refactor SingleplayerPage component feat(marketing): pass locale as prop to SinglePlayerClient component

--- apps/marketing/src/app/[locale]/(marketing)/singleplayer/page.tsx
+++ apps/marketing/src/app/[locale]/(marketing)/singleplayer/page.tsx
@@ -1,10 +1,16 @@
+import { LocaleTypes } from '@documenso/ui/i18n/settings';
+
 import { SinglePlayerClient } from './client';
 
 export const revalidate = 0;
-
+export type SinglePlayerModePageProps = {
+  params: {
+    locale: LocaleTypes;
+  };
+};
 // !: This entire file is a hack to get around failed prerendering of
 // !: the Single Player Mode page. This regression was introduced during
 // !: the upgrade of Next.js to v13.5.x.
-export default function SingleplayerPage() {
-  return <SinglePlayerClient />;
+export default function SingleplayerPage({ params }: SinglePlayerModePageProps) {
+  return <SinglePlayerClient params={params} />;
 }
