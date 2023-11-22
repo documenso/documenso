be0fe079a39f479e29d1274ff0b37495ebe3ca40
Lucas Smith <me@lucasjamessmith.me>
2023-11-22 15:46:21 +1100

    fix: add healthcheck endpoint (#671)

--- packages/trpc/server/router.ts
+++ packages/trpc/server/router.ts
@@ -4,12 +4,9 @@ import { documentRouter } from './document-router/router';
 import { fieldRouter } from './field-router/router';
 import { profileRouter } from './profile-router/router';
 import { shareLinkRouter } from './share-link-router/router';
-import { procedure, router } from './trpc';
+import { router } from './trpc';
 
 export const appRouter = router({
-  health: procedure.query(() => {
-    return { status: 'ok' };
-  }),
   auth: authRouter,
   profile: profileRouter,
   document: documentRouter,
