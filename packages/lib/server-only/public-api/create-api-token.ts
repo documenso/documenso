fbee6eedc10a4d7cc53a8afb323443198d022319
Catalin Pit <catalinpit@gmail.com>
2023-11-24 16:13:09 +0200

    feat: api token functions

---
+++ packages/lib/server-only/public-api/create-api-token.ts
@@ -0,0 +1,34 @@
+import crypto from 'crypto';
+
+import { prisma } from '@documenso/prisma';
+
+// temporary choice for testing only
+import { ONE_WEEK } from '../../constants/time';
+
+type CreateApiTokenInput = {
+  userId: number;
+  tokenName: string;
+};
+
+export const createApiToken = async ({ userId, tokenName }: CreateApiTokenInput) => {
+  // quick implementation for testing; it needs double checking
+  const tokenHash = crypto
+    .createHash('sha512')
+    .update(crypto.randomBytes(32).toString('hex'))
+    .digest('hex');
+
+  const token = await prisma.apiToken.create({
+    data: {
+      token: tokenHash,
+      name: tokenName,
+      userId,
+      expires: new Date(Date.now() + ONE_WEEK),
+    },
+  });
+
+  if (!token) {
+    throw new Error(`Failed to create the API token`);
+  }
+
+  return token;
+};
