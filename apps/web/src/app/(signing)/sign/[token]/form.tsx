b936b5dff81e7c9e7db02ede7c5f95859f402e99
Adithya Krishna <adi@documenso.com>
2023-12-01 14:44:26 +0530

    chore: dummy change
    Signed-off-by: Adithya Krishna <adi@documenso.com>

--- apps/web/src/app/(signing)/sign/[token]/form.tsx
+++ apps/web/src/app/(signing)/sign/[token]/form.tsx
@@ -40,6 +40,7 @@ export const SigningForm = ({ document, recipient, fields }: SigningFormProps) =
   const router = useRouter();
   const { data: session } = useSession();
 
+  // Dummy Change
   const { fullName, signature, setFullName, setSignature, dateFormat, setDateFormat } =
     useRequiredSigningContext();
 
