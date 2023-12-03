dbf5a7e334e9d50cf2978ca06285ae25629d31fd
nafees nazik <nafeesnazik21@gmail.com>
2023-12-03 10:02:28 +0530

    refactor: cleanup unused

--- packages/ui/primitives/document-flow/document-flow-root.tsx
+++ packages/ui/primitives/document-flow/document-flow-root.tsx
@@ -1,11 +1,9 @@
 'use client';
 
-import React, { HTMLAttributes } from 'react';
-
-import { motion } from 'framer-motion';
+import type { HTMLAttributes } from 'react';
+import React from 'react';
 
 import { cn } from '@documenso/ui/lib/utils';
-import { Button } from '@documenso/ui/primitives/button';
 
 export type DocumentFlowFormContainerProps = HTMLAttributes<HTMLFormElement> & {
   children?: React.ReactNode;
@@ -69,100 +67,6 @@ export const DocumentFlowFormContainerContent = ({
       {...props}
     >
       <div className="flex flex-1 flex-col">{children}</div>
-    </div>
-  );
-};
-
-export type DocumentFlowFormContainerFooterProps = HTMLAttributes<HTMLDivElement> & {
-  children?: React.ReactNode;
-};
-
-export const DocumentFlowFormContainerFooter = ({
-  children,
-  className,
-  ...props
-}: DocumentFlowFormContainerFooterProps) => {
-  return (
-    <div className={cn('mt-4 flex-shrink-0', className)} {...props}>
-      {children}
-    </div>
-  );
-};
-
-export type DocumentFlowFormContainerStepProps = {
-  title: string;
-  step: number;
-  maxStep: number;
-};
-
-export const DocumentFlowFormContainerStep = ({
-  step,
-  maxStep,
-}: DocumentFlowFormContainerStepProps) => {
-  return (
-    <div>
-      <p className="text-muted-foreground text-sm">
-        Step <span>{`${step} of ${maxStep}`}</span>
-      </p>
-
-      <div className="bg-muted relative mt-4 h-[2px] rounded-md">
-        <motion.div
-          layout="size"
-          layoutId="document-flow-container-step"
-          className="bg-documenso absolute inset-y-0 left-0"
-          style={{
-            width: `${(100 / maxStep) * step}%`,
-          }}
-        />
-      </div>
-    </div>
-  );
-};
-
-export type DocumentFlowFormContainerActionsProps = {
-  canGoBack?: boolean;
-  canGoNext?: boolean;
-  goNextLabel?: string;
-  goBackLabel?: string;
-  onGoBackClick?: () => void;
-  onGoNextClick?: () => void;
-  loading?: boolean;
-  disabled?: boolean;
-};
-
-export const DocumentFlowFormContainerActions = ({
-  canGoBack = true,
-  canGoNext = true,
-  goNextLabel = 'Continue',
-  goBackLabel = 'Go Back',
-  onGoBackClick,
-  onGoNextClick,
-  loading,
-  disabled,
-}: DocumentFlowFormContainerActionsProps) => {
-  return (
-    <div className="mt-4 flex gap-x-4">
-      <Button
-        type="button"
-        className="dark:bg-muted dark:hover:bg-muted/80 flex-1 bg-black/5 hover:bg-black/10"
-        size="lg"
-        variant="secondary"
-        disabled={disabled || loading || !canGoBack || !onGoBackClick}
-        onClick={onGoBackClick}
-      >
-        {goBackLabel}
-      </Button>
-
-      <Button
-        type="button"
-        className="bg-documenso flex-1"
-        size="lg"
-        disabled={disabled || loading || !canGoNext}
-        loading={loading}
-        onClick={onGoNextClick}
-      >
-        {goNextLabel}
-      </Button>
     </div>
   );
 };
