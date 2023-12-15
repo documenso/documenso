682cb37786ea6ac78cdefb88063d000110d02c1d
Lucas Smith <me@lucasjamessmith.me>
2023-12-15 20:41:54 +1100

    fix: update auth-options

--- packages/lib/next-auth/auth-options.ts
+++ packages/lib/next-auth/auth-options.ts
@@ -164,13 +164,15 @@ export const NEXT_AUTH_OPTIONS: AuthOptions = {
     },
 
     async signIn({ user }) {
+      // We do this to stop OAuth providers from creating an account
+      // when signups are disabled
       if (process.env.NEXT_PUBLIC_DISABLE_SIGNUP === 'true') {
         const userData = await getUserByEmail({ email: user.email! });
 
         return !!userData;
-      } else {
-        return true;
       }
+
+      return true;
     },
   },
 };
