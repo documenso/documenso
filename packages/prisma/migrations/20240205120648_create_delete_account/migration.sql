cab875f68a70512d669622c215cecfd789878403
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2024-02-14 13:20:40 +0000

    fix: update create delete user sql script

--- packages/prisma/migrations/20240205120648_create_delete_account/migration.sql
+++ packages/prisma/migrations/20240205120648_create_delete_account/migration.sql
@@ -1,25 +1,30 @@
 -- Create deleted@documenso.com
-INSERT INTO
-  "public"."User" (
-    "email",
-    "emailVerified",
-    "password",
-    "createdAt",
-    "updatedAt",
-    "lastSignedIn",
-    "roles",
-    "identityProvider",
-    "twoFactorEnabled"
-  )
-VALUES
-  (
-    'deleted@documenso.com',
-    '2024-02-05 11:58:39.668 UTC',
-    NULL,
-    '2024-02-05 11:58:39.670 UTC',
-    '2024-02-05 11:58:39.670 UTC',
-    '2024-02-05 11:58:39.670 UTC',
-    ARRAY['USER'::TEXT]::"public"."Role" [],
-    CAST('GOOGLE'::TEXT AS "public"."IdentityProvider"),
-    FALSE
-  )
+DO $$
+BEGIN  
+  IF NOT EXISTS (SELECT 1 FROM "public"."User" WHERE "email" = 'deleted@documenso.com') THEN  
+    INSERT INTO
+      "public"."User" (
+        "email",
+        "emailVerified",
+        "password",
+        "createdAt",
+        "updatedAt",
+        "lastSignedIn",
+        "roles",
+        "identityProvider",
+        "twoFactorEnabled"
+      )
+    VALUES
+      (
+        'deleted@documenso.com',
+        NOW(),
+        NULL,
+        NOW(),
+        NOW(),
+        NOW(),
+        ARRAY['USER'::TEXT]::"public"."Role" [],
+        CAST('GOOGLE'::TEXT AS "public"."IdentityProvider"),
+        FALSE
+      );
+  END IF;  
+END $$
