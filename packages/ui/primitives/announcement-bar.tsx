0664de93ab1fa5a7709fcbe2e2d4e266f5ca9849
Adithya Krishna <adithya@documenso.com>
2024-02-16 17:54:23 +0530

    feat: update marketing site
    Signed-off-by: Adithya Krishna <adithya@documenso.com>

---
+++ packages/ui/primitives/announcement-bar.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+
+interface AnnouncementBarProps {
+  isShown: boolean;
+}
+
+export const AnnouncementBar: React.FC<AnnouncementBarProps> = ({ isShown }) => {
+  return (
+    isShown && (
+      <div className="flex h-full w-full items-center justify-center gap-4 border-b-2 bg-green-400 p-1">
+        <div className="text-center">
+          <span className="text-sm text-gray-800">
+            Claim your documenso public profile URL now!
+          </span>{' '}
+          <span className="text-sm font-medium text-gray-800">documenso.com/u/yourname</span>
+        </div>
+
+        <div className="flex items-center justify-center gap-4 rounded-lg bg-white px-3 py-1">
+          <div className="text-xs text-gray-900">
+            <Link
+              href="https://app.documenso.com"
+            >
+              Claim now
+            </Link>
+          </div>
+        </div>
+      </div>
+    )
+  );
+};
