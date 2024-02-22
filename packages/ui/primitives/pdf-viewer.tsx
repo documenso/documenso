45fce66239f5fa213028042e6e3dbfcb82dbedcb
David Nguyen <davidngu28@gmail.com>
2024-02-22 16:35:34 +1100

    fix: minor styling changes

--- packages/ui/primitives/pdf-viewer.tsx
+++ packages/ui/primitives/pdf-viewer.tsx
@@ -233,8 +233,8 @@ export const PDFViewer = ({
             {Array(numPages)
               .fill(null)
               .map((_, i) => (
-                <div key={i} className="mb-5">
-                  <div className="border-border  overflow-hidden rounded border will-change-transform first:mt-0 last:mb-0">
+                <div key={i} className="last:-mb-2">
+                  <div className="border-border overflow-hidden rounded border will-change-transform">
                     <PDFPage
                       pageNumber={i + 1}
                       width={width}
@@ -244,7 +244,7 @@ export const PDFViewer = ({
                       onClick={(e) => onDocumentPageClick(e, i + 1)}
                     />
                   </div>
-                  <p className="mt-1.5 text-center text-[11px]">
+                  <p className="text-muted-foreground/80 my-2 text-center text-[11px]">
                     Page {i + 1} of {numPages}
                   </p>
                 </div>
