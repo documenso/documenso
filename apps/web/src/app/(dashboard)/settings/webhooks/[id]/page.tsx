1ec549b8693ece271585627bbf3e17cd956589c1
Mythie <me@lucasjamessmith.me>
2024-02-27 12:54:37 +1100

    chore: add webhook-call model

--- apps/web/src/app/(dashboard)/settings/webhooks/[id]/page.tsx
+++ apps/web/src/app/(dashboard)/settings/webhooks/[id]/page.tsx
@@ -89,11 +89,13 @@ export default function WebhookPage({ params }: WebhookPageOptions) {
         title="Edit webhook"
         subtitle="On this page, you can edit the webhook and its settings."
       />
+
       {isLoading && (
         <div className="absolute inset-0 z-50 flex items-center justify-center bg-white/50">
           <Loader className="h-8 w-8 animate-spin text-gray-500" />
         </div>
       )}
+
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)}>
           <fieldset
