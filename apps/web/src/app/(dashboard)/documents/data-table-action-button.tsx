f013be5a19ca69533f527db8b4f42829cacb3fdf
Lyhour Lay <lyhourlay1@gmail.com>
2024-02-27 15:20:23 -0800

    feat: add appending loging using pdd lib and draw the outline of the certificates

--- apps/web/src/app/(dashboard)/documents/data-table-action-button.tsx
+++ apps/web/src/app/(dashboard)/documents/data-table-action-button.tsx
@@ -1,12 +1,16 @@
 'use client';
 
+import React from 'react';
+
 import Link from 'next/link';
 
 import { CheckCircle, Download, Edit, EyeIcon, Pencil } from 'lucide-react';
 import { useSession } from 'next-auth/react';
+import { PDFDocument, rgb } from 'pdf-lib';
 import { match } from 'ts-pattern';
 
-import { downloadPDF } from '@documenso/lib/client-only/download-pdf';
+import { downloadFile } from '@documenso/lib/client-only/download-file';
+import { getFile } from '@documenso/lib/universal/upload/get-file';
 import { formatDocumentsPath } from '@documenso/lib/utils/teams';
 import type { Document, Recipient, Team, User } from '@documenso/prisma/client';
 import { DocumentStatus, RecipientRole, SigningStatus } from '@documenso/prisma/client';
@@ -48,7 +52,6 @@ export const DataTableActionButton = ({ row, team }: DataTableActionButtonProps)
   const onDownloadClick = async () => {
     try {
       let document: DocumentWithData | null = null;
-
       if (!recipient) {
         document = await trpcClient.document.getDocumentById.query({
           id: row.id,
@@ -61,12 +64,82 @@ export const DataTableActionButton = ({ row, team }: DataTableActionButtonProps)
       }
 
       const documentData = document?.documentData;
-
       if (!documentData) {
         throw Error('No document available');
       }
+      const signatures = await trpcClient.document.getSignaturesByDocumentId.query({
+        id: row.id,
+      });
+      const bytes = await getFile(documentData);
+      const signImg = signatures[0].signatureImageAsBase64;
 
-      await downloadPDF({ documentData, fileName: row.title });
+      const pdfDoc = await PDFDocument.load(bytes);
+      const pages = pdfDoc.getPages();
+      const page = pdfDoc.addPage();
+      const { width, height } = page.getSize();
+      const fontSize = 12;
+      page.drawRectangle({
+        x: width * 0.05,
+        y: height * 0.05,
+        width: width * 0.9,
+        height: height * 0.85,
+        borderColor: rgb(229 / 255, 229 / 255, 229 / 255),
+        borderWidth: 1.5,
+      });
+      page.drawRectangle({
+        x: width * 0.05 + 1.5,
+        y: height * 0.05 + 1.5,
+        width: width * 0.9 - 3,
+        height: height * 0.85 - 3,
+        borderColor: rgb(162 / 255, 231 / 255, 113 / 255),
+        borderWidth: 1.5,
+      });
+
+      // if(signImg) {
+      //   const pngImage = await pdfDoc.embedPng(signImg);
+      //   page.drawImage(pngImage, {
+      //     width: pngImage.width,
+      //     height: pngImage.height,
+      //   });
+      // }
+      page.drawText('Signing Certificate', {
+        x: width * 0.05,
+        y: height * 0.93,
+        size: fontSize + 4,
+      });
+      page.drawText('Signer Events', {
+        x: width * 0.07,
+        y: height * 0.87,
+        size: fontSize,
+      });
+      page.drawText('Signature', {
+        x: (width - 50) / 3 + width * 0.07,
+        y: height * 0.87,
+        size: fontSize,
+      });
+      page.drawText('Timestamp', {
+        x: ((width - 50) * 2) / 3 + width * 0.07,
+        y: height * 0.87,
+        size: fontSize,
+      });
+      page.drawLine({
+        start: { x: width * 0.05, y: height * 0.85 },
+        end: { x: width * 0.95, y: height * 0.85 },
+        thickness: 1.5,
+        color: rgb(229 / 255, 229 / 255, 229 / 255),
+      });
+
+      const pdfBytes = await pdfDoc.save();
+      const blob = new Blob([pdfBytes], {
+        type: 'application/pdf',
+      });
+
+      downloadFile({
+        filename: row.title,
+        data: blob,
+      });
+
+      // await downloadPDF({ documentData, fileName: row.title });
     } catch (err) {
       toast({
         title: 'Something went wrong',
@@ -132,11 +205,13 @@ export const DataTableActionButton = ({ row, team }: DataTableActionButtonProps)
         View
       </Button>
     ))
-    .with({ isComplete: true }, () => (
-      <Button className="w-32" onClick={onDownloadClick}>
-        <Download className="-ml-1 mr-2 inline h-4 w-4" />
-        Download
-      </Button>
-    ))
+    .with({ isComplete: true }, () => {
+      return (
+        <Button className="w-32" onClick={onDownloadClick}>
+          <Download className="-ml-1 mr-2 inline h-4 w-4" />
+          Download
+        </Button>
+      );
+    })
     .otherwise(() => <div></div>);
 };
