a03ce728f35791d09eafc13a4420052fe33c47b0
Adithya Krishna <adithya@documenso.com>
2024-03-05 15:34:42 +0530

    chore: remove ssr
    Signed-off-by: Adithya Krishna <adithya@documenso.com>

--- apps/marketing/src/app/(marketing)/open/typefully.tsx
+++ apps/marketing/src/app/(marketing)/open/typefully.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import type { HTMLAttributes } from 'react';
-import { useEffect, useState } from 'react';
 
 import Image from 'next/image';
 import Link from 'next/link';
@@ -13,34 +12,27 @@ import { Button } from '@documenso/ui/primitives/button';
 export type TypefullyProps = HTMLAttributes<HTMLDivElement>;
 
 export const Typefully = ({ className, ...props }: TypefullyProps) => {
-  const [isSSR, setIsSSR] = useState(true);
-
-  useEffect(() => {
-    setIsSSR(false);
-  }, []);
   return (
     <div className={cn('flex flex-col', className)} {...props}>
       <h3 className="px-4 text-lg font-semibold">Twitter Stats</h3>
 
       <div className="border-border mt-2.5 flex flex-1 items-center justify-center rounded-2xl border py-8 shadow-sm hover:shadow">
-        {!isSSR && (
-          <div className="flex flex-col gap-y-4 text-center">
-            <Image className="opacity-75" src={Twitter} alt="Twitter Logo" width={120} />
+        <div className="flex flex-col gap-y-4 text-center">
+          <Image className="opacity-75" src={Twitter} alt="Twitter Logo" width={120} />
+          <Link href="https://typefully.com/documenso/stats" target="_blank">
+            <h1>Documenso on X</h1>
+          </Link>
+          <Button className="rounded-full" size="sm" asChild>
             <Link href="https://typefully.com/documenso/stats" target="_blank">
-              <h1>Documenso on X</h1>
+              View all stats
             </Link>
-            <Button className="rounded-full" size="sm" asChild>
-              <Link href="https://typefully.com/documenso/stats" target="_blank">
-                View all stats
-              </Link>
-            </Button>
-            <Button className="rounded-full bg-white" size="sm" asChild>
-              <Link href="https://twitter.com/documenso" target="_blank">
-                Follow us on X
-              </Link>
-            </Button>
-          </div>
-        )}
+          </Button>
+          <Button className="rounded-full bg-white" size="sm" asChild>
+            <Link href="https://twitter.com/documenso" target="_blank">
+              Follow us on X
+            </Link>
+          </Button>
+        </div>
       </div>
     </div>
   );
