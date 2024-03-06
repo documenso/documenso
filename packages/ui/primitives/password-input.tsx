38381d587a75138fd3022bd71947256903efc796
Anik Dhabal Babu <adhabal2002@gmail.com>
2024-03-06 08:05:45 +0000

    feat: password hint

--- packages/ui/primitives/password-input.tsx
+++ packages/ui/primitives/password-input.tsx
@@ -1,84 +1,15 @@
 import * as React from 'react';
 
-import { usePathname } from 'next/navigation';
-
 import { Eye, EyeOff } from 'lucide-react';
-import { Dot } from 'lucide-react';
-import { Check } from 'lucide-react';
-import { GoTriangleLeft } from 'react-icons/go';
 
 import { cn } from '../lib/utils';
 import { Button } from './button';
 import type { InputProps } from './input';
 import { Input } from './input';
 
-interface Validation {
-  hint: string;
-  success: boolean;
-}
-
-const PASSWORD_REGEX = {
-  UPPER_CASE: /^(?=.*[A-Z])/,
-  LOWER_CASE: /^(?=.*[a-z])/,
-  NUMBER: /\d/,
-  SPECIAL_CHARACTER: /[\W_]/,
-};
-
-const DEFAULT_VALIDATIONS = [
-  { hint: 'An uppercase character', success: false },
-  { hint: 'A lowercase character', success: false },
-  { hint: 'A number', success: false },
-  { hint: 'A special character', success: false },
-  { hint: '8+ characters long', success: false },
-];
-
 const PasswordInput = React.forwardRef<HTMLInputElement, Omit<InputProps, 'type'>>(
   ({ className, ...props }, ref) => {
     const [showPassword, setShowPassword] = React.useState(false);
-    const [isFocused, setIsFocused] = React.useState(false);
-    const [validations, setValidations] = React.useState<Validation[]>(DEFAULT_VALIDATIONS);
-    const [input, setInput] = React.useState('');
-    const pathname = usePathname();
-
-    const handleFocus = () => setIsFocused(true);
-    const handleBlur = () => setIsFocused(false);
-
-    function handleInputChange(inputVal: string) {
-      const newValidations = [...DEFAULT_VALIDATIONS];
-      const currentInput = inputVal;
-
-      newValidations.forEach((validation) => {
-        validation.success = false;
-      });
-      PASSWORD_REGEX.UPPER_CASE.test(currentInput) && (newValidations[0].success = true);
-      PASSWORD_REGEX.LOWER_CASE.test(currentInput) && (newValidations[1].success = true);
-      PASSWORD_REGEX.NUMBER.test(currentInput) && (newValidations[2].success = true);
-      PASSWORD_REGEX.SPECIAL_CHARACTER.test(currentInput) && (newValidations[3].success = true);
-      currentInput.length >= 8 && currentInput.length <= 72 && (newValidations[4].success = true);
-
-      setValidations(newValidations);
-    }
-
-    const validationHints = (
-      <ul className="-ml-1 max-w-xs py-4 pr-4">
-        {validations.map((validation, index) => (
-          <li
-            key={index}
-            className={`${
-              validation.success ? 'text-green-500' : 'text-muted-foreground'
-            } ml-3 flex items-center text-xs`}
-            style={{ whiteSpace: 'nowrap' }}
-          >
-            {validation.success ? (
-              <Check className="my-1 mr-2 h-4 w-4 text-green-500 " />
-            ) : (
-              <Dot className="text-muted-foreground" />
-            )}
-            {validation.hint}
-          </li>
-        ))}
-      </ul>
-    );
 
     return (
       <div className="relative">
@@ -87,21 +18,7 @@ const PasswordInput = React.forwardRef<HTMLInputElement, Omit<InputProps, 'type'
           className={cn('pr-10', className)}
           ref={ref}
           {...props}
-          value={input}
-          onChange={(e) => {
-            setInput(e.target.value);
-            handleInputChange(e.target.value);
-          }}
-          onFocus={handleFocus}
-          onBlur={handleBlur}
         />
-
-        {isFocused && pathname === '/signup' && (
-          <div className="absolute left-full top-1/2 flex -translate-y-1/2 transform items-center justify-center">
-            <GoTriangleLeft />
-            <div className="bg-background -ml-1 rounded shadow">{validationHints}</div>
-          </div>
-        )}
 
         <Button
           variant="link"
