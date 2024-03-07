10ef5b6e5159f81b2a60ac5d3ff082221947c5e8
Lucas Smith <me@lucasjamessmith.me>
2024-03-07 02:57:02 +0000

    fix: improvements from testing

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-npx prisma migrate deploy --schema ./node_modules/.prisma/client/schema.prisma
+set -x
+
+npx prisma migrate deploy --schema ./packages/prisma/schema.prisma
 
 node apps/web/server.js
