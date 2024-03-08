c463d5a0ed82bc82317e10bc7baa5b2e73f79d63
nafees nazik <nafeesnazik21@gmail.com>
2024-03-08 16:47:38 +0530

    fix: add double quote

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,10 +1,10 @@
 const path = require('path');
 
 const buildEslintCommand = (filenames) =>
-  `eslint --fix ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')} `;
+  `eslint --fix ${filenames.map((f) => `"${path.relative(process.cwd(), f)}"`).join(' ')}`;
 
 const buildPrettierCommand = (filenames) =>
-  `prettier --write ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')} `;
+  `prettier --write ${filenames.map((f) => `"${path.relative(process.cwd(), f)}"`).join(' ')}`;
 
 /** @type {import('lint-staged').Config} */
 module.exports = {
