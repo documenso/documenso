e8b209eb822e9c8280be689599f2ec1ec624adf1
Catalin Pit <25515812+catalinpit@users.noreply.github.com>
2024-03-08 15:46:44 +0200

    fix: fixed cta component

--- apps/marketing/src/components/(marketing)/CTA.tsx
+++ apps/marketing/src/components/(marketing)/CTA.tsx
@@ -6,16 +6,16 @@ import { Card, CardContent } from '@documenso/ui/primitives/card';
 
 export default function CTA() {
   return (
-    <Card spotlight className="mt-12">
-      <CardContent className="flex flex-col items-center justify-center p-6">
+    <Card spotlight className="mt-8">
+      <CardContent className="flex flex-col items-center justify-center p-12">
         <h2 className="text-center text-2xl font-bold">Join the Open Signing Movement</h2>
 
-        <p className="text-muted-foreground max-w-[55ch] text-center leading-normal">
+        <p className="text-muted-foreground mt-4 max-w-[55ch] text-center leading-normal">
           Create your account and start using state-of-the-art document signing. Open and beautiful
           signing is within your grasp.
         </p>
 
-        <Button className="mt-2.5 rounded-full no-underline" size="sm" asChild>
+        <Button className="mt-8 rounded-full no-underline" size="lg" asChild>
           <Link href={`${NEXT_PUBLIC_WEBAPP_URL()}/signup?utm_source=cta`} target="_blank">
             Get started
           </Link>
