0fdb7f7a8d6f80f74cada410c825b4bc000dc5fe
Catalin Pit <25515812+catalinpit@users.noreply.github.com>
2024-03-08 15:30:08 +0200

    fix: changed to card component

--- apps/marketing/src/components/(marketing)/CTA.tsx
+++ apps/marketing/src/components/(marketing)/CTA.tsx
@@ -1,24 +1,26 @@
 import Link from 'next/link';
 
+import { NEXT_PUBLIC_WEBAPP_URL } from '@documenso/lib/constants/app';
 import { Button } from '@documenso/ui/primitives/button';
+import { Card, CardContent } from '@documenso/ui/primitives/card';
 
 export default function CTA() {
   return (
-    <div className="bg-background shadow-foreground/5 border-documenso mt-12 flex flex-col items-center justify-center rounded-lg border-4 border-double p-8 shadow-lg hover:shadow-md">
-      <h2 className="mt-4 text-center text-2xl font-bold">
-        Join the Open Document Signing Movement
-      </h2>
+    <Card spotlight className="mt-12">
+      <CardContent className="flex flex-col items-center justify-center p-6">
+        <h2 className="text-center text-2xl font-bold">Join the Open Signing Movement</h2>
 
-      <p className="text-muted-foreground max-w-[55ch] text-center text-lg leading-normal">
-        Create your account and start using state-of-the-art document signing. Open and beautiful
-        signing is within your grasp.
-      </p>
+        <p className="text-muted-foreground max-w-[55ch] text-center leading-normal">
+          Create your account and start using state-of-the-art document signing. Open and beautiful
+          signing is within your grasp.
+        </p>
 
-      <Button className="mt-4 rounded-full no-underline" size="sm" asChild>
-        <Link href="https://app.documenso.com/signup?utm_source=cta" target="_blank">
-          Get started
-        </Link>
-      </Button>
-    </div>
+        <Button className="mt-2.5 rounded-full no-underline" size="sm" asChild>
+          <Link href={`${NEXT_PUBLIC_WEBAPP_URL()}/signup?utm_source=cta`} target="_blank">
+            Get started
+          </Link>
+        </Button>
+      </CardContent>
+    </Card>
   );
 }
