d0b9cee500b8262d1991a522440f482b498b09d5
Rohit Saluja <saluja730@gmail.com>
2024-03-12 18:55:59 +0530

    feat: created the dialog file for delete of document

---
+++ apps/web/src/app/(dashboard)/admin/documents/[id]/delete-document-dialog.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import type { Document } from '@documenso/prisma/client';
+
+export type DeleteDocumentDialogProps = {
+  document: Document;
+};
+
+export const DeleteDocumentDialog = ({ document }: DeleteDocumentDialogProps) => {
+  return <div className=""></div>;
+};
