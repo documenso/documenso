4dc9e1295b9fbbc2c14e6142add8a7f2a7b8def6
Rohit Saluja <saluja730@gmail.com>
2024-03-12 21:15:17 +0530

    feat: added the templates for the delete of the documents from the admin

---
+++ packages/email/template-components/template-document-delete.tsx
@@ -0,0 +1,35 @@
+import { Section, Text } from '../components';
+import { TemplateDocumentImage } from './template-document-image';
+
+export interface TemplateDocumentDeleteProps {
+  inviterName: string;
+  inviterEmail: string;
+  reason: string;
+  documentName: string;
+  assetBaseUrl: string;
+}
+
+export const TemplateDocumentDelete = ({
+  reason,
+  documentName,
+  assetBaseUrl,
+}: TemplateDocumentDeleteProps) => {
+  return (
+    <>
+      <TemplateDocumentImage className="mt-6" assetBaseUrl={assetBaseUrl} />
+
+      <Section>
+        <Text className="text-primary mx-auto mb-0 max-w-[80%] text-center text-lg font-semibold">
+          Your document has been deleted
+          <br />"{documentName}"
+        </Text>
+        <Text className="text-primary mx-auto mb-0 max-w-[80%] text-center text-lg font-semibold">
+          Reason as below
+          <br />"{reason}"
+        </Text>
+      </Section>
+    </>
+  );
+};
+
+export default TemplateDocumentDelete;
