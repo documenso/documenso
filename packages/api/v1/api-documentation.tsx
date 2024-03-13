cc483016d8d6732b8a47365181dc31cee4323888
Adithya Krishna <aadithya794@gmail.com>
2024-03-13 13:24:09 +0530

    chore: updated styling
    Signed-off-by: Adithya Krishna <aadithya794@gmail.com>

--- packages/api/v1/api-documentation.tsx
+++ packages/api/v1/api-documentation.tsx
@@ -1,15 +1,30 @@
 'use client';
 
+import { useEffect } from 'react';
+
+import { useTheme } from 'next-themes';
 import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
 
 import { OpenAPIV1 } from '@documenso/api/v1/openapi';
 
-// Custom Dark Mode CSS for the Swagger UI
-// eslint-disable-next-line prettier/prettier
-import './api-documentation.css';
-
 export const OpenApiDocsPage = () => {
+  const { theme } = useTheme();
+
+  useEffect(() => {
+    const body = document.body;
+
+    if (theme === 'dark') {
+      body.classList.add('swagger-dark-theme');
+    } else {
+      body.classList.remove('swagger-dark-theme');
+    }
+
+    return () => {
+      body.classList.remove('swagger-dark-theme');
+    };
+  }, [theme]);
+
   return <SwaggerUI spec={OpenAPIV1} displayOperationId={true} />;
 };
 
