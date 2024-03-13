e5497efe7c9f74f93e10557fda22aa3cd67c611f
Adithya Krishna <aadithya794@gmail.com>
2024-03-13 12:38:56 +0530

    chore: updated dark mode styling
    Signed-off-by: Adithya Krishna <aadithya794@gmail.com>

--- packages/api/v1/api-documentation.tsx
+++ packages/api/v1/api-documentation.tsx
@@ -5,7 +5,7 @@ import 'swagger-ui-react/swagger-ui.css';
 
 import { OpenAPIV1 } from '@documenso/api/v1/openapi';
 
-// Custom CSS for the Swagger UI
+// Custom Dark Mode CSS for the Swagger UI
 // eslint-disable-next-line prettier/prettier
 import './api-documentation.css';
 
