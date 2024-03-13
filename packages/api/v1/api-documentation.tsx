27a69819f9b925119cac61518a0c5bb761593512
Adithya Krishna <aadithya794@gmail.com>
2024-03-13 09:49:31 +0530

    feat: added custom styling for swagger ui
    Signed-off-by: Adithya Krishna <aadithya794@gmail.com>

--- packages/api/v1/api-documentation.tsx
+++ packages/api/v1/api-documentation.tsx
@@ -5,6 +5,10 @@ import 'swagger-ui-react/swagger-ui.css';
 
 import { OpenAPIV1 } from '@documenso/api/v1/openapi';
 
+// Custom CSS for the Swagger UI
+// eslint-disable-next-line prettier/prettier
+import './api-documentation.css';
+
 export const OpenApiDocsPage = () => {
   return <SwaggerUI spec={OpenAPIV1} displayOperationId={true} />;
 };
