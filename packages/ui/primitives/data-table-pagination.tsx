4379b43ad92a5d7f7c2b6e0c200e44a0bfe776bc
Gautam-Hegde <gautamatjob@protonmail.com>
2024-03-15 22:09:58 +0530

    chore: tidy code

--- packages/ui/primitives/data-table-pagination.tsx
+++ packages/ui/primitives/data-table-pagination.tsx
@@ -20,9 +20,6 @@ export function DataTablePagination<TData>({
   table,
   additionalInformation = 'VisibleCount',
 }: DataTablePaginationProps<TData>) {
-  const pageCount = table.getPageCount();
-  const isEmptyTable = pageCount === 0;
-
   return (
     <div className="flex flex-wrap items-center justify-between gap-x-4 gap-y-4 px-2">
       <div className="text-muted-foreground flex-1 text-sm">
@@ -68,9 +65,7 @@ export function DataTablePagination<TData>({
       </div>
       <div className="flex flex-wrap items-center gap-x-6 gap-y-4 lg:gap-x-8">
         <div className="flex items-center text-sm font-medium md:justify-center">
-          {isEmptyTable
-            ? 'Page 1 of 1'
-            : `Page ${table.getState().pagination.pageIndex + 1} of ${pageCount}`}
+          Page {table.getState().pagination.pageIndex + 1} of {table.getPageCount() || 1}
         </div>
 
         <div className="flex items-center gap-x-2">
@@ -104,7 +99,7 @@ export function DataTablePagination<TData>({
           <Button
             variant="outline"
             className="hidden h-8 w-8 p-0 lg:flex"
-            onClick={() => table.setPageIndex(pageCount - 1)}
+            onClick={() => table.setPageIndex(table.getPageCount() - 1)}
             disabled={!table.getCanNextPage()}
           >
             <span className="sr-only">Go to last page</span>
