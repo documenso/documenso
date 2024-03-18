de9c9f4aabf8282a314131ce213c163f327c1800
Lucas Smith <me@lucasjamessmith.me>
2024-03-18 02:44:39 +0000

    chore: tidying

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,15 +1,15 @@
 const path = require('path');
 
-const buildEslintCommand = (filenames) =>
+const eslint = (filenames) =>
   `eslint --fix ${filenames.map((f) => `"${path.relative(process.cwd(), f)}"`).join(' ')}`;
 
-const buildPrettierCommand = (filenames) =>
+const prettier = (filenames) =>
   `prettier --write ${filenames.map((f) => `"${path.relative(process.cwd(), f)}"`).join(' ')}`;
 
 /** @type {import('lint-staged').Config} */
 module.exports = {
-  '**/*.{ts,tsx,cts,mts}': [buildEslintCommand, buildPrettierCommand],
-  '**/*.{js,jsx,cjs,mjs}': [buildPrettierCommand],
-  '**/*.{yml,mdx}': [buildPrettierCommand],
+  '**/*.{ts,tsx,cts,mts}': [eslint, prettier],
+  '**/*.{js,jsx,cjs,mjs}': [prettier],
+  '**/*.{yml,mdx}': [prettier],
   '**/*/package.json': 'npm run precommit',
 };
