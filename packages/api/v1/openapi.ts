2b9882cdfba492790c11a369ff29ae90a1259d61
Lucas Smith <me@lucasjamessmith.me>
2024-03-20 12:01:00 +1100

    chore: use object.assign

--- packages/api/v1/openapi.ts
+++ packages/api/v1/openapi.ts
@@ -2,34 +2,34 @@ import { generateOpenApi } from '@ts-rest/open-api';
 
 import { ApiContractV1 } from './contract';
 
-const generatedOpenApi = generateOpenApi(
-  ApiContractV1,
-  {
-    info: {
-      title: 'Documenso API',
-      version: '1.0.0',
-      description: 'The Documenso API for retrieving, creating, updating and deleting documents.',
-    },
-  },
-  {
-    setOperationId: true,
-  },
-);
-
-export const OpenAPIV1 = {
-  ...generatedOpenApi,
-  components: {
-    securitySchemes: {
-      authorization: {
-        type: 'apiKey',
-        in: 'header',
-        name: 'Authorization',
+export const OpenAPIV1 = Object.assign(
+  generateOpenApi(
+    ApiContractV1,
+    {
+      info: {
+        title: 'Documenso API',
+        version: '1.0.0',
+        description: 'The Documenso API for retrieving, creating, updating and deleting documents.',
       },
     },
-  },
-  security: [
     {
-      authorization: [],
+      setOperationId: true,
     },
-  ],
-};
+  ),
+  {
+    components: {
+      securitySchemes: {
+        authorization: {
+          type: 'apiKey',
+          in: 'header',
+          name: 'Authorization',
+        },
+      },
+    },
+    security: [
+      {
+        authorization: [],
+      },
+    ],
+  },
+);
