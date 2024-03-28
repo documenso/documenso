6de0143c36e1426e5fb4a23f916bceee55f0402e
David Nguyen <davidngu28@gmail.com>
2024-03-28 18:05:13 +0800

    fix: refactor tests

--- packages/app-tests/playwright.config.ts
+++ packages/app-tests/playwright.config.ts
@@ -20,9 +20,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  retries: process.env.CI ? 2 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
