7615c9d2fa8e064e005c904c6a65e2020ec9f5af
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2024-04-05 17:49:32 +0000

    feat: add chat to admin dashboard

--- apps/web/src/app/(dashboard)/admin/stats/page.tsx
+++ apps/web/src/app/(dashboard)/admin/stats/page.tsx
@@ -14,18 +14,35 @@ import {
 import { getDocumentStats } from '@documenso/lib/server-only/admin/get-documents-stats';
 import { getRecipientsStats } from '@documenso/lib/server-only/admin/get-recipients-stats';
 import {
+  getUserWithAtLeastOneDocumentPerMonth,
+  getUserWithAtLeastOneDocumentSignedPerMonth,
+  getUserWithSignedDocumentMonthlyGrowth,
   getUsersCount,
   getUsersWithSubscriptionsCount,
 } from '@documenso/lib/server-only/admin/get-users-stats';
 
 import { CardMetric } from '~/components/(dashboard)/metric-card/metric-card';
 
+import { UserWithDocumentChart } from './user-with-document';
+import { UserWithDocumentCummulativeChart } from './user-with-document-cummulative';
+
 export default async function AdminStatsPage() {
-  const [usersCount, usersWithSubscriptionsCount, docStats, recipientStats] = await Promise.all([
+  const [
+    usersCount,
+    usersWithSubscriptionsCount,
+    docStats,
+    recipientStats,
+    userWithAtLeastOneDocumentPerMonth,
+    userWithAtLeastOneDocumentSignedPerMonth,
+    MONTHLY_USERS_SIGNED,
+  ] = await Promise.all([
     getUsersCount(),
     getUsersWithSubscriptionsCount(),
     getDocumentStats(),
     getRecipientsStats(),
+    getUserWithAtLeastOneDocumentPerMonth(),
+    getUserWithAtLeastOneDocumentSignedPerMonth(),
+    getUserWithSignedDocumentMonthlyGrowth(),
   ]);
 
   return (
@@ -43,12 +60,30 @@ export default async function AdminStatsPage() {
         <CardMetric icon={UserPlus2} title="App Version" value={`v${process.env.APP_VERSION}`} />
       </div>
 
-      <div className="mt-16 grid grid-cols-1 gap-8 md:grid-cols-1 lg:grid-cols-2">
+      {/* TODO: remove grid and see something */}
+      <div className="mt-16 gap-8">
+        <div>
+          <h3 className="text-3xl font-semibold">User metrics</h3>
+
+          <div className="mb-8 mt-4 grid flex-1 grid-cols-2 gap-4">
+            <CardMetric
+              icon={File}
+              title="Users with document in the last month"
+              value={userWithAtLeastOneDocumentPerMonth}
+            />
+            <CardMetric
+              icon={File}
+              title="Users with signed document in the last month"
+              value={userWithAtLeastOneDocumentSignedPerMonth}
+            />
+          </div>
+        </div>
+
         <div>
           <h3 className="text-3xl font-semibold">Document metrics</h3>
 
-          <div className="mt-8 grid flex-1 grid-cols-2 gap-4">
-            <CardMetric icon={File} title="Total Documents" value={docStats.ALL} />
+          <div className="mb-8 mt-4 grid flex-1 grid-cols-2 gap-4">
+            {/* <CardMetric icon={File} title="Total Documents" value={docStats.ALL} /> */}
             <CardMetric icon={FileEdit} title="Drafted Documents" value={docStats.DRAFT} />
             <CardMetric icon={FileClock} title="Pending Documents" value={docStats.PENDING} />
             <CardMetric icon={FileCheck} title="Completed Documents" value={docStats.COMPLETED} />
@@ -58,7 +93,7 @@ export default async function AdminStatsPage() {
         <div>
           <h3 className="text-3xl font-semibold">Recipients metrics</h3>
 
-          <div className="mt-8 grid flex-1 grid-cols-2 gap-4">
+          <div className="mb-8 mt-4 grid flex-1 grid-cols-2 gap-4">
             <CardMetric
               icon={UserSquare2}
               title="Total Recipients"
@@ -69,6 +104,20 @@ export default async function AdminStatsPage() {
             <CardMetric icon={PenTool} title="Signatures Collected" value={recipientStats.SIGNED} />
           </div>
         </div>
+      </div>
+
+      <div className="mt-16">
+        <h3 className="text-3xl font-semibold">User Charts</h3>
+
+        <UserWithDocumentChart
+          data={MONTHLY_USERS_SIGNED}
+          className="col-span-12 mb-8 mt-4 lg:col-span-6"
+        />
+
+        <UserWithDocumentCummulativeChart
+          data={MONTHLY_USERS_SIGNED}
+          className="col-span-12 mb-8 mt-4 lg:col-span-6"
+        />
       </div>
     </div>
   );
--- packages/lib/server-only/admin/get-users-stats.ts
+++ packages/lib/server-only/admin/get-users-stats.ts
@@ -1,5 +1,7 @@
+import { DateTime } from 'luxon';
+
 import { prisma } from '@documenso/prisma';
-import { SubscriptionStatus } from '@documenso/prisma/client';
+import { DocumentStatus, SubscriptionStatus } from '@documenso/prisma/client';
 
 export const getUsersCount = async () => {
   return await prisma.user.count();
@@ -15,4 +17,74 @@ export const getUsersWithSubscriptionsCount = async () => {
       },
     },
   });
+};
+
+export const getUserWithAtLeastOneDocumentPerMonth = async () => {
+  return await prisma.user.count({
+    where: {
+      Document: {
+        some: {
+          createdAt: {
+            gte: new Date(new Date().setMonth(new Date().getMonth() - 1)),
+          },
+        },
+      },
+    },
+  });
+};
+
+export const getUserWithAtLeastOneDocumentSignedPerMonth = async () => {
+  return await prisma.user.count({
+    where: {
+      Document: {
+        some: {
+          status: {
+            equals: DocumentStatus.COMPLETED,
+          },
+          createdAt: {
+            gte: new Date(new Date().setMonth(new Date().getMonth() - 1)),
+          },
+        },
+      },
+    },
+  });
+};
+
+export type GetUserWithDocumentMonthlyGrowth = Array<{
+  month: string;
+  count: number;
+  cume_count: number;
+  signed_count: number;
+  cume_signed_count: number;
+}>;
+
+type GetUserWithDocumentMonthlyGrowthQueryResult = Array<{
+  month: Date;
+  count: bigint;
+  cume_count: bigint;
+  signed_count: bigint;
+  cume_signed_count: bigint;
+}>;
+
+export const getUserWithSignedDocumentMonthlyGrowth = async () => {
+  const result = await prisma.$queryRaw<GetUserWithDocumentMonthlyGrowthQueryResult>`
+      SELECT
+        DATE_TRUNC('month', "Document"."createdAt") AS "month",
+        COUNT(DISTINCT "Document"."userId") as "count",
+        SUM(COUNT(DISTINCT "Document"."userId")) OVER (ORDER BY DATE_TRUNC('month', "Document"."createdAt")) as "cume_count",
+        COUNT(DISTINCT CASE WHEN "Document"."status" = 'COMPLETED' THEN "Document"."userId" END) as "signed_count",
+        SUM(COUNT(DISTINCT CASE WHEN "Document"."status" = 'COMPLETED' THEN "Document"."userId" END)) OVER (ORDER BY DATE_TRUNC('month', "Document"."createdAt")) as "cume_signed_count"
+      FROM "Document"
+      GROUP BY "month"
+      ORDER BY "month" DESC
+      LIMIT 12
+`;
+
+  return result.map((row) => ({
+    month: DateTime.fromJSDate(row.month).toFormat('yyyy-MM'),
+    count: Number(row.count),
+    cume_count: Number(row.cume_count),
+    signed_count: Number(row.signed_count),
+    cume_signed_count: Number(row.cume_signed_count),
+  }));
 };
