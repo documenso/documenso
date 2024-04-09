1400c335a5291a272ea82b1d7c113632c029d6da
David Nguyen <davidngu28@gmail.com>
2024-04-09 11:31:53 +0700

    fix: improve document loading ui consistency (#1082)
    ## Description
    
    General UI updates
    
    ## Changes Made
    
    - Add consistent spacing between document edit/view/log pages
    - Add document status to document audit log page
    - Update document loading page to reserve space for the document status
    below the title
    - Update the document audit log page to show full dates in the correct
    locale

--- apps/web/src/app/(dashboard)/documents/[id]/loading.tsx
+++ apps/web/src/app/(dashboard)/documents/[id]/loading.tsx
@@ -2,6 +2,8 @@ import Link from 'next/link';
 
 import { ChevronLeft, Loader } from 'lucide-react';
 
+import { Skeleton } from '@documenso/ui/primitives/skeleton';
+
 export default function Loading() {
   return (
     <div className="mx-auto -mt-4 flex w-full max-w-screen-xl flex-col px-4 md:px-8">
@@ -13,7 +15,12 @@ export default function Loading() {
       <h1 className="mt-4 grow-0 truncate text-2xl font-semibold md:text-3xl">
         Loading Document...
       </h1>
-      <div className="mt-8 grid h-[80vh] max-h-[60rem] w-full grid-cols-12 gap-x-8">
+
+      <div className="flex h-10 items-center">
+        <Skeleton className="my-6 h-4 w-24 rounded-2xl" />
+      </div>
+
+      <div className="mt-4 grid h-[80vh] max-h-[60rem] w-full grid-cols-12 gap-x-8">
         <div className="dark:bg-background border-border col-span-12 rounded-xl border-2 bg-white/50 p-2 before:rounded-xl lg:col-span-6 xl:col-span-7">
           <div className="flex h-[80vh] max-h-[60rem] flex-col items-center justify-center">
             <Loader className="text-documenso h-12 w-12 animate-spin" />
