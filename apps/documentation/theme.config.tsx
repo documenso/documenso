442e88cff8f6d8ae41010ecb1fdacd168e62bbfb
Catalin Pit <25515812+catalinpit@users.noreply.github.com>
2024-04-22 16:58:00 +0300

    chore: added webhooks content and updated home page

--- apps/documentation/theme.config.tsx
+++ apps/documentation/theme.config.tsx
@@ -2,7 +2,7 @@ import { useConfig } from 'nextra-theme-docs';
 import type { DocsThemeConfig } from 'nextra-theme-docs';
 
 const themeConfig: DocsThemeConfig = {
-  logo: <span>Documenso</span>,
+  logo: <span>Documenso Docs</span>,
   head: function useHead() {
     const config = useConfig<{ title?: string; description?: string }>();
 
