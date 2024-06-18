5e5b9e36f703d4e2d2a4ffb1d206ae0c40aea874
Julius <eller.julius@gmail.com>
2024-06-18 10:11:20 +0100

    fix: changed certificate to reference var
    To access the value of the certificate I changed the reference value

--- docker/start.sh
+++ docker/start.sh
@@ -4,6 +4,6 @@ set -x
 
 npx prisma migrate deploy --schema ./packages/prisma/schema.prisma
 
-echo ${cert_file} | base64 --decode > /opt/documenso/cert.p12
+echo $CERTIFICATE | base64 --decode > /opt/documenso/cert.p12
 
 node apps/web/server.js
