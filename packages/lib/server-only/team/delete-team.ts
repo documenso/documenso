589b760998b69a5f5840e9e281c3b52ff7aac1bf
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2024-07-13 06:13:44 +0000

    feat: send delete team email

--- packages/lib/server-only/team/delete-team.ts
+++ packages/lib/server-only/team/delete-team.ts
@@ -1,7 +1,15 @@
-import { prisma } from '@documenso/prisma';
+import { createElement } from 'react';
 
-import { AppError } from '../../errors/app-error';
-import { stripe } from '../stripe';
+import { mailer } from '@documenso/email/mailer';
+import { render } from '@documenso/email/render';
+import type { TeamDeleteEmailProps } from '@documenso/email/templates/team-delete';
+import { TeamDeleteEmailTemplate } from '@documenso/email/templates/team-delete';
+import { WEBAPP_BASE_URL } from '@documenso/lib/constants/app';
+import { FROM_ADDRESS, FROM_NAME } from '@documenso/lib/constants/email';
+import { AppError } from '@documenso/lib/errors/app-error';
+import { stripe } from '@documenso/lib/server-only/stripe';
+import { prisma } from '@documenso/prisma';
+import { TeamMemberRole } from '@documenso/prisma/client';
 
 export type DeleteTeamOptions = {
   userId: number;
@@ -18,6 +26,16 @@ export const deleteTeam = async ({ userId, teamId }: DeleteTeamOptions) => {
         },
         include: {
           subscription: true,
+          members: {
+            include: {
+              user: {
+                select: {
+                  id: true,
+                  email: true,
+                },
+              },
+            },
+          },
         },
       });
 
@@ -33,6 +51,48 @@ export const deleteTeam = async ({ userId, teamId }: DeleteTeamOptions) => {
           });
       }
 
+      const teamOwners = team.members.filter((member) => member.role === TeamMemberRole.ADMIN);
+      const teamMembers = team.members.filter(
+        (member) => member.role === TeamMemberRole.MANAGER || member.role === TeamMemberRole.MEMBER,
+      );
+
+      const ownerSendEmailResults = await Promise.allSettled(
+        teamOwners.map(async (owner) =>
+          sendTeamDeleteEmail({
+            email: owner.user.email,
+            teamName: team.name,
+            teamUrl: team.url,
+            isOwner: true,
+          }),
+        ),
+      );
+
+      const memberSendEmailResults = await Promise.allSettled(
+        teamMembers.map(async (member) =>
+          sendTeamDeleteEmail({
+            email: member.user.email,
+            teamName: team.name,
+            teamUrl: team.url,
+            isOwner: false,
+          }),
+        ),
+      );
+
+      const sendEmailResult = [...ownerSendEmailResults, ...memberSendEmailResults];
+      const sendEmailResultErrorList = sendEmailResult.filter(
+        (result): result is PromiseRejectedResult => result.status === 'rejected',
+      );
+
+      if (sendEmailResultErrorList.length > 0) {
+        console.error(JSON.stringify(sendEmailResultErrorList));
+
+        throw new AppError(
+          'EmailDeliveryFailed',
+          'Failed to send invite emails to one or more users.',
+          `Failed to send invites to ${sendEmailResultErrorList.length}/${team.members.length} users.`,
+        );
+      }
+
       await tx.team.delete({
         where: {
           id: teamId,
@@ -42,4 +102,31 @@ export const deleteTeam = async ({ userId, teamId }: DeleteTeamOptions) => {
     },
     { timeout: 30_000 },
   );
+};
+
+type SendTeamDeleteEmailOptions = Omit<TeamDeleteEmailProps, 'baseUrl' | 'assetBaseUrl'> & {
+  email: string;
+  teamName: string;
+};
+
+export const sendTeamDeleteEmail = async ({
+  email,
+  ...emailTemplateOptions
+}: SendTeamDeleteEmailOptions) => {
+  const template = createElement(TeamDeleteEmailTemplate, {
+    assetBaseUrl: WEBAPP_BASE_URL,
+    baseUrl: WEBAPP_BASE_URL,
+    ...emailTemplateOptions,
+  });
+
+  await mailer.sendMail({
+    to: email,
+    from: {
+      name: FROM_NAME,
+      address: FROM_ADDRESS,
+    },
+    subject: `Team "${emailTemplateOptions.teamName}" has been deleted on Documenso`,
+    html: render(template),
+    text: render(template, { plainText: true }),
+  });
 };
