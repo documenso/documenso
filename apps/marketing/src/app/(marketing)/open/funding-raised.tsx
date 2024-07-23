a24fda6cd1692a27fe26daed6d27ed2bfb0d9a8f
David Nguyen <davidngu28@gmail.com>
2024-07-23 11:58:51 +1000

    chore: extract marketing translations

--- apps/marketing/src/app/(marketing)/open/funding-raised.tsx
+++ apps/marketing/src/app/(marketing)/open/funding-raised.tsx
@@ -2,6 +2,8 @@
 
 import type { HTMLAttributes } from 'react';
 
+import { Trans, msg } from '@lingui/macro';
+import { useLingui } from '@lingui/react';
 import { Bar, BarChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 import { formatMonth } from '@documenso/lib/client-only/format-month';
@@ -11,6 +13,8 @@ export type FundingRaisedProps = HTMLAttributes<HTMLDivElement> & {
 };
 
 export const FundingRaised = ({ className, data, ...props }: FundingRaisedProps) => {
+  const { _ } = useLingui();
+
   const formattedData = data.map((item) => ({
     amount: Number(item.amount),
     // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
@@ -21,7 +25,9 @@ export const FundingRaised = ({ className, data, ...props }: FundingRaisedProps)
     <div className={className} {...props}>
       <div className="border-border flex flex-col justify-center rounded-2xl border p-6 pl-2 shadow-sm hover:shadow">
         <div className="mb-6 flex px-4">
-          <h3 className="text-lg font-semibold">Total Funding Raised</h3>
+          <h3 className="text-lg font-semibold">
+            <Trans>Total Funding Raised</Trans>
+          </h3>
         </div>
 
         <ResponsiveContainer width="100%" height={400}>
@@ -49,14 +55,14 @@ export const FundingRaised = ({ className, data, ...props }: FundingRaisedProps)
                   currency: 'USD',
                   maximumFractionDigits: 0,
                 }),
-                'Amount Raised',
+                _(msg`Amount Raised`),
               ]}
               cursor={{ fill: 'hsl(var(--primary) / 10%)' }}
             />
             <Bar
               dataKey="amount"
               fill="hsl(var(--primary))"
-              label="Amount Raised"
+              label={_(msg`Amount Raised`)}
               maxBarSize={60}
               radius={[4, 4, 0, 0]}
             />
