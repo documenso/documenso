0a06198932a2b5a1323fa94983e8a7c7d12d0daa
Mythie <me@lucasjamessmith.me>
2024-07-24 12:06:21 +1000

    fix: simplify implementation

--- apps/web/src/components/forms/token.tsx
+++ apps/web/src/components/forms/token.tsx
@@ -1,14 +1,16 @@
 'use client';
 
-import { forwardRef, useImperativeHandle, useState } from 'react';
+import { useState, useTransition } from 'react';
 
 import { useRouter } from 'next/navigation';
 
 import { zodResolver } from '@hookform/resolvers/zod';
+import { AnimatePresence, motion } from 'framer-motion';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 
 import { useCopyToClipboard } from '@documenso/lib/client-only/hooks/use-copy-to-clipboard';
+import type { ApiToken } from '@documenso/prisma/client';
 import { TRPCClientError } from '@documenso/trpc/client';
 import { trpc } from '@documenso/trpc/react';
 import type { TCreateTokenMutationSchema } from '@documenso/trpc/server/api-token-router/schema';
@@ -44,244 +46,237 @@ const ZCreateTokenFormSchema = ZCreateTokenMutationSchema.extend({
 
 type TCreateTokenFormSchema = z.infer<typeof ZCreateTokenFormSchema>;
 
+type NewlyCreatedToken = {
+  id: number;
+  token: string;
+};
+
 export type ApiTokenFormProps = {
   className?: string;
   teamId?: number;
+  tokens?: ApiToken[];
 };
 
-export type HandleApiTokenFormReset = {
-  reset: () => void;
-};
+export const ApiTokenForm = ({ className, teamId, tokens }: ApiTokenFormProps) => {
+  const router = useRouter();
+  const [isTransitionPending, startTransition] = useTransition();
 
-const ApiTokenForm = forwardRef<HandleApiTokenFormReset, ApiTokenFormProps>(
-  ({ teamId, className }: ApiTokenFormProps, ref) => {
-    const router = useRouter();
+  const [, copy] = useCopyToClipboard();
+  const { toast } = useToast();
 
-    const [, copy] = useCopyToClipboard();
-    const { toast } = useToast();
+  const [newlyCreatedToken, setNewlyCreatedToken] = useState<NewlyCreatedToken | null>();
+  const [noExpirationDate, setNoExpirationDate] = useState(false);
 
-    const NEWLY_CREATED_TOKEN_DEFAULT = '';
-    const NO_EXPIRATION_DATE_DEFAULT = false;
-    const [newlyCreatedToken, setNewlyCreatedToken] = useState(NEWLY_CREATED_TOKEN_DEFAULT);
-    const [noExpirationDate, setNoExpirationDate] = useState(NO_EXPIRATION_DATE_DEFAULT);
+  // This lets us hide the token from being copied if it has been deleted without
+  // resorting to a useEffect or any other fanciness. This comes at the cost of it
+  // taking slighly longer to appear since it will need to wait for the router.refresh()
+  // to finish updating.
+  const hasNewlyCreatedToken =
+    tokens?.find((token) => token.id === newlyCreatedToken?.id) !== undefined;
 
-    const { mutateAsync: createTokenMutation } = trpc.apiToken.createToken.useMutation({
-      onSuccess(data) {
-        setNewlyCreatedToken(data.token);
-      },
-    });
+  const { mutateAsync: createTokenMutation } = trpc.apiToken.createToken.useMutation({
+    onSuccess(data) {
+      setNewlyCreatedToken(data);
+    },
+  });
 
-    const form = useForm<TCreateTokenFormSchema>({
-      resolver: zodResolver(ZCreateTokenFormSchema),
-      defaultValues: {
-        tokenName: '',
-        expirationDate: '',
-        enabled: false,
-      },
-    });
+  const form = useForm<TCreateTokenFormSchema>({
+    resolver: zodResolver(ZCreateTokenFormSchema),
+    defaultValues: {
+      tokenName: '',
+      expirationDate: '',
+      enabled: false,
+    },
+  });
 
-    useImperativeHandle(
-      ref,
-      () => {
-        return {
-          reset() {
-            form.reset();
-            setNewlyCreatedToken(NEWLY_CREATED_TOKEN_DEFAULT);
-            setNoExpirationDate(NO_EXPIRATION_DATE_DEFAULT);
-          },
-        };
-      },
-      [NEWLY_CREATED_TOKEN_DEFAULT, NO_EXPIRATION_DATE_DEFAULT, form],
-    );
+  const copyToken = async (token: string) => {
+    try {
+      const copied = await copy(token);
 
-    const copyToken = async (token: string) => {
-      try {
-        const copied = await copy(token);
+      if (!copied) {
+        throw new Error('Unable to copy the token');
+      }
 
-        if (!copied) {
-          throw new Error('Unable to copy the token');
-        }
+      toast({
+        title: 'Token copied to clipboard',
+        description: 'The token was copied to your clipboard.',
+      });
+    } catch (error) {
+      toast({
+        title: 'Unable to copy token',
+        description: 'We were unable to copy the token to your clipboard. Please try again.',
+        variant: 'destructive',
+      });
+    }
+  };
 
+  const onSubmit = async ({ tokenName, expirationDate }: TCreateTokenMutationSchema) => {
+    try {
+      await createTokenMutation({
+        teamId,
+        tokenName,
+        expirationDate: noExpirationDate ? null : expirationDate,
+      });
+
+      toast({
+        title: 'Token created',
+        description: 'A new token was created successfully.',
+        duration: 5000,
+      });
+
+      form.reset();
+
+      startTransition(() => router.refresh());
+    } catch (error) {
+      if (error instanceof TRPCClientError && error.data?.code === 'BAD_REQUEST') {
         toast({
-          title: 'Token copied to clipboard',
-          description: 'The token was copied to your clipboard.',
-        });
-      } catch (error) {
-        toast({
-          title: 'Unable to copy token',
-          description: 'We were unable to copy the token to your clipboard. Please try again.',
+          title: 'An error occurred',
+          description: error.message,
           variant: 'destructive',
         });
-      }
-    };
-
-    const onSubmit = async ({ tokenName, expirationDate }: TCreateTokenMutationSchema) => {
-      try {
-        await createTokenMutation({
-          teamId,
-          tokenName,
-          expirationDate: noExpirationDate ? null : expirationDate,
-        });
-
+      } else {
         toast({
-          title: 'Token created',
-          description: 'A new token was created successfully.',
+          title: 'An unknown error occurred',
+          variant: 'destructive',
           duration: 5000,
+          description:
+            'We encountered an unknown error while attempting create the new token. Please try again later.',
         });
-
-        form.reset();
-
-        router.refresh();
-      } catch (error) {
-        if (error instanceof TRPCClientError && error.data?.code === 'BAD_REQUEST') {
-          toast({
-            title: 'An error occurred',
-            description: error.message,
-            variant: 'destructive',
-          });
-        } else {
-          toast({
-            title: 'An unknown error occurred',
-            variant: 'destructive',
-            duration: 5000,
-            description:
-              'We encountered an unknown error while attempting create the new token. Please try again later.',
-          });
-        }
       }
-    };
+    }
+  };
 
-    return (
-      <div className={cn(className)}>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)}>
-            <fieldset className="mt-6 flex w-full flex-col gap-4">
+  return (
+    <div className={cn(className)}>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <fieldset className="mt-6 flex w-full flex-col gap-4">
+            <FormField
+              control={form.control}
+              name="tokenName"
+              render={({ field }) => (
+                <FormItem className="flex-1">
+                  <FormLabel className="text-muted-foreground">Token name</FormLabel>
+
+                  <div className="flex items-center gap-x-4">
+                    <FormControl className="flex-1">
+                      <Input type="text" {...field} />
+                    </FormControl>
+                  </div>
+
+                  <FormDescription className="text-xs italic">
+                    Please enter a meaningful name for your token. This will help you identify it
+                    later.
+                  </FormDescription>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <div className="flex flex-col gap-4 md:flex-row">
               <FormField
                 control={form.control}
-                name="tokenName"
+                name="expirationDate"
                 render={({ field }) => (
                   <FormItem className="flex-1">
-                    <FormLabel className="text-muted-foreground">Token name</FormLabel>
+                    <FormLabel className="text-muted-foreground">Token expiration date</FormLabel>
 
                     <div className="flex items-center gap-x-4">
                       <FormControl className="flex-1">
-                        <Input type="text" {...field} />
+                        <Select onValueChange={field.onChange} disabled={noExpirationDate}>
+                          <SelectTrigger className="w-full">
+                            <SelectValue placeholder="Choose..." />
+                          </SelectTrigger>
+                          <SelectContent>
+                            {Object.entries(EXPIRATION_DATES).map(([key, date]) => (
+                              <SelectItem key={key} value={key}>
+                                {date}
+                              </SelectItem>
+                            ))}
+                          </SelectContent>
+                        </Select>
                       </FormControl>
                     </div>
-
-                    <FormDescription className="text-xs italic">
-                      Please enter a meaningful name for your token. This will help you identify it
-                      later.
-                    </FormDescription>
 
                     <FormMessage />
                   </FormItem>
                 )}
               />
 
-              <div className="flex flex-col gap-4 md:flex-row">
-                <FormField
-                  control={form.control}
-                  name="expirationDate"
-                  render={({ field }) => (
-                    <FormItem className="flex-1">
-                      <FormLabel className="text-muted-foreground">Token expiration date</FormLabel>
-
-                      <div className="flex items-center gap-x-4">
-                        <FormControl className="flex-1">
-                          <Select
-                            onValueChange={field.onChange}
-                            disabled={noExpirationDate}
-                            value={field.value ?? ''}
-                          >
-                            <SelectTrigger className="w-full">
-                              <SelectValue placeholder="Choose..." />
-                            </SelectTrigger>
-                            <SelectContent>
-                              {Object.entries(EXPIRATION_DATES).map(([key, date]) => (
-                                <SelectItem key={key} value={key}>
-                                  {date}
-                                </SelectItem>
-                              ))}
-                            </SelectContent>
-                          </Select>
-                        </FormControl>
+              <FormField
+                control={form.control}
+                name="enabled"
+                render={({ field }) => (
+                  <FormItem className="">
+                    <FormLabel className="text-muted-foreground mt-2">Never expire</FormLabel>
+                    <FormControl>
+                      <div className="block md:py-1.5">
+                        <Switch
+                          className="bg-background"
+                          checked={field.value}
+                          onCheckedChange={(val) => {
+                            setNoExpirationDate((prev) => !prev);
+                            field.onChange(val);
+                          }}
+                        />
                       </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
 
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
+            <Button
+              type="submit"
+              className="hidden md:inline-flex"
+              disabled={!form.formState.isDirty}
+              loading={form.formState.isSubmitting || isTransitionPending}
+            >
+              Create token
+            </Button>
 
-                <FormField
-                  control={form.control}
-                  name="enabled"
-                  render={({ field }) => (
-                    <FormItem className="">
-                      <FormLabel className="text-muted-foreground mt-2">Never expire</FormLabel>
-                      <FormControl>
-                        <div className="block md:py-1.5">
-                          <Switch
-                            className="bg-background"
-                            checked={field.value}
-                            onCheckedChange={(val) => {
-                              setNoExpirationDate((prev) => !prev);
-                              field.onChange(val);
-                            }}
-                          />
-                        </div>
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-
+            <div className="md:hidden">
               <Button
                 type="submit"
-                className="hidden md:inline-flex"
                 disabled={!form.formState.isDirty}
-                loading={form.formState.isSubmitting}
+                loading={form.formState.isSubmitting || isTransitionPending}
               >
                 Create token
               </Button>
+            </div>
+          </fieldset>
+        </form>
+      </Form>
 
-              <div className="md:hidden">
-                <Button
-                  type="submit"
-                  disabled={!form.formState.isDirty}
-                  loading={form.formState.isSubmitting}
-                >
-                  Create token
+      <AnimatePresence initial={!hasNewlyCreatedToken}>
+        {newlyCreatedToken && hasNewlyCreatedToken && (
+          <motion.div
+            className="mt-8"
+            initial={{ opacity: 0, y: -40 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 40 }}
+          >
+            <Card gradient>
+              <CardContent className="p-4">
+                <p className="text-muted-foreground mt-2 text-sm">
+                  Your token was created successfully! Make sure to copy it because you won't be
+                  able to see it again!
+                </p>
+
+                <p className="bg-muted-foreground/10 my-4 rounded-md px-2.5 py-1 font-mono text-sm">
+                  {newlyCreatedToken.token}
+                </p>
+
+                <Button variant="outline" onClick={() => void copyToken(newlyCreatedToken.token)}>
+                  Copy token
                 </Button>
-              </div>
-            </fieldset>
-          </form>
-        </Form>
-
-        {newlyCreatedToken && (
-          <Card className="mt-8" gradient>
-            <CardContent className="p-4">
-              <p className="text-muted-foreground mt-2 text-sm">
-                Your token was created successfully! Make sure to copy it because you won't be able
-                to see it again!
-              </p>
-
-              <p className="bg-muted-foreground/10 my-4 rounded-md px-2.5 py-1 font-mono text-sm">
-                {newlyCreatedToken}
-              </p>
-
-              <Button variant="outline" onClick={() => void copyToken(newlyCreatedToken)}>
-                Copy token
-              </Button>
-            </CardContent>
-          </Card>
+              </CardContent>
+            </Card>
+          </motion.div>
         )}
-      </div>
-    );
-  },
-);
-
-ApiTokenForm.displayName = 'API Token Form';
-
-export { ApiTokenForm };
+      </AnimatePresence>
+    </div>
+  );
+};
--- packages/trpc/client/guards.ts
+++ packages/trpc/client/guards.ts
@@ -1,6 +1,6 @@
 import { TRPCClientError } from '@trpc/client';
 
-import type { AppRouter } from '../server/router';
+import { AppRouter } from '../server/router';
 
 export const isTRPCBadRequestError = (err: unknown): err is TRPCClientError<AppRouter> => {
   return err instanceof TRPCClientError && err.shape?.code === 'BAD_REQUEST';
