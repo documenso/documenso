d2aff9a2900b41d161e16e74c15c160249daccd5
Catalin Pit <25515812+catalinpit@users.noreply.github.com>
2024-07-29 12:09:16 +0100

    fix: add timezones

--- packages/api/v1/schema.ts
+++ packages/api/v1/schema.ts
@@ -2,6 +2,7 @@ import { extendZodWithOpenApi } from '@anatine/zod-openapi';
 import { z } from 'zod';
 
 import { DATE_FORMATS } from '@documenso/lib/constants/date-formats';
+import { TIME_ZONES } from '@documenso/lib/constants/time-zones';
 import { ZUrlSchema } from '@documenso/lib/schemas/common';
 import {
   DocumentDataType,
@@ -101,7 +102,11 @@ export const ZCreateDocumentMutationSchema = z.object({
     .object({
       subject: z.string(),
       message: z.string(),
-      timezone: z.string(),
+      timezone: z.string().openapi({
+        description:
+          'The timezone of the date. Must be one of the options listed in the list below.',
+        enum: TIME_ZONES,
+      }),
       dateFormat: z.string().openapi({
         description: 'The format of the date. Must be one of the options listed in the list below.',
         enum: DATE_FORMATS.map((format) => format.value),
