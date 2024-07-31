a9025b5d97af7ab1115b65b2c7d4474ae8ba5e0d
aeris <51246+aeris@users.noreply.github.com>
2024-07-31 07:26:05 +0200

    fix: use native URL parser instead of wrong regex (#1206)
    Updates the current regex based approach for validating redirect urls to 
    instead use the native URL constructor which is available in browsers and 
    Node.js and handles several valid cases that were previously not working.

--- packages/lib/schemas/common.ts
+++ packages/lib/schemas/common.ts
@@ -1,12 +1,12 @@
 import { z } from 'zod';
 
-import { URL_REGEX } from '../constants/url-regex';
+import { isValidRedirectUrl } from '../utils/is-valid-redirect-url';
 
 /**
  * Note this allows empty strings.
  */
 export const ZUrlSchema = z
   .string()
-  .refine((value) => value === undefined || value === '' || URL_REGEX.test(value), {
-    message: 'Please enter a valid URL',
+  .refine((value) => value === undefined || value === '' || isValidRedirectUrl(value), {
+    message: 'Please enter a valid URL, make sure you include http:// or https:// part of the url.',
   });
