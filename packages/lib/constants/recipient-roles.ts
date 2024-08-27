cb5027445047e6ac7f12c93c47d2f73f4829159e
David Nguyen <davidngu28@gmail.com>
2024-08-27 23:22:27 +0900

    fix: typo

--- packages/lib/constants/recipient-roles.ts
+++ packages/lib/constants/recipient-roles.ts
@@ -71,7 +71,7 @@ export const RECIPIENT_ROLE_TO_EMAIL_TYPE = {
 export const RECIPIENT_ROLE_SIGNING_REASONS = {
   [RecipientRole.SIGNER]: msg`I am a signer of this document`,
   [RecipientRole.APPROVER]: msg`I am an approver of this document`,
-  [RecipientRole.CC]: msg`I am required to recieve a copy of this document`,
+  [RecipientRole.CC]: msg`I am required to receive a copy of this document`,
   [RecipientRole.VIEWER]: msg`I am a viewer of this document`,
 } satisfies Record<keyof typeof RecipientRole, MessageDescriptor>;
 
@@ -81,6 +81,6 @@ export const RECIPIENT_ROLE_SIGNING_REASONS = {
 export const RECIPIENT_ROLE_SIGNING_REASONS_ENG = {
   [RecipientRole.SIGNER]: `I am a signer of this document`,
   [RecipientRole.APPROVER]: `I am an approver of this document`,
-  [RecipientRole.CC]: `I am required to recieve a copy of this document`,
+  [RecipientRole.CC]: `I am required to receive a copy of this document`,
   [RecipientRole.VIEWER]: `I am a viewer of this document`,
 } satisfies Record<keyof typeof RecipientRole, string>;
