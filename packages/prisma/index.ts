0c611ccacc5e4e2faf67f245e84abd5d4ef78c07
Jon Harrell <4829245+jharrell@users.noreply.github.com>
2024-08-29 11:18:17 -0500

    fix(prisma): re-add kysely if needed in the future

--- packages/prisma/index.ts
+++ packages/prisma/index.ts
@@ -5,7 +5,10 @@ import {
   userMonthlyGrowth,
   userWithSignedDocumentMonthlyGrowth,
 } from '@prisma/client/sql';
+import { Kysely, PostgresAdapter, PostgresIntrospector, PostgresQueryCompiler } from 'kysely';
+import kyselyExtension from 'prisma-extension-kysely';
 
+import type { DB } from './generated/types';
 import { getDatabaseUrl } from './helper';
 import { remember } from './utils/remember';
 
@@ -23,3 +26,21 @@ export const SQL = {
   userMonthlyGrowth,
   userWithSignedDocumentMonthlyGrowth,
 };
+
+export const kyselyPrisma = remember('kyselyPrisma', () =>
+  prisma.$extends(
+    kyselyExtension({
+      kysely: (driver) =>
+        new Kysely<DB>({
+          dialect: {
+            createAdapter: () => new PostgresAdapter(),
+            createDriver: () => driver,
+            createIntrospector: (db) => new PostgresIntrospector(db),
+            createQueryCompiler: () => new PostgresQueryCompiler(),
+          },
+        }),
+    }),
+  ),
+);
+
+export { sql } from 'kysely';
