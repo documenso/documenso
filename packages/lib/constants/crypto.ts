e1fb44e259a0e6c536827cb6f523bcf9053cb182
Jack <50677723+jtmaveryk@users.noreply.github.com>
2024-09-28 07:03:00 -0400

    Fix: console logging incorrect variable names
    If you're missing either of the variables, it'll say you're missing the variable variant including `DOCUMENSO` instead of `NEXT_PRIVATE`, which can be misleading while debugging.

--- packages/lib/constants/crypto.ts
+++ packages/lib/constants/crypto.ts
@@ -1,25 +1,19 @@
-export const DOCUMENSO_ENCRYPTION_KEY = process.env.NEXT_PRIVATE_ENCRYPTION_KEY;
+export const ENCRYPTION_KEY = process.env.NEXT_PRIVATE_ENCRYPTION_KEY;
 
-export const DOCUMENSO_ENCRYPTION_SECONDARY_KEY = process.env.NEXT_PRIVATE_ENCRYPTION_SECONDARY_KEY;
+export const ENCRYPTION_SECONDARY_KEY = process.env.NEXT_PRIVATE_ENCRYPTION_SECONDARY_KEY;
 
 if (typeof window === 'undefined') {
-  if (!DOCUMENSO_ENCRYPTION_KEY || !DOCUMENSO_ENCRYPTION_SECONDARY_KEY) {
-    throw new Error('Missing DOCUMENSO_ENCRYPTION_KEY or DOCUMENSO_ENCRYPTION_SECONDARY_KEY keys');
+  if (!ENCRYPTION_KEY || !ENCRYPTION_SECONDARY_KEY) {
+    throw new Error('Missing NEXT_PRIVATE_ENCRYPTION_KEY or NEXT_PRIVATE_ENCRYPTION_SECONDARY_KEY key variables');
   }
 
-  if (DOCUMENSO_ENCRYPTION_KEY === DOCUMENSO_ENCRYPTION_SECONDARY_KEY) {
+  if (ENCRYPTION_KEY === ENCRYPTION_SECONDARY_KEY) {
     throw new Error(
-      'DOCUMENSO_ENCRYPTION_KEY and DOCUMENSO_ENCRYPTION_SECONDARY_KEY cannot be equal',
+      'NEXT_PRIVATE_ENCRYPTION_KEY and NEXT_PRIVATE_ENCRYPTION_SECONDARY_KEY cannot be the same',
     );
   }
 }
 
-if (DOCUMENSO_ENCRYPTION_KEY === 'CAFEBABE') {
-  console.warn('*********************************************************************');
-  console.warn('*');
-  console.warn('*');
+if (DENCRYPTION_KEY === 'CAFEBABE') {
   console.warn('Please change the encryption key from the default value of "CAFEBABE"');
-  console.warn('*');
-  console.warn('*');
-  console.warn('*********************************************************************');
 }
