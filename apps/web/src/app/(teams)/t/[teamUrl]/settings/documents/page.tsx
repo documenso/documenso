f108db6e3461ad38ba090a5a09f9a1830ea24271
Catalin Pit <25515812+catalinpit@users.noreply.github.com>
2024-10-09 13:58:14 +0300

    feat: add global settings for teams

---
+++ apps/web/src/app/(teams)/t/[teamUrl]/settings/documents/page.tsx
@@ -0,0 +1,35 @@
+import { msg } from '@lingui/macro';
+import { useLingui } from '@lingui/react';
+
+import { setupI18nSSR } from '@documenso/lib/client-only/providers/i18n.server';
+import { getRequiredServerComponentSession } from '@documenso/lib/next-auth/get-server-component-session';
+import { getTeamByUrl } from '@documenso/lib/server-only/team/get-team';
+
+import { SettingsHeader } from '~/components/(dashboard)/settings/layout/header';
+import { TeamDocumentSettings } from '~/components/forms/team-document-settings';
+
+export type TeamDocumentSettingsPageProps = { params: { teamUrl: string } };
+
+export default async function TeamDocumentSettingsPage({ params }: TeamDocumentSettingsPageProps) {
+  setupI18nSSR();
+
+  const { _ } = useLingui();
+
+  const { teamUrl } = params;
+
+  const { user } = await getRequiredServerComponentSession();
+  const team = await getTeamByUrl({ userId: user.id, teamUrl });
+
+  console.log({ team });
+
+  return (
+    <div>
+      <SettingsHeader
+        title={_(msg`Document Settings`)}
+        subtitle={_(msg`The global settings for the documents in your team account.`)}
+      />
+
+      <TeamDocumentSettings />
+    </div>
+  );
+}
---
+++ apps/web/src/components/forms/team-document-settings.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export const TeamDocumentSettings = () => {
+  console.log('TeamDocumentSettings');
+
+  return (
+    <div>
+      <h1>Team Document Settings</h1>
+    </div>
+  );
+};
