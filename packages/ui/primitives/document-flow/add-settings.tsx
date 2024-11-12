3e1ef925966c532f06a5030625b0f2a754337eaa
David Nguyen <davidngu28@gmail.com>
2024-11-12 15:59:04 +0700

    fix: missing trans tags

--- packages/ui/primitives/document-flow/add-settings.tsx
+++ packages/ui/primitives/document-flow/add-settings.tsx
@@ -180,9 +180,11 @@ export const AddSettingsFormPartial = ({
                       </TooltipTrigger>
 
                       <TooltipContent className="text-foreground max-w-md space-y-2 p-4">
-                        Controls the language for the document, including the language to be used
-                        for email notifications, and the final certificate that is generated and
-                        attached to the document.
+                        <Trans>
+                          Controls the language for the document, including the language to be used
+                          for email notifications, and the final certificate that is generated and
+                          attached to the document.
+                        </Trans>
                       </TooltipContent>
                     </Tooltip>
                   </FormLabel>
