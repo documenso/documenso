74382e21e7523afc2d52d36c1da84c8b87f5c044
David Nguyen <davidngu28@gmail.com>
2024-12-26 17:25:14 +1100

    feat: add get recipient route (#1553)

--- packages/trpc/server/template-router/router.ts
+++ packages/trpc/server/template-router/router.ts
@@ -445,7 +445,7 @@ export const templateRouter = router({
     }),
 
   /**
-   * @internal
+   * @private
    */
   updateTemplateTypedSignatureSettings: authenticatedProcedure
     .input(ZUpdateTemplateTypedSignatureSettingsMutationSchema)
