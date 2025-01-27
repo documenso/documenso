b01eaceeb8ce57a1d662fe81890b2b9d448c389d
Catalin Pit <25515812+catalinpit@users.noreply.github.com>
2025-01-27 16:36:53 +0200

    feat: allow same signer email multiple times

--- packages/ui/primitives/document-flow/add-signers.types.ts
+++ packages/ui/primitives/document-flow/add-signers.types.ts
@@ -6,34 +6,22 @@ import { ZRecipientActionAuthTypesSchema } from '@documenso/lib/types/document-a
 import { ZMapNegativeOneToUndefinedSchema } from './add-settings.types';
 import { DocumentSigningOrder, RecipientRole } from '.prisma/client';
 
-export const ZAddSignersFormSchema = z
-  .object({
-    signers: z.array(
-      z.object({
-        formId: z.string().min(1),
-        nativeId: z.number().optional(),
-        email: z
-          .string()
-          .email({ message: msg`Invalid email`.id })
-          .min(1),
-        name: z.string(),
-        role: z.nativeEnum(RecipientRole),
-        signingOrder: z.number().optional(),
-        actionAuth: ZMapNegativeOneToUndefinedSchema.pipe(
-          ZRecipientActionAuthTypesSchema.optional(),
-        ),
-      }),
-    ),
-    signingOrder: z.nativeEnum(DocumentSigningOrder),
-  })
-  .refine(
-    (schema) => {
-      const emails = schema.signers.map((signer) => signer.email.toLowerCase());
-
-      return new Set(emails).size === emails.length;
-    },
-    // Dirty hack to handle errors when .root is populated for an array type
-    { message: msg`Signers must have unique emails`.id, path: ['signers__root'] },
-  );
+export const ZAddSignersFormSchema = z.object({
+  signers: z.array(
+    z.object({
+      formId: z.string().min(1),
+      nativeId: z.number().optional(),
+      email: z
+        .string()
+        .email({ message: msg`Invalid email`.id })
+        .min(1),
+      name: z.string(),
+      role: z.nativeEnum(RecipientRole),
+      signingOrder: z.number().optional(),
+      actionAuth: ZMapNegativeOneToUndefinedSchema.pipe(ZRecipientActionAuthTypesSchema.optional()),
+    }),
+  ),
+  signingOrder: z.nativeEnum(DocumentSigningOrder),
+});
 
 export type TAddSignersFormSchema = z.infer<typeof ZAddSignersFormSchema>;
