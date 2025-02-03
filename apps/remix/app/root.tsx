d1913dbf9cef007dc64bc63bc04a1099d367cd3c
David Nguyen <davidngu28@gmail.com>
2025-02-03 20:09:35 +1100

    fix: wip

--- apps/remix/app/root.tsx
+++ apps/remix/app/root.tsx
@@ -75,6 +75,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
     <html translate="no" lang={lang} data-theme={theme ?? ''}>
       <head>
         <meta charSet="utf-8" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
