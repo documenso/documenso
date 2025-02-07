cdc65bfcc7fe9aa584f68b450a8b79285e50530b
Kacper <kacper@kacperadler.info>
2025-02-07 13:42:35 +0100

    Hardcoded schema "public"
    https://github.com/documenso/documenso/issues/1640

--- packages/prisma/migrations/20240205120648_create_delete_account/migration.sql
+++ packages/prisma/migrations/20240205120648_create_delete_account/migration.sql
@@ -1,9 +1,9 @@
 -- Create deleted@documenso.com
 DO $$
 BEGIN  
-  IF NOT EXISTS (SELECT 1 FROM "public"."User" WHERE "email" = 'deleted-account@documenso.com') THEN  
+  IF NOT EXISTS (SELECT 1 FROM "User" WHERE "email" = 'deleted-account@documenso.com') THEN  
     INSERT INTO
-      "public"."User" (
+      "User" (
         "email",
         "emailVerified",
         "password",
@@ -22,8 +22,8 @@ BEGIN
         NOW(),
         NOW(),
         NOW(),
-        ARRAY['USER'::TEXT]::"public"."Role" [],
-        CAST('GOOGLE'::TEXT AS "public"."IdentityProvider"),
+        ARRAY['USER'::TEXT]::"Role" [],
+        CAST('GOOGLE'::TEXT AS "IdentityProvider"),
         FALSE
       );
   END IF;  
