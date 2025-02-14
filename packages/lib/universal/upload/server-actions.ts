0fdf42c4305effa16fa8176430310546ed22899b
Kenton Duprey <kenton@hausofweb.com>
2025-02-14 00:31:01 -0500

    feat(upload): enhance AWS integration with role-based credentials and update environment variables
    Signed-off-by: Kenton Duprey <kenton@hausofweb.com>

--- packages/lib/universal/upload/server-actions.ts
+++ packages/lib/universal/upload/server-actions.ts
@@ -9,7 +9,9 @@ import {
   PutObjectCommand,
   S3Client,
 } from '@aws-sdk/client-s3';
+import { createCredentialChain, fromEnv, fromWebToken } from '@aws-sdk/credential-providers';
 import slugify from '@sindresorhus/slugify';
+import { getVercelOidcToken } from '@vercel/functions/oidc';
 import { type JWT, getToken } from 'next-auth/jwt';
 import { env } from 'next-runtime-env';
 import path from 'node:path';
@@ -19,7 +21,7 @@ import { ONE_HOUR, ONE_SECOND } from '../../constants/time';
 import { alphaid } from '../id';
 
 export const getPresignPostUrl = async (fileName: string, contentType: string) => {
-  const client = getS3Client();
+  const client = await getS3Client();
 
   const { getSignedUrl } = await import('@aws-sdk/s3-request-presigner');
 
@@ -60,7 +62,7 @@ export const getPresignPostUrl = async (fileName: string, contentType: string) =
 };
 
 export const getAbsolutePresignPostUrl = async (key: string) => {
-  const client = getS3Client();
+  const client = await getS3Client();
 
   const { getSignedUrl: getS3SignedUrl } = await import('@aws-sdk/s3-request-presigner');
 
@@ -92,7 +94,7 @@ export const getPresignGetUrl = async (key: string) => {
     return { key, url };
   }
 
-  const client = getS3Client();
+  const client = await getS3Client();
 
   const { getSignedUrl: getS3SignedUrl } = await import('@aws-sdk/s3-request-presigner');
 
@@ -109,7 +111,7 @@ export const getPresignGetUrl = async (key: string) => {
 };
 
 export const deleteS3File = async (key: string) => {
-  const client = getS3Client();
+  const client = await getS3Client();
 
   await client.send(
     new DeleteObjectCommand({
@@ -119,26 +121,25 @@ export const deleteS3File = async (key: string) => {
   );
 };
 
-const getS3Client = () => {
+const getS3Client = async () => {
   const NEXT_PUBLIC_UPLOAD_TRANSPORT = env('NEXT_PUBLIC_UPLOAD_TRANSPORT');
 
   if (NEXT_PUBLIC_UPLOAD_TRANSPORT !== 's3') {
     throw new Error('Invalid upload transport');
   }
 
-  const hasCredentials =
-    process.env.NEXT_PRIVATE_UPLOAD_ACCESS_KEY_ID &&
-    process.env.NEXT_PRIVATE_UPLOAD_SECRET_ACCESS_KEY;
-
   return new S3Client({
     endpoint: process.env.NEXT_PRIVATE_UPLOAD_ENDPOINT || undefined,
     forcePathStyle: process.env.NEXT_PRIVATE_UPLOAD_FORCE_PATH_STYLE === 'true',
     region: process.env.NEXT_PRIVATE_UPLOAD_REGION || 'us-east-1',
-    credentials: hasCredentials
-      ? {
-          accessKeyId: String(process.env.NEXT_PRIVATE_UPLOAD_ACCESS_KEY_ID),
-          secretAccessKey: String(process.env.NEXT_PRIVATE_UPLOAD_SECRET_ACCESS_KEY),
-        }
-      : undefined,
+    credentials: createCredentialChain(
+      process.env.NEXT_PRIVATE_UPLOAD_AWS_ROLE_ARN
+        ? fromWebToken({
+            roleArn: process.env.NEXT_PRIVATE_UPLOAD_AWS_ROLE_ARN,
+            durationSeconds: 3600,
+            webIdentityToken: await getVercelOidcToken(),
+          })
+        : fromEnv(),
+    ),
   });
 };
