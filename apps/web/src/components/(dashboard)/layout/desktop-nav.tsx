9913c3d5d7f81137da4c687ee46eacf1457b5e99
Jenil <jenilsavani1@gmail.com>
2025-03-12 19:10:18 +0530

    fix: limit maximum width of desktop nav button for better layout

--- apps/web/src/components/(dashboard)/layout/desktop-nav.tsx
+++ apps/web/src/components/(dashboard)/layout/desktop-nav.tsx
@@ -73,7 +73,7 @@ export const DesktopNav = ({ className, setIsCommandMenuOpen, ...props }: Deskto
 
       <Button
         variant="outline"
-        className="text-muted-foreground flex w-full items-center justify-between rounded-lg"
+        className="text-muted-foreground flex w-full max-w-96 items-center justify-between rounded-lg"
         onClick={() => setIsCommandMenuOpen(true)}
       >
         <div className="flex items-center">
