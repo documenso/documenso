8906884efcf110a4c8591424e47df3fd68288b41
Catalin Pit <me+github@catalins.tech>
2025-03-14 14:44:58 +0200

    chore: update links

--- apps/remix/app/components/forms/signup.tsx
+++ apps/remix/app/components/forms/signup.tsx
@@ -538,7 +538,14 @@ export const SignUpForm = ({
               to="https://documen.so/terms"
               className="text-documenso-700 duration-200 hover:opacity-70"
             >
-              Terms and Privacy Policy
+              Terms of Service
+            </Link>{' '}
+            and{' '}
+            <Link
+              to="https://documen.so/privacy"
+              className="text-documenso-700 duration-200 hover:opacity-70"
+            >
+              Privacy Policy
             </Link>
             .
           </Trans>
