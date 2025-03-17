673ee3e396d7f5190272fe5acb93efa7cca44dff
stonespren <51301343+stonespren@users.noreply.github.com>
2025-03-17 19:30:17 +0000

    fix: instructions for ecr deploy

--- scripts/deploy-ecr.sh
+++ scripts/deploy-ecr.sh
@@ -3,6 +3,26 @@
 # Exit on error.
 set -eo pipefail
 
+# If getting permission errors, run `sudo chown -R $(whoami) /workspaces/documenso`
+# If getting docker store errors, run `nano ~/.docker/config.json` and remove credsStore
+
+if ! command aws --version &> /dev/null; then
+
+    # Install dependencies
+    sudo apt update && sudo apt install -y curl unzip
+
+    # Download and install AWS CLI v2
+    sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    sudo unzip awscliv2.zip
+    sudo ./aws/install
+
+    # Clean up
+    rm -rf awscliv2.zip aws
+
+    # Verify installation
+    echo "AWS cli is installed with version: $(aws --version)"
+fi
+
 SCRIPT_DIR="$(readlink -f "$(dirname "$0")")"
 MONOREPO_ROOT="$(readlink -f "$SCRIPT_DIR/../")"
 CI_COMMIT_SHORT_SHA="$(git rev-parse --short HEAD)"
@@ -13,20 +33,6 @@ npm run prisma:generate
 npm run prisma:migrate-deploy
 
 npm run build
-
-# Install dependencies
-sudo apt-get update && sudo apt-get install -y curl unzip
-
-# Download and install AWS CLI v2
-sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-sudo unzip awscliv2.zip
-sudo ./aws/install
-
-# Clean up
-rm -rf awscliv2.zip aws
-
-# Verify installation
-aws --version
 
 # ECR is in production only, so use prod credentials
 
