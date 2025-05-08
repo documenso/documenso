6b0ca53caed2a66f86eaf4e6a8624b2e7e08e92f
Lucas Smith <me@lucasjamessmith.me>
2025-05-08 15:51:44 +1000

    fix: make lang cookie httpOnly

--- apps/remix/app/storage/lang-cookie.server.ts
+++ apps/remix/app/storage/lang-cookie.server.ts
@@ -3,4 +3,6 @@ import { createCookie } from 'react-router';
 export const langCookie = createCookie('lang', {
   path: '/',
   maxAge: 60 * 60 * 24 * 365 * 2,
+  httpOnly: true,
+  secure: process.env.NODE_ENV === 'production',
 });
