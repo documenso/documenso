d6e41ad6f78364b22ee2a0f28bd9d98bb7281da2
Lucas Smith <me@lucasjamessmith.me>
2025-05-08 15:54:41 +1000

    fix: use env() method

--- apps/remix/app/storage/lang-cookie.server.ts
+++ apps/remix/app/storage/lang-cookie.server.ts
@@ -1,8 +1,10 @@
 import { createCookie } from 'react-router';
 
+import { env } from '@documenso/lib/utils/env';
+
 export const langCookie = createCookie('lang', {
   path: '/',
   maxAge: 60 * 60 * 24 * 365 * 2,
   httpOnly: true,
-  secure: process.env.NODE_ENV === 'production',
+  secure: env('NODE_ENV') === 'production',
 });
