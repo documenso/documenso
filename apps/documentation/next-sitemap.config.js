855ffe938a9ea3c258b2759bd0b4855dc0f5196d
Shubham Palriwala <spalriwalau@gmail.com>
2025-06-04 19:19:37 +0530

    feat: sitemap auto-generation for docs

---
+++ apps/documentation/next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://docs.documenso.com', // Replace with your actual site URL
+  generateRobotsTxt: true, // Generates robots.txt
+  outDir: 'out', // Output directory for the sitemap
+};
