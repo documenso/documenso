004ceb0d2a2d028c8fa466558d008a7213ca8d27
Catalin Documenso <catalin@Catalins-MacBook-Pro.local>
2025-06-28 20:29:31 +0300

    chore: api v2 tests

--- packages/app-tests/e2e/api/v2/test-unauthorized-api-access.spec.ts
+++ packages/app-tests/e2e/api/v2/test-unauthorized-api-access.spec.ts
@@ -80,6 +80,7 @@ test.describe('Unauthorized Access - Document API V2', () => {
     expect(res.status()).toBe(401);
   });
 
+  // fix this test
   test('should block unauthorized access to document move endpoint', async ({ request }) => {
     const doc = await seedDraftDocument(userA, teamA.id, []);
 
@@ -153,5 +154,273 @@ test.describe('Unauthorized Access - Document API V2', () => {
 
     expect(res.ok()).toBeFalsy();
     expect(res.status()).toBe(500);
+  });
+
+  test('should block unauthorized access to document duplicate endpoint', async ({ request }) => {
+    const doc = await seedDraftDocument(userA, teamA.id, ['test@example.com']);
+
+    const res = await request.post(`${WEBAPP_BASE_URL}/api/v2-beta/document/duplicate`, {
+      headers: { Authorization: `Bearer ${tokenB}` },
+      data: { documentId: doc.id },
+    });
+
+    expect(res.ok()).toBeFalsy();
+    expect(res.status()).toBe(404);
+  });
+
+  test('should block unauthorized access to document field endpoint', async ({ request }) => {
+    const { user: userRecipient } = await seedUser();
+
+    const doc = await seedDraftDocument(userA, teamA.id, [userRecipient.email]);
+
+    const recipient = await prisma.recipient.findFirst({
+      where: {
+        documentId: doc.id,
+        email: userRecipient.email,
+      },
+    });
+
+    const field = await prisma.field.create({
+      data: {
+        documentId: doc.id,
+        recipientId: recipient!.id,
+        type: 'TEXT',
+        page: 1,
+        positionX: 1,
+        positionY: 1,
+        width: 1,
+        height: 1,
+        customText: '',
+        inserted: false,
+        fieldMeta: { type: 'text', label: 'Test' },
+      },
+    });
+
+    const res = await request.get(`${WEBAPP_BASE_URL}/api/v2-beta/document/field/${field.id}`, {
+      headers: { Authorization: `Bearer ${tokenB}` },
+    });
+
+    expect(res.ok()).toBeFalsy();
+    expect(res.status()).toBe(404);
+  });
+
+  test('should block unauthorized access to document field create endpoint', async ({
+    request,
+  }) => {
+    const { user: userRecipient } = await seedUser();
+
+    const doc = await seedDraftDocument(userA, teamA.id, [userRecipient.email]);
+
+    const recipient = await prisma.recipient.findFirst({
+      where: { documentId: doc.id },
+    });
+
+    const res = await request.post(`${WEBAPP_BASE_URL}/api/v2-beta/document/field/create`, {
+      headers: { Authorization: `Bearer ${tokenB}` },
+      data: {
+        documentId: doc.id,
+        field: {
+          recipientId: recipient!.id,
+          type: 'TEXT',
+          pageNumber: 791.77,
+          pageX: 7845.22,
+          pageY: 6843.16,
+          width: 3932.15,
+          height: 8879.89,
+          fieldMeta: { type: 'text', label: 'Test Field' },
+        },
+      },
+    });
+
+    expect(res.ok()).toBeFalsy();
+    expect([404, 401, 500]).toContain(res.status());
+  });
+
+  test('should block unauthorized access to document field create-many endpoint', async ({
+    request,
+  }) => {
+    const { user: userRecipient } = await seedUser();
+
+    const doc = await seedDraftDocument(userA, teamA.id, [userRecipient.email]);
+
+    const recipient = await prisma.recipient.findFirst({
+      where: { documentId: doc.id },
+    });
+
+    const res = await request.post(`${WEBAPP_BASE_URL}/api/v2-beta/document/field/create-many`, {
+      headers: { Authorization: `Bearer ${tokenB}` },
+      data: {
+        documentId: doc.id,
+        fields: [
+          {
+            recipientId: recipient!.id,
+            type: 'TEXT',
+            pageNumber: 791.77,
+            pageX: 7845.22,
+            pageY: 6843.16,
+            width: 3932.15,
+            height: 8879.89,
+            fieldMeta: { type: 'text', label: 'First test field' },
+          },
+          {
+            recipientId: recipient!.id,
+            type: 'TEXT',
+            pageNumber: 791.77,
+            pageX: 845.22,
+            pageY: 843.16,
+            width: 932.15,
+            height: 879.89,
+            fieldMeta: { type: 'text', label: 'Second test field' },
+          },
+        ],
+      },
+    });
+
+    expect(res.ok()).toBeFalsy();
+    expect(res.status()).toBe(404);
+  });
+
+  test('should block unauthorized access to document field update endpoint', async ({
+    request,
+  }) => {
+    const { user: userRecipient } = await seedUser();
+
+    const doc = await seedDraftDocument(userA, teamA.id, [userRecipient.email]);
+
+    const recipient = await prisma.recipient.findFirst({
+      where: { documentId: doc.id },
+    });
+
+    const field = await prisma.field.create({
+      data: {
+        documentId: doc.id,
+        recipientId: recipient!.id,
+        type: 'TEXT',
+        page: 1,
+        positionX: 1,
+        positionY: 1,
+        width: 1,
+        height: 1,
+        customText: '',
+        inserted: false,
+        fieldMeta: { type: 'text', label: 'A text field' },
+      },
+    });
+
+    const res = await request.post(`${WEBAPP_BASE_URL}/api/v2-beta/document/field/update`, {
+      headers: { Authorization: `Bearer ${tokenB}` },
+      data: {
+        documentId: doc.id,
+        field: {
+          id: field.id,
+          type: FieldType.TEXT,
+          fieldMeta: { type: 'text', label: 'An updated text field' },
+        },
+      },
+    });
+
+    expect(res.ok()).toBeFalsy();
+    expect(res.status()).toBe(404);
+  });
+
+  test('should block unauthorized access to document field update-many endpoint', async ({
+    request,
+  }) => {
+    const { user: userRecipient } = await seedUser();
+
+    const doc = await seedDraftDocument(userA, teamA.id, [userRecipient.email]);
+
+    const recipient = await prisma.recipient.findFirst({
+      where: { documentId: doc.id },
+    });
+
+    const fields = await prisma.field.createManyAndReturn({
+      data: [
+        {
+          documentId: doc.id,
+          recipientId: recipient!.id,
+          type: FieldType.TEXT,
+          page: 1,
+          positionX: 1,
+          positionY: 1,
+          width: 1,
+          height: 1,
+          customText: '',
+          inserted: false,
+          fieldMeta: { type: 'text', label: 'Test' },
+        },
+        {
+          documentId: doc.id,
+          recipientId: recipient!.id,
+          type: FieldType.NUMBER,
+          page: 1,
+          positionX: 1,
+          positionY: 1,
+          width: 1,
+          height: 1,
+          customText: '',
+          inserted: false,
+          fieldMeta: { type: 'text', label: 'Test' },
+        },
+      ],
+    });
+
+    const res = await request.post(`${WEBAPP_BASE_URL}/api/v2-beta/document/field/update-many`, {
+      headers: { Authorization: `Bearer ${tokenB}` },
+      data: {
+        documentId: doc.id,
+        fields: [
+          {
+            id: fields[0].id,
+            type: FieldType.TEXT,
+            fieldMeta: { type: 'text', label: 'Updated first test field' },
+          },
+          {
+            id: fields[1].id,
+            type: FieldType.NUMBER,
+            fieldMeta: { type: 'number', label: 'Updated second test field' },
+          },
+        ],
+      },
+    });
+
+    expect(res.ok()).toBeFalsy();
+    expect(res.status()).toBe(404);
+  });
+
+  test('should block unauthorized access to document field delete endpoint', async ({
+    request,
+  }) => {
+    const { user: userRecipient } = await seedUser();
+
+    const doc = await seedDraftDocument(userA, teamA.id, [userRecipient.email]);
+
+    const recipient = await prisma.recipient.findFirst({
+      where: { documentId: doc.id },
+    });
+
+    const field = await prisma.field.create({
+      data: {
+        documentId: doc.id,
+        recipientId: recipient!.id,
+        type: FieldType.TEXT,
+        page: 1,
+        positionX: 1,
+        positionY: 1,
+        width: 1,
+        height: 1,
+        customText: '',
+        inserted: false,
+        fieldMeta: { type: 'text', label: 'Test' },
+      },
+    });
+
+    const res = await request.post(`${WEBAPP_BASE_URL}/api/v2-beta/document/field/delete`, {
+      headers: { Authorization: `Bearer ${tokenB}` },
+      data: { fieldId: field.id },
+    });
+
+    expect(res.ok()).toBeFalsy();
+    expect(res.status()).toBe(404);
   });
 });
