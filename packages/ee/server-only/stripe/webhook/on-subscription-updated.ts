192e79665ef59fdc37e6f639c67f68749e46be4f
David Nguyen <davidngu28@gmail.com>
2025-07-16 14:35:42 +1000

    fix: restrict individual plans to upgrade only (#1900)
    Prevent users from creating a separate organisation for individual
    plans. Only applies to users who have 1 personal organisation and are
    subscribing to the "Individual" plan.
    
    The reason for this change is to keep the layout in the "Personal" mode
    which means it doesn't show a bunch of unusable "organisation" related
    UI.

--- packages/ee/server-only/stripe/webhook/on-subscription-updated.ts
+++ packages/ee/server-only/stripe/webhook/on-subscription-updated.ts
@@ -1,8 +1,9 @@
-import { SubscriptionStatus } from '@prisma/client';
+import { OrganisationType, SubscriptionStatus } from '@prisma/client';
 import { match } from 'ts-pattern';
 
 import { createOrganisationClaimUpsertData } from '@documenso/lib/server-only/organisation/create-organisation';
 import { type Stripe, stripe } from '@documenso/lib/server-only/stripe';
+import { INTERNAL_CLAIM_ID } from '@documenso/lib/types/subscription';
 import { prisma } from '@documenso/prisma';
 
 export type OnSubscriptionUpdatedOptions = {
@@ -91,6 +92,22 @@ export const onSubscriptionUpdated = async ({
     subscription.status === 'trialing' && subscription.trial_end
       ? new Date(subscription.trial_end * 1000)
       : new Date(subscription.current_period_end * 1000);
+
+  // Migrate the organisation type if it is no longer an individual plan.
+  if (
+    updatedSubscriptionClaim.id !== INTERNAL_CLAIM_ID.INDIVIDUAL &&
+    updatedSubscriptionClaim.id !== INTERNAL_CLAIM_ID.FREE &&
+    organisation.type === OrganisationType.PERSONAL
+  ) {
+    await prisma.organisation.update({
+      where: {
+        id: organisation.id,
+      },
+      data: {
+        type: OrganisationType.ORGANISATION,
+      },
+    });
+  }
 
   await prisma.$transaction(async (tx) => {
     await tx.subscription.update({
