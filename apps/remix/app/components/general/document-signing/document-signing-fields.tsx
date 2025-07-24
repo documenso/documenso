7623c029b1f6d7f076e1347d15b3a80262dbd1e9
David Nguyen <davidngu28@gmail.com>
2025-07-24 11:07:47 +1000

    fix: correctly render new lines in text fields

--- apps/remix/app/components/general/document-signing/document-signing-fields.tsx
+++ apps/remix/app/components/general/document-signing/document-signing-fields.tsx
@@ -12,7 +12,7 @@ export const DocumentSigningFieldsLoader = () => {
 
 export const DocumentSigningFieldsUninserted = ({ children }: { children: React.ReactNode }) => {
   return (
-    <p className="group-hover:text-primary text-foreground group-hover:text-recipient-green text-[clamp(0.425rem,25cqw,0.825rem)] duration-200">
+    <p className="text-foreground group-hover:text-recipient-green whitespace-pre-wrap text-[clamp(0.425rem,25cqw,0.825rem)] duration-200">
       {children}
     </p>
   );
@@ -37,7 +37,7 @@ export const DocumentSigningFieldsInserted = ({
     <div className="flex h-full w-full items-center overflow-hidden">
       <p
         className={cn(
-          'text-foreground w-full text-left text-[clamp(0.425rem,25cqw,0.825rem)] duration-200',
+          'text-foreground w-full whitespace-pre-wrap text-left text-[clamp(0.425rem,25cqw,0.825rem)] duration-200',
           {
             '!text-center': textAlign === 'center',
             '!text-right': textAlign === 'right',
