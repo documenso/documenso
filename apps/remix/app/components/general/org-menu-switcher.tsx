1b901e4422f42d2e438d841db87fb3a07b973111
Catalin Pit <me+github@catalins.tech>
2025-08-19 13:59:03 +0300

    chore: add support option (#1853)

--- apps/remix/app/components/general/org-menu-switcher.tsx
+++ apps/remix/app/components/general/org-menu-switcher.tsx
@@ -321,6 +321,19 @@ export const OrgMenuSwitcher = () => {
                 <Trans>Language</Trans>
               </DropdownMenuItem>
 
+              {currentOrganisation && (
+                <DropdownMenuItem className="text-muted-foreground px-4 py-2" asChild>
+                  <Link
+                    to={{
+                      pathname: `/o/${currentOrganisation.url}/support`,
+                      search: currentTeam ? `?team=${currentTeam.id}` : '',
+                    }}
+                  >
+                    <Trans>Support</Trans>
+                  </Link>
+                </DropdownMenuItem>
+              )}
+
               <DropdownMenuItem
                 className="text-muted-foreground hover:!text-muted-foreground px-4 py-2"
                 onSelect={async () => authClient.signOut()}
