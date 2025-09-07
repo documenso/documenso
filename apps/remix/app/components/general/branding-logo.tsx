1a27e147940fb644813b57885e69efb71d588679
amai-solutions <info@amai.solutions>
2025-09-07 19:43:22 +0200

    Update branding-logo.tsx

--- apps/remix/app/components/general/branding-logo.tsx
+++ apps/remix/app/components/general/branding-logo.tsx
@@ -1,12 +1,16 @@
-import type { SVGAttributes } from 'react';
-import Image from "next/image";
+import type { SVGAttributes } from "react";
 
 export type LogoProps = SVGAttributes<SVGSVGElement>;
 
 export const BrandingLogo = ({ ...props }: LogoProps) => {
   return (
     <div className="flex items-center">
-      <Image src="https://gcmasesores.io/images/logo-gcm-white.png" alt="GCM Asesores" width={140} height={28} priority />
+      <img
+        src="https://gcmasesores.io/images/logo-gcm-white.png"
+        alt="GCM Asesores"
+        width={140}
+        height={28}
+      />
     </div>
   );
 };
