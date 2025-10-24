a1bc5a0a35315b4983b19a22298fa8dc6b345e82
David Nguyen <davidngu28@gmail.com>
2025-10-24 14:04:59 +1100

    fix: migration

--- packages/prisma/migrations/20251023021214_add_envelope_item_limits/migration.sql
+++ packages/prisma/migrations/20251023021214_add_envelope_item_limits/migration.sql
@@ -3,7 +3,7 @@ ALTER TABLE "SubscriptionClaim" ADD COLUMN     "envelopeItemCount" INTEGER;
 ALTER TABLE "OrganisationClaim" ADD COLUMN     "envelopeItemCount" INTEGER;
 
 -- Update ALL subscriptions to have 5 envelope items
-UPDATE "SubscriptionClaim" SET "envelopeItemCount" = 10;
+UPDATE "SubscriptionClaim" SET "envelopeItemCount" = 5;
 
 -- Override platform and enterprise claims to have 10 envelope items
 UPDATE "SubscriptionClaim" SET "envelopeItemCount" = 10 WHERE "id" = 'platform';
