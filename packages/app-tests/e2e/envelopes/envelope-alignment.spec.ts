3efe0de52fb96ef7d6c5c673dd7f913816b28e72
David Nguyen <davidngu28@gmail.com>
2025-10-31 17:43:33 +1100

    fix: increase threshold

--- packages/app-tests/e2e/envelopes/envelope-alignment.spec.ts
+++ packages/app-tests/e2e/envelopes/envelope-alignment.spec.ts
@@ -257,7 +257,7 @@ const compareSignedPdfWithImages = async ({
       width,
       height,
       {
-        threshold: 0.1,
+        threshold: 0.2,
         // includeAA: true, // This allows stricter testing.
       },
     );
