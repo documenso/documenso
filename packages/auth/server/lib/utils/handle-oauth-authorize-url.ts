45845d87474457f5358546d8e4f03a3b1f964e66
Lucas Smith <me@lucasjamessmith.me>
2025-11-20 13:07:25 +1100

    fix: support selecting prompt type instead

--- packages/auth/server/lib/utils/handle-oauth-authorize-url.ts
+++ packages/auth/server/lib/utils/handle-oauth-authorize-url.ts
@@ -27,13 +27,13 @@ type HandleOAuthAuthorizeUrlOptions = {
   /**
    * Optional prompt to pass to the authorization endpoint.
    */
-  prompt?: 'login' | 'consent' | 'select_account';
+  prompt?: 'none' | 'login' | 'consent' | 'select_account';
 };
 
 const oauthCookieMaxAge = 60 * 10; // 10 minutes.
 
 export const handleOAuthAuthorizeUrl = async (options: HandleOAuthAuthorizeUrlOptions) => {
-  const { c, clientOptions, redirectPath, prompt = 'login' } = options;
+  const { c, clientOptions, redirectPath } = options;
 
   if (!clientOptions.clientId || !clientOptions.clientSecret) {
     throw new AppError(AppErrorCode.NOT_SETUP);
@@ -63,7 +63,9 @@ export const handleOAuthAuthorizeUrl = async (options: HandleOAuthAuthorizeUrlOp
   );
 
   // Pass the prompt to the authorization endpoint.
-  if (process.env.NEXT_PRIVATE_OIDC_DISABLE_LOGIN_PARAMETER !== 'true') {
+  if (process.env.NEXT_PRIVATE_OIDC_PROMPT !== '') {
+    const prompt = process.env.NEXT_PRIVATE_OIDC_PROMPT ?? 'login';
+
     url.searchParams.append('prompt', prompt);
   }
 
