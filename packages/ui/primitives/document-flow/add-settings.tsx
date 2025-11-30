59da8b3db6d3538922f9824c6ff2a49ca2082f3f
Konrad <11725227+mKoonrad@users.noreply.github.com>
2025-11-30 12:59:01 +0100

    fix(i18n): mark strings for translation

--- packages/ui/primitives/document-flow/add-settings.tsx
+++ packages/ui/primitives/document-flow/add-settings.tsx
@@ -312,7 +312,7 @@ export const AddSettingsFormPartial = ({
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel className="flex flex-row items-center">
-                      Document visibility
+                      <Trans>Document visibility</Trans>
                       <DocumentVisibilityTooltip />
                     </FormLabel>
 
