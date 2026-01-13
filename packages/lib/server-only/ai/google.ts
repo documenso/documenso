729875e4e3f3035edf4c474e4522d55574f17888
johann.zschorlich <zschorlichjohann@gmail.com>
2026-01-13 08:36:05 +0100

    chore(docker): disable temporary google vertex ai key to avoid type error

--- packages/lib/server-only/ai/google.ts
+++ packages/lib/server-only/ai/google.ts
@@ -5,5 +5,5 @@ import { env } from '../../utils/env';
 export const vertex = createVertex({
   project: env('GOOGLE_VERTEX_PROJECT_ID'),
   location: env('GOOGLE_VERTEX_LOCATION') || 'global',
-  apiKey: env('GOOGLE_VERTEX_API_KEY'),
+  // apiKey: env('GOOGLE_VERTEX_API_KEY'),
 });
