6c19f69b7eaf1fa599eebc17aad9300fbfb401a5
Konrad <11725227+mKoonrad@users.noreply.github.com>
2026-02-24 21:50:12 +0100

    fix(i18n): mark dropdown and radio placeholder for translation

--- packages/lib/types/field-meta.ts
+++ packages/lib/types/field-meta.ts
@@ -347,7 +347,6 @@ export const FIELD_CHECKBOX_META_DEFAULT_VALUES: TCheckboxFieldMeta = {
 export const FIELD_DROPDOWN_META_DEFAULT_VALUES: TDropdownFieldMeta = {
   type: 'dropdown',
   fontSize: DEFAULT_FIELD_FONT_SIZE,
-  values: [{ value: 'Option 1' }],
   defaultValue: '',
   required: false,
   readOnly: false,
