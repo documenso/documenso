0db3005cac3e299271b913e3af809c2371a59523
David Nguyen <davidngu28@gmail.com>
2026-03-02 22:23:00 +1100

    chore: remove redundant change

--- packages/lib/constants/pdf-viewer.ts
+++ packages/lib/constants/pdf-viewer.ts
@@ -1,8 +1,3 @@
 // Keep these two constants in sync.
 export const PDF_VIEWER_PAGE_SELECTOR = '.react-pdf__Page';
 export const PDF_VIEWER_PAGE_CLASSNAME = 'react-pdf__Page z-0';
-
-/**
- * Changing this will require large testing.
- */
-export const PDF_IMAGE_RENDER_SCALE = 2;
