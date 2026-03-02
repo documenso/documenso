454f73f2a9fb06960c511028acc0702d51bcaba9
Lucas Smith <me@lucasjamessmith.me>
2026-03-02 13:47:40 +1100

    chore: remove old docs (#2550)

--- apps/documentation/next-sitemap.config.js
+++
@@ -1,5 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://docs.documenso.com', // Replace with your actual site URL
-  generateRobotsTxt: true, // Generates robots.txt
-};
