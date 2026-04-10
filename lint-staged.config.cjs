5939a08de379d8bd24186aa143ac97868f86eeed
David Nguyen <davidngu28@gmail.com>
2026-04-10 14:18:13 +1000

    fix: resolve issues

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,5 +1,5 @@
 /** @type {import('lint-staged').Config} */
 module.exports = {
-  '**/*.{ts,tsx,cts,mts,js,jsx,cjs,mjs,json,css}': 'npm run lint:fix -- --staged',
+  '**/*.{ts,tsx,cts,mts,js,jsx,cjs,mjs,json,css}': 'npm run lint:staged',
   '**/*/package.json': 'npm run precommit',
 };
