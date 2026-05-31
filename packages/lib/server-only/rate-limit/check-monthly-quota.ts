44c4826e92d2ac5124e6072a7418998f20ac4a5d
David Nguyen <davidngu28@gmail.com>
2026-05-31 13:34:10 +1000

    fix: track monthly usage for unlimited quotas (#2894)

--- packages/lib/server-only/rate-limit/check-monthly-quota.ts
+++ packages/lib/server-only/rate-limit/check-monthly-quota.ts
@@ -19,10 +19,6 @@ const COUNTER_COLUMN = {
 } as const satisfies Record<LimitCounter, string>;
 
 export const checkMonthlyQuota = async (opts: CheckMonthlyQuotaOptions): Promise<void> => {
-  if (opts.quota === null) {
-    return;
-  }
-
   if (opts.quota === 0) {
     throw new AppError(AppErrorCode.TOO_MANY_REQUESTS, {
       message:
@@ -51,6 +47,11 @@ export const checkMonthlyQuota = async (opts: CheckMonthlyQuotaOptions): Promise
       [column]: opts.count,
     },
   });
+
+  // For unlimited quotas, we still allow the request to send so we can collect the monthly stat.
+  if (opts.quota === null) {
+    return;
+  }
 
   const newCount = latestMonthlyStat[column];
   const previousCount = newCount - opts.count;
